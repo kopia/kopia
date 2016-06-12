47ea638141a12d11da9ef7f4f91540bb13e5776b
Jarek Kowalski <jaak@jkowalski.net>
2016-06-11 21:57:00 -0700

    cmdline tool

--- cmd/kopia/command_vault_show.go
+++ cmd/kopia/command_vault_show.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"os"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	vaultShowCommand = vaultCommands.Command("show", "Show contents of a vault item")
-	vaultShowID      = vaultShowCommand.Arg("id", "ID of the vault item to show").String()
+	vaultShowCommand   = vaultCommands.Command("show", "Show contents of a vault item")
+	vaultShowID        = vaultShowCommand.Arg("id", "ID of the vault item to show").String()
+	vaultShowJSON      = vaultShowCommand.Flag("json", "Pretty-print JSON").Short('j').Bool()
+	vaultShowNoNewLine = vaultShowCommand.Flag("nonewline", "Do not emit newline").Short('n').Bool()
 )
 
 func init() {
@@ -26,7 +30,17 @@ func showVaultObject(context *kingpin.ParseContext) error {
 		return err
 	}
 
-	os.Stdout.Write(b)
+	if *vaultShowJSON {
+		var buf bytes.Buffer
+		json.Indent(&buf, b, "", "  ")
+		buf.WriteTo(os.Stdout)
+	} else {
+		os.Stdout.Write(b)
+	}
+
+	if !*vaultShowNoNewLine {
+		os.Stdout.WriteString("\n")
+	}
 
 	return nil
 }
