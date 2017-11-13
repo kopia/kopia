40a367f79e2af55393e7d8e832e1303a5e05afcd
Jarek Kowalski <jaak@jkowalski.net>
2017-11-12 22:43:21 -0800

    added 'block rm' subcommand

---
+++ cli/command_block_rm.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"github.com/kopia/kopia/repo"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	removeBlockCommand = blockCommands.Command("remove", "Remove block(s)").Alias("rm")
+
+	removeBlockIDs = removeBlockCommand.Arg("id", "IDs of blocks to remove").Required().Strings()
+)
+
+func runRemoveBlockCommand(context *kingpin.ParseContext) error {
+	rep := mustOpenRepository(nil)
+	defer rep.Close()
+
+	for _, blockID := range *removeBlockIDs {
+		if err := removeBlock(rep, blockID); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func removeBlock(r *repo.Repository, blockID string) error {
+	return r.Blocks.DeleteBlock(blockID)
+}
+
+func init() {
+	setupShowCommand(removeBlockCommand)
+	removeBlockCommand.Action(runRemoveBlockCommand)
+}
