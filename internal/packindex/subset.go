db0fd9ed28f29e227ec72103928cbc11b334f303
Jarek Kowalski <jaak@jkowalski.net>
2018-05-06 16:37:35 -0700

    added packindex.IsSubset()

---
+++ internal/packindex/subset.go
@@ -0,0 +1,33 @@
+package packindex
+
+// IsSubset returns true if all entries in index 'a' are contained in index 'b'.
+func IsSubset(a, b Index) bool {
+	if a.EntryCount() > b.EntryCount() {
+		// no point iterating.
+		return false
+	}
+
+	done := make(chan bool)
+	defer close(done)
+
+	ach := iterateChan("", a, done)
+	bch := iterateChan("", b, done)
+
+	for ait := range ach {
+		bit, ok := <-bch
+		if !ok {
+			return false
+		}
+		for bit.BlockID < ait.BlockID {
+			bit, ok = <-bch
+			if !ok {
+				return false
+			}
+		}
+
+		if bit.BlockID != ait.BlockID {
+			return false
+		}
+	}
+	return true
+}
