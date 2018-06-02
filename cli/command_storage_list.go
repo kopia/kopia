02c5e9067135e356fe4cf90a416c8d6c8bab2b5b
Jarek Kowalski <jaak@jkowalski.net>
2018-06-02 10:46:59 -0700

    added new cli flags to 'storage list'

--- cli/command_storage_list.go
+++ cli/command_storage_list.go
@@ -10,6 +10,8 @@ import (
 var (
 	storageListCommand = storageCommands.Command("list", "List storage blocks").Alias("ls")
 	storageListPrefix  = storageListCommand.Flag("prefix", "Block prefix").String()
+	storageListMinSize = storageListCommand.Flag("min-size", "Minimum size").Int64()
+	storageListMaxSize = storageListCommand.Flag("max-size", "Maximum size").Int64()
 )
 
 func runListStorageBlocks(ctx context.Context, rep *repo.Repository) error {
@@ -20,6 +22,14 @@ func runListStorageBlocks(ctx context.Context, rep *repo.Repository) error {
 	for b := range ch {
 		if b.Error != nil {
 			return b.Error
+		}
+
+		if *storageListMaxSize != 0 && b.Length > *storageListMaxSize {
+			continue
+		}
+
+		if *storageListMinSize != 0 && b.Length < *storageListMinSize {
+			continue
 		}
 
 		fmt.Printf("%-70v %10v %v\n", b.BlockID, b.Length, b.TimeStamp.Local().Format(timeFormat))
