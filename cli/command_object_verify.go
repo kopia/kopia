6838d820c8d095e4d840be8ffb8561c933024081
Jarek Kowalski <jaak@jkowalski.net>
2018-06-09 21:25:28 -0700

    revamped object verification to be parallelized, added option to randomly verify a percentage of object contents

--- cli/command_object_verify.go
+++ cli/command_object_verify.go
@@ -3,13 +3,19 @@ package cli
 import (
 	"context"
 	"fmt"
-
-	"github.com/rs/zerolog/log"
+	"io"
+	"io/ioutil"
+	"os"
+	"sync"
+	"time"
 
 	"github.com/kopia/kopia/fs"
+	"github.com/kopia/kopia/internal/parallelwork"
 	"github.com/kopia/kopia/object"
 	"github.com/kopia/kopia/repo"
 	"github.com/kopia/kopia/snapshot"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/exp/rand"
 )
 
 var (
@@ -19,16 +25,42 @@ var (
 	verifyCommandFileObjectIDs  = verifyCommand.Flag("file-id", "File object IDs to verify").Strings()
 	verifyCommandAllSources     = verifyCommand.Flag("all-sources", "Verify all snapshots").Bool()
 	verifyCommandSources        = verifyCommand.Flag("sources", "Verify the provided sources").Strings()
+	verifyCommandParallel       = verifyCommand.Flag("parallel", "Parallelization").Default("16").Int()
+	verifyCommandFilesPercent   = verifyCommand.Flag("verify-files-percent", "Randomly verify a percentage of files").Default("0").Int()
 )
 
 type verifier struct {
-	mgr     *snapshot.Manager
-	om      *object.Manager
-	visited map[string]bool
-	errors  []error
+	mgr       *snapshot.Manager
+	om        *object.Manager
+	workQueue *parallelwork.Queue
+	startTime time.Time
+
+	mu   sync.Mutex
+	seen map[object.ID]bool
+
+	errors []error
+}
+
+func (v *verifier) progressCallback(enqueued, active, completed int64) {
+	elapsed := time.Since(v.startTime)
+	maybeTimeRemaining := ""
+	if elapsed > 1*time.Second && enqueued > 0 && completed > 0 {
+		completedRatio := float64(completed) / float64(enqueued)
+		predictedSeconds := elapsed.Seconds() / completedRatio
+		predictedEndTime := v.startTime.Add(time.Duration(predictedSeconds) * time.Second)
+
+		dt := time.Until(predictedEndTime)
+		if dt > 0 {
+			maybeTimeRemaining = fmt.Sprintf(" remaining %v (ETA %v)", dt.Truncate(1*time.Second), predictedEndTime.Truncate(1*time.Second).Format(timeFormat))
+		}
+	}
+	fmt.Fprintf(os.Stderr, "Found %v objects, verifying %v, completed %v objects%v.\n", enqueued, active, completed, maybeTimeRemaining)
 }
 
 func (v *verifier) tooManyErrors() bool {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
 	if *verifyCommandErrorThreshold == 0 {
 		return false
 	}
@@ -37,17 +69,47 @@ func (v *verifier) tooManyErrors() bool {
 }
 
 func (v *verifier) reportError(path string, err error) bool {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
 	log.Warn().Str("path", path).Err(err).Msg("failed")
 	v.errors = append(v.errors, err)
 	return len(v.errors) >= *verifyCommandErrorThreshold
 }
 
-func (v *verifier) verifyDirectory(ctx context.Context, oid object.ID, path string) {
-	if v.visited[oid.String()] {
+func (v *verifier) shouldEnqueue(oid object.ID) bool {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	if v.seen[oid] {
+		return false
+	}
+
+	v.seen[oid] = true
+	return true
+}
+
+func (v *verifier) enqueueVerifyDirectory(ctx context.Context, oid object.ID, path string) {
+	// push to the front of the queue, so that we quickly discover all directories to get reliable ETA.
+	if !v.shouldEnqueue(oid) {
 		return
 	}
-	v.visited[oid.String()] = true
+	v.workQueue.EnqueueFront(func() {
+		v.doVerifyDirectory(ctx, oid, path)
+	})
+}
 
+func (v *verifier) enqueueVerifyObject(ctx context.Context, oid object.ID, path string, expectedLength int64) {
+	// push to the back of the queue, so that we process non-directories at the end.
+	if !v.shouldEnqueue(oid) {
+		return
+	}
+	v.workQueue.EnqueueBack(func() {
+		v.doVerifyObject(ctx, oid, path, expectedLength)
+	})
+}
+
+func (v *verifier) doVerifyDirectory(ctx context.Context, oid object.ID, path string) {
 	log.Printf("verifying directory %q (%v)", path, oid)
 
 	d := v.mgr.DirectoryEntry(oid, nil)
@@ -66,26 +128,38 @@ func (v *verifier) verifyDirectory(ctx context.Context, oid object.ID, path stri
 		objectID := e.(object.HasObjectID).ObjectID()
 		childPath := path + "/" + m.Name
 		if m.FileMode().IsDir() {
-			v.verifyDirectory(ctx, objectID, childPath)
+			v.enqueueVerifyDirectory(ctx, objectID, childPath)
 		} else {
-			v.verifyObject(ctx, objectID, childPath, m.FileSize)
+			v.enqueueVerifyObject(ctx, objectID, childPath, m.FileSize)
 		}
 	}
 }
 
-func (v *verifier) verifyObject(ctx context.Context, oid object.ID, path string, expectedLength int64) {
-	if v.visited[oid.String()] {
-		return
-	}
-	v.visited[oid.String()] = true
-
+func (v *verifier) doVerifyObject(ctx context.Context, oid object.ID, path string, expectedLength int64) {
+	readFile := rand.Intn(100) < *verifyCommandFilesPercent
 	if expectedLength < 0 {
-		log.Printf("verifying object %v", oid)
+		log.Printf("verifying object %v (readFile=%v)", oid, readFile)
 	} else {
-		log.Printf("verifying object %v (%v) with length %v", path, oid, expectedLength)
+		log.Printf("verifying object %v (%v) with length %v (readFile=%v)", path, oid, expectedLength, readFile)
 	}
 
-	length, _, err := v.om.VerifyObject(ctx, oid)
+	var length int64
+	var err error
+
+	if readFile {
+		var r object.Reader
+		r, err = v.om.Open(ctx, oid)
+		if err != nil {
+			v.reportError(path, fmt.Errorf("error verifying %v: %v", oid, err))
+			return
+		}
+		defer r.Close() //nolint:errcheck
+
+		length, err = io.Copy(ioutil.Discard, r)
+	} else {
+		length, _, err = v.om.VerifyObject(ctx, oid)
+	}
+
 	if err != nil {
 		v.reportError(path, fmt.Errorf("error verifying %v: %v", oid, err))
 	}
@@ -98,39 +172,44 @@ func (v *verifier) verifyObject(ctx context.Context, oid object.ID, path string,
 func runVerifyCommand(ctx context.Context, rep *repo.Repository) error {
 	mgr := snapshot.NewManager(rep)
 
-	v := verifier{
-		mgr,
-		rep.Objects,
-		make(map[string]bool),
-		nil,
+	v := &verifier{
+		mgr:       mgr,
+		om:        rep.Objects,
+		startTime: time.Now(),
+		workQueue: parallelwork.NewQueue(),
+		seen:      map[object.ID]bool{},
 	}
 
-	if *verifyCommandAllSources || len(*verifyCommandSources) > 0 {
-		var manifestIDs []string
-		if *verifyCommandAllSources {
-			manifestIDs = append(manifestIDs, mgr.ListSnapshotManifests(nil)...)
+	if err := enqueueRootsToVerify(ctx, v, mgr); err != nil {
+		return err
+	}
+
+	v.workQueue.ProgressCallback = v.progressCallback
+	v.workQueue.Process(*verifyCommandParallel)
+
+	if len(v.errors) == 0 {
+		return nil
+	}
+
+	return fmt.Errorf("encountered %v errors", len(v.errors))
+}
+
+func enqueueRootsToVerify(ctx context.Context, v *verifier, mgr *snapshot.Manager) error {
+	manifests, err := loadSourceManifests(mgr, *verifyCommandAllSources, *verifyCommandSources)
+	if err != nil {
+		return err
+	}
+
+	for _, man := range manifests {
+		path := fmt.Sprintf("%v@%v", man.Source, man.StartTime.Format(timeFormat))
+		if man.RootEntry == nil {
+			continue
+		}
+
+		if man.RootEntry.Type == fs.EntryTypeDirectory {
+			v.enqueueVerifyDirectory(ctx, man.RootObjectID(), path)
 		} else {
-			for _, srcStr := range *verifyCommandSources {
-				src, err := snapshot.ParseSourceInfo(srcStr, getHostName(), getUserName())
-				if err != nil {
-					return fmt.Errorf("error parsing %q: %v", srcStr, err)
-				}
-				manifestIDs = append(manifestIDs, mgr.ListSnapshotManifests(&src)...)
-			}
-		}
-		manifests, err := mgr.LoadSnapshots(manifestIDs)
-		if err != nil {
-			return err
-		}
-		for _, man := range manifests {
-			path := fmt.Sprintf("%v@%v", man.Source, man.StartTime.Format(timeFormat))
-			if man.RootEntry != nil {
-				if man.RootEntry.Type == fs.EntryTypeDirectory {
-					v.verifyDirectory(ctx, man.RootObjectID(), path)
-				} else {
-					v.verifyObject(ctx, man.RootObjectID(), path, -1)
-				}
-			}
+			v.enqueueVerifyObject(ctx, man.RootObjectID(), path, -1)
 		}
 	}
 
@@ -140,7 +219,7 @@ func runVerifyCommand(ctx context.Context, rep *repo.Repository) error {
 			return err
 		}
 
-		v.verifyDirectory(ctx, oid, oidStr)
+		v.enqueueVerifyDirectory(ctx, oid, oidStr)
 	}
 
 	for _, oidStr := range *verifyCommandFileObjectIDs {
@@ -149,14 +228,26 @@ func runVerifyCommand(ctx context.Context, rep *repo.Repository) error {
 			return err
 		}
 
-		v.verifyObject(ctx, oid, oidStr, -1)
+		v.enqueueVerifyObject(ctx, oid, oidStr, -1)
 	}
 
-	if len(v.errors) == 0 {
-		return nil
-	}
+	return nil
+}
 
-	return fmt.Errorf("encountered %v errors", len(v.errors))
+func loadSourceManifests(mgr *snapshot.Manager, all bool, sources []string) ([]*snapshot.Manifest, error) {
+	var manifestIDs []string
+	if *verifyCommandAllSources {
+		manifestIDs = append(manifestIDs, mgr.ListSnapshotManifests(nil)...)
+	} else {
+		for _, srcStr := range *verifyCommandSources {
+			src, err := snapshot.ParseSourceInfo(srcStr, getHostName(), getUserName())
+			if err != nil {
+				return nil, fmt.Errorf("error parsing %q: %v", srcStr, err)
+			}
+			manifestIDs = append(manifestIDs, mgr.ListSnapshotManifests(&src)...)
+		}
+	}
+	return mgr.LoadSnapshots(manifestIDs)
 }
 
 func init() {
---
+++ internal/parallelwork/parallel_work_queue.go
@@ -0,0 +1,127 @@
+package parallelwork
+
+import (
+	"container/list"
+	"sync"
+	"time"
+)
+
+// Queue represents a work queue with multiple parallel workers.
+type Queue struct {
+	monitor *sync.Cond
+
+	queueItems        *list.List
+	enqueuedWork      int64
+	activeWorkerCount int64
+	completedWork     int64
+
+	nextReportTime time.Time
+
+	ProgressCallback func(enqueued, active, completed int64)
+}
+
+// EnqueueFront adds the work to the front of the queue.
+func (v *Queue) EnqueueFront(callback func()) {
+	v.enqueue(true, callback)
+}
+
+// EnqueueBack adds the work to the back of the queue.
+func (v *Queue) EnqueueBack(callback func()) {
+	v.enqueue(false, callback)
+}
+
+func (v *Queue) enqueue(front bool, callback func()) {
+	v.monitor.L.Lock()
+	defer v.monitor.L.Unlock()
+
+	v.enqueuedWork++
+
+	// add to the queue and signal one reader
+	if front {
+		v.queueItems.PushFront(callback)
+	} else {
+		v.queueItems.PushBack(callback)
+	}
+	v.maybeReportProgress()
+	v.monitor.Signal()
+}
+
+// Process starts N workers, which will be processing elements in the queue until the queue
+// is empty and all workers are idle.
+func (v *Queue) Process(workers int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+
+		go func(workerID int) {
+			defer wg.Done()
+
+			for {
+				callback := v.dequeue()
+				if callback == nil {
+					break
+				}
+				callback()
+				v.completed()
+			}
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func (v *Queue) dequeue() func() {
+	v.monitor.L.Lock()
+	defer v.monitor.L.Unlock()
+
+	for v.queueItems.Len() == 0 && v.activeWorkerCount > 0 {
+		// no items in queue, but some workers are active, they may add more.
+		v.monitor.Wait()
+	}
+
+	// no items in queue, no workers are active, no more work.
+	if v.queueItems.Len() == 0 {
+		return nil
+	}
+
+	v.activeWorkerCount++
+	v.maybeReportProgress()
+
+	front := v.queueItems.Front()
+	v.queueItems.Remove(front)
+	return front.Value.(func())
+}
+
+func (v *Queue) completed() {
+	v.monitor.L.Lock()
+	defer v.monitor.L.Unlock()
+
+	v.activeWorkerCount--
+	v.completedWork++
+	v.maybeReportProgress()
+
+	v.monitor.Broadcast()
+}
+
+func (v *Queue) maybeReportProgress() {
+	cb := v.ProgressCallback
+	if cb == nil {
+		return
+	}
+
+	if time.Now().Before(v.nextReportTime) {
+		return
+	}
+	v.nextReportTime = time.Now().Add(1 * time.Second)
+
+	cb(v.enqueuedWork, v.activeWorkerCount, v.completedWork)
+}
+
+// NewQueue returns new parallel work queue.
+func NewQueue() *Queue {
+	return &Queue{
+		queueItems: list.New(),
+		monitor:    sync.NewCond(&sync.Mutex{}),
+	}
+}
