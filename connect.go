7d8f4a469564179aa88b49084c8cdcb386777191
Jarek Kowalski <jaak@jkowalski.net>
2018-10-26 20:35:39 -0700

    repo: fixed default cache dir

--- connect.go
+++ connect.go
@@ -75,7 +75,7 @@ func setupCaching(configPath string, lc *LocalConfig, opt block.CachingOptions, 
 		h := sha256.New()
 		h.Write(uniqueID)
 		h.Write([]byte(configPath))
-		lc.Caching.CacheDirectory = filepath.Join(cacheDir, hex.EncodeToString(h.Sum(nil))[0:16])
+		lc.Caching.CacheDirectory = filepath.Join(cacheDir, "kopia", hex.EncodeToString(h.Sum(nil))[0:16])
 	} else {
 		absCacheDir, err := filepath.Abs(opt.CacheDirectory)
 		if err != nil {
