4eea68077c447c47a9aaaee6758fb060af4b63ff
Jarek Kowalski <jaak@jkowalski.net>
2018-10-28 20:26:13 -0700

    example: moved example from kopia/kopia/examples to this repository

---
+++ examples/upload_download/main.go
@@ -0,0 +1,38 @@
+// Command repository_api demonstrates the use of Kopia's Repository API.
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/kopia/repo"
+)
+
+func main() {
+	ctx := context.Background()
+
+	if err := setupRepositoryAndConnect(ctx, masterPassword); err != nil {
+		log.Printf("unable to set up repository: %v", err)
+		os.Exit(1)
+	}
+
+	r, err := repo.Open(ctx, configFile, masterPassword, nil)
+	if err != nil {
+		log.Printf("unable to open repository: %v", err)
+		os.Exit(1)
+	}
+	defer r.Close(ctx) //nolint:errcheck
+
+	uploadAndDownloadObjects(ctx, r)
+
+	// Now list blocks found in the repository.
+	blks, err := r.Blocks.ListBlocks("")
+	if err != nil {
+		log.Printf("err: %v", err)
+	}
+
+	for _, b := range blks {
+		log.Printf("found block %v", b)
+	}
+}
