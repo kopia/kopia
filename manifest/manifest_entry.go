e5af47227c58b7a012ac0b81e590eacd5a8806a4
Jarek Kowalski <jaak@jkowalski.net>
2018-12-24 10:35:02 -0800

    manifest: removed manifest.EntryIDs

--- manifest/manifest_entry.go
+++ manifest/manifest_entry.go
@@ -10,12 +10,3 @@ type EntryMetadata struct {
 	Labels  map[string]string
 	ModTime time.Time
 }
-
-// EntryIDs returns the list of IDs for the provided list of EntryMetadata.
-func EntryIDs(entries []*EntryMetadata) []string {
-	var ids []string
-	for _, e := range entries {
-		ids = append(ids, e.ID)
-	}
-	return ids
-}
