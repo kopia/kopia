kopia/kopia

commit 3e903b4fc58e5a9da4522f38a902f00fd5a0e4d0

content: added missing locks to Refresh() and CompactIndexes()

repo/content/content_manager.go
@@ -494,6 +494,9 @@ func (bm *Manager) assertLocked() {
 
 // Refresh reloads the committed content indexes.
 func (bm *Manager) Refresh(ctx context.Context) (bool, error) {
+	bm.lock()
+	defer bm.unlock()
+
 	log.Debugf("Refresh started")
 	t0 := time.Now()
 	_, updated, err := bm.loadPackIndexesUnlocked(ctx)

repo/content/block_manager_compaction.go
@@ -23,6 +23,9 @@ type CompactOptions struct {
 
 // CompactIndexes performs compaction of index blobs ensuring that # of small contents is between minSmallContentCount and maxSmallContentCount
 func (bm *Manager) CompactIndexes(ctx context.Context, opt CompactOptions) error {
+	bm.lock()
+	defer bm.unlock()
+
 	log.Debugf("CompactIndexes(%+v)", opt)
 	if opt.MaxSmallBlobs < opt.MinSmallBlobs {
 		return errors.Errorf("invalid content counts")

commit cbc0b7c86da94b7c21974668295143c37763b9cb

content: fixed content.Manager.Flush()
Previously, it was possible for Flush() to miss in-flight writes,
but only when using repository manually since Uploader guarantees
there are no in-flight writes when it completes.

With this change Flush() will guarantee that any pending writes
completed before Flush() has started are guaranteed to be committed
to the repository before Flush() returns.

This was actually a regression introduced in #105.
Added regression test to prevent it from reoccurring.

internal/blobtesting/faulty.go
@@ -102,6 +102,7 @@ func (s *FaultyStorage) getNextFault(method string, args ...interface{}) error {
 	}
 	if f.Sleep > 0 {
 		log.Debugf("sleeping for %v in %v %v", f.Sleep, method, args)
+		time.Sleep(f.Sleep)
 	}
 	if f.ErrCallback != nil {
 		err := f.ErrCallback()

repo/content/content_manager.go
@@ -64,8 +64,9 @@ type IndexBlobInfo struct {
 
 // Manager builds content-addressable storage with encryption, deduplication and packaging on top of BLOB store.
 type Manager struct {
-	mu     sync.RWMutex
-	locked bool
+	mu       *sync.RWMutex
+	cond     *sync.Cond
+	flushing bool
 
 	pendingPacks     map[blob.ID]*pendingPackInfo
 	writingPacks     []*pendingPackInfo // list of packs that are being written
@@ -144,6 +145,12 @@ func (bm *Manager) addToPackUnlocked(ctx context.Context, contentID ID, data []b
 
 	data = cloneBytes(data)
 	bm.lock()
+
+	// do not start new uploads while flushing
+	for bm.flushing {
+		bm.cond.Wait()
+	}
+
 	if bm.timeNow().After(bm.flushPackIndexesAfter) {
 		if err := bm.flushPackIndexesLocked(ctx); err != nil {
 			return err
@@ -207,8 +214,6 @@ func (bm *Manager) EnableIndexFlush() {
 }
 
 func (bm *Manager) verifyInvariantsLocked() {
-	bm.assertLocked()
-
 	bm.verifyCurrentPackItemsLocked()
 	bm.verifyPackIndexBuilderLocked()
 }
@@ -249,8 +254,6 @@ func (bm *Manager) assertInvariant(ok bool, errorMsg string, arg ...interface{})
 }
 
 func (bm *Manager) flushPackIndexesLocked(ctx context.Context) error {
-	bm.assertLocked()
-
 	if bm.disableIndexFlushCount > 0 {
 		log.Debugf("not flushing index because flushes are currently disabled")
 		return nil
@@ -324,6 +327,8 @@ func (bm *Manager) writePackAndAddToIndex(ctx context.Context, pp *pendingPackIn
 	}
 
 	if !lockHeld {
+		// we changed the in-flight packs, notify all waiting on the condition variable
+		bm.cond.Broadcast()
 		bm.unlock()
 	}
 	return nil
@@ -340,17 +345,36 @@ func removePendingPack(slice []*pendingPackInfo, pp *pendingPackInfo) []*pending
 }
 
 // Close closes the content manager.
-func (bm *Manager) Close() {
+func (bm *Manager) Close(ctx context.Context) error {
+	if err := bm.Flush(ctx); err != nil {
+		return errors.Wrap(err, "error flushing")
+	}
 	bm.contentCache.close()
 	bm.metadataCache.close()
 	close(bm.closed)
+	return nil
 }
 
-// Flush completes writing any pending packs and writes pack indexes to the underlyign storage.
+// Flush completes writing any pending packs and writes pack indexes to the underlying storage.
+// Any pending writes completed before Flush() has started are guaranteed to be committed to the
+// repository before Flush() returns.
 func (bm *Manager) Flush(ctx context.Context) error {
 	bm.lock()
 	defer bm.unlock()
 
+	bm.flushing = true
+	defer func() {
+		bm.flushing = false
+	}()
+
+	for len(bm.writingPacks) > 0 {
+		log.Infof("waiting for %v in-progress packs to finish", len(bm.writingPacks))
+
+		// wait packs that are currently writing in other goroutines to finish
+		bm.cond.Wait()
+	}
+
+	// finish all new pending packs
 	if err := bm.finishAllPacksLocked(ctx); err != nil {
 		return errors.Wrap(err, "error writing pending content")
 	}
@@ -358,6 +382,7 @@ func (bm *Manager) Flush(ctx context.Context) error {
 	if err := bm.flushPackIndexesLocked(ctx); err != nil {
 		return errors.Wrap(err, "error flushing indexes")
 	}
+
 	return nil
 }
 
@@ -474,7 +499,6 @@ func (bm *Manager) ContentInfo(ctx context.Context, contentID ID) (Info, error) 
 
 func (bm *Manager) lock() {
 	bm.mu.Lock()
-	bm.locked = true
 }
 
 func (bm *Manager) unlock() {
@@ -482,14 +506,7 @@ func (bm *Manager) unlock() {
 		bm.verifyInvariantsLocked()
 	}
 
-	bm.locked = false
 	bm.mu.Unlock()
-}
-
-func (bm *Manager) assertLocked() {
-	if !bm.locked {
-		panic("must be locked")
-	}
 }
 
 // Refresh reloads the committed content indexes.
@@ -545,6 +562,7 @@ func newManagerWithOptions(ctx context.Context, st blob.Storage, f *FormattingOp
 
 	contentIndex := newCommittedContentIndex(caching)
 
+	mu := &sync.RWMutex{}
 	m := &Manager{
 		lockFreeManager: lockFreeManager{
 			Format:                  *f,
@@ -565,6 +583,9 @@ func newManagerWithOptions(ctx context.Context, st blob.Storage, f *FormattingOp
 			writeFormatVersion:      int32(f.Version),
 			committedContents:       contentIndex,
 		},
+
+		mu:   mu,
+		cond: sync.NewCond(mu),
 
 		flushPackIndexesAfter: timeNow().Add(flushPackIndexTimeout),
 		pendingPacks:          map[blob.ID]*pendingPackInfo{},

repo/repository.go
@@ -31,7 +31,7 @@ func (r *Repository) Close(ctx context.Context) error {
 	if err := r.Manifests.Flush(ctx); err != nil {
 		return errors.Wrap(err, "error flushing manifests")
 	}
-	if err := r.Content.Flush(ctx); err != nil {
+	if err := r.Content.Close(ctx); err != nil {
 		return errors.Wrap(err, "error closing content-addressable storage manager")
 	}
 	if err := r.Blobs.Close(ctx); err != nil {

repo/content/content_manager_test.go
@@ -414,26 +414,107 @@ func TestDeleteContent(t *testing.T) {
 func TestParallelWrites(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
+
 	data := blobtesting.DataMap{}
 	keyTime := map[blob.ID]time.Time{}
-	bm := newTestContentManager(data, keyTime, nil)
+	st := blobtesting.NewMapStorage(data, keyTime, nil)
 
-	workers := 8
-	writesPerWorker := 1000
-	var wg sync.WaitGroup
-	for workerID := 0; workerID < workers; workerID++ {
+	// set up fake storage that is slow at PutBlob causing writes to be piling up
+	fs := &blobtesting.FaultyStorage{
+		Base: st,
+		Faults: map[string][]*blobtesting.Fault{
+			"PutBlob": {
+				{
+					Repeat: 1000000000,
+					Sleep:  1 * time.Second,
+				},
+			},
+		},
+	}
+	bm := newTestContentManagerWithStorage(fs, nil)
+
+	numWorkers := 8
+
+	var workersWG sync.WaitGroup
+	closeWorkers := make(chan bool)
+
+	// workerLock allows workers to append to their own list of IDs (when R-locked) in parallel.
+	// W-lock allows flusher to capture the state without any worker being able to modify it.
+	var workerLock sync.RWMutex
+	workerWritten := make([][]ID, numWorkers)
+
+	// start numWorkers, each writing random block and recording it
+	for workerID := 0; workerID < numWorkers; workerID++ {
 		workerID := workerID
-		wg.Add(1)
+		workersWG.Add(1)
 		go func() {
-			defer wg.Done()
-			for i := 0; i < writesPerWorker; i++ {
-				writeContentAndVerify(ctx, t, bm, seededRandomData(writesPerWorker*workerID+i, 100))
+			defer workersWG.Done()
+			for {
+				select {
+				case <-closeWorkers:
+					return
+				case <-time.After(1 * time.Nanosecond):
+					id := writeContentAndVerify(ctx, t, bm, seededRandomData(rand.Int(), 100)) //nolint:gosec
+					workerLock.RLock()
+					workerWritten[workerID] = append(workerWritten[workerID], id)
+					workerLock.RUnlock()
+				}
 			}
 		}()
 	}
-	wg.Wait()
-	if err := bm.Flush(ctx); err != nil {
-		t.Fatalf("flush error: %v", err)
+
+	closeFlusher := make(chan bool)
+	var flusherWG sync.WaitGroup
+	flusherWG.Add(1)
+	go func() {
+		defer flusherWG.Done()
+		for {
+			select {
+			case <-closeFlusher:
+				log.Infof("closing flusher goroutine")
+				return
+			case <-time.After(2 * time.Second):
+				log.Infof("about to flush")
+
+				// capture snapshot of all content IDs while holding a writer lock
+				allWritten := map[ID]bool{}
+				workerLock.Lock()
+				for _, ww := range workerWritten {
+					for _, id := range ww {
+						allWritten[id] = true
+					}
+				}
+				workerLock.Unlock()
+				log.Infof("captured %v contents", len(allWritten))
+				if err := bm.Flush(ctx); err != nil {
+					t.Errorf("flush error: %v", err)
+				}
+
+				// open new content manager and verify all contents are visible there.
+				verifyAllDataPresent(t, data, allWritten)
+			}
+		}
+	}()
+
+	// run workers and flushers for some time, enough for 2 flushes to complete
+	time.Sleep(5 * time.Second)
+
+	// shut down workers and wait for them
+	close(closeWorkers)
+	workersWG.Wait()
+
+	close(closeFlusher)
+	flusherWG.Wait()
+}
+
+func verifyAllDataPresent(t *testing.T, data map[blob.ID][]byte, contentIDs map[ID]bool) {
+	bm := newTestContentManager(data, nil, nil)
+	_ = bm.IterateContents(IterateOptions{}, func(ci Info) error {
+		delete(contentIDs, ci.ID)
+		return nil
+	})
+	if len(contentIDs) != 0 {
+		t.Errorf("some blocks not written: %v", contentIDs)
 	}
 }
 
@@ -959,10 +1040,14 @@ func verifyContentManagerDataSet(ctx context.Context, t *testing.T, mgr *Manager
 }
 
 func newTestContentManager(data blobtesting.DataMap, keyTime map[blob.ID]time.Time, timeFunc func() time.Time) *Manager {
+	st := blobtesting.NewMapStorage(data, keyTime, timeFunc)
+	return newTestContentManagerWithStorage(st, timeFunc)
+}
+
+func newTestContentManagerWithStorage(st blob.Storage, timeFunc func() time.Time) *Manager {
 	if timeFunc == nil {
 		timeFunc = fakeTimeNowWithAutoAdvance(fakeTime, 1*time.Second)
 	}
-	st := blobtesting.NewMapStorage(data, keyTime, timeFunc)
 	bm, err := newManagerWithOptions(context.Background(), st, &FormattingOptions{
 		Hash:        "HMAC-SHA256",
 		Encryption:  "NONE",

tests/repository_stress_test/repository_stress_test.go
@@ -173,7 +173,6 @@ func repositoryTest(ctx context.Context, t *testing.T, cancel chan struct{}, rep
 	for {
 		select {
 		case <-cancel:
-			rep.Close(ctx)
 			return
 		default:
 		}