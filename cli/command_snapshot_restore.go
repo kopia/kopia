1279a5f3aad1a5987e7c8305630d916af05efdcd
Nick <nick@kasten.io>
2019-12-12 18:01:22 -0800

    Snapshot restore command
    Snapshot restore will take a snapshot ID and restore the
    associated snapshot to the target path.
    - Looks up the manifest with the snapshot ID
    - Gets the snapshot root entry
    - Copies the snapshot from the root entry to the target path
    
    Because it uses the parent manifest with the copied permissions,
    the restored directory will have the permissions of the original
    source directory.

---
+++ cli/command_snapshot_restore.go
@@ -0,0 +1,23 @@
+package cli
+
+import (
+	"context"
+
+	"github.com/kopia/kopia/repo"
+	"github.com/kopia/kopia/repo/manifest"
+	"github.com/kopia/kopia/snapshot/snapshotfs"
+)
+
+var (
+	snapshotRestoreCommand    = snapshotCommands.Command("restore", "Restore a snapshot from the snapshot ID to the given target path")
+	snapshotRestoreSnapID     = snapshotRestoreCommand.Arg("id", "Snapshot ID to be restored").Required().String()
+	snapshotRestoreTargetPath = snapshotRestoreCommand.Arg("target-path", "Path of the directory for the contents to be restored").Required().String()
+)
+
+func runSnapRestoreCommand(ctx context.Context, rep *repo.Repository) error {
+	return snapshotfs.Restore(ctx, rep, *snapshotRestoreTargetPath, manifest.ID(*snapshotRestoreSnapID))
+}
+
+func init() {
+	snapshotRestoreCommand.Action(repositoryAction(runSnapRestoreCommand))
+}
