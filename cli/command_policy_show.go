d648707173fffd54734b7951121b44fb7490fe28
Nick Wright <nick@kasten.io>
2020-02-10 20:12:08 -0800

    Correct print output file vs dir

--- cli/command_policy_show.go
+++ cli/command_policy_show.go
@@ -161,13 +161,13 @@ func printErrorHandlingPolicy(p *policy.Policy, parents []*policy.Policy) {
 	printStdout("Error handling policy:\n")
 
 	printStdout("  Ignore file read errors:       %5v       %v\n",
-		p.ErrorHandlingPolicy.IgnoreDirectoryErrorsOrDefault(false),
+		p.ErrorHandlingPolicy.IgnoreFileErrorsOrDefault(false),
 		getDefinitionPoint(parents, func(pol *policy.Policy) bool {
 			return pol.ErrorHandlingPolicy.IgnoreFileErrors != nil
 		}))
 
 	printStdout("  Ignore directory read errors:  %5v       %v\n",
-		p.ErrorHandlingPolicy.IgnoreFileErrorsOrDefault(false),
+		p.ErrorHandlingPolicy.IgnoreDirectoryErrorsOrDefault(false),
 		getDefinitionPoint(parents, func(pol *policy.Policy) bool {
 			return pol.ErrorHandlingPolicy.IgnoreDirectoryErrors != nil
 		}))
