03dad366e1d2c9d03dec6baa16e0cdfc09cd0c7f
Jarek Kowalski <jaak@jkowalski.net>
2020-02-16 15:55:05 -0800

    htmlui: added first version of UI policy editor

--- htmlui/src/forms.js
+++ htmlui/src/forms.js
@@ -24,32 +24,181 @@ export function validateRequiredFields(component, fields) {
     return true;
 }
 
-export function handleChange(event) {
-    this.setState({
-        [event.target.name]: event.target.value,
-    });
+export function handleChange(event, valueGetter=x=>x.value) {
+    let newState = { ...this.state };
+    let st = newState;
+
+    const parts = event.target.name.split(/\./);
+
+    for (let i = 0; i < parts.length - 1; i++) {
+        const part = parts[i];
+
+        if (st[part] === undefined) {
+            st[part] = {}
+        }
+
+        st = st[part]
+    }
+
+    const part = parts[parts.length - 1]
+    const v = valueGetter(event.target);
+    st[part] = v;
+
+    this.setState(newState);
 }
 
-export function RequiredField(component, label, name, props={}) {
+export function stateProperty(component, name) {
+    let st = component.state;
+    const parts = name.split(/\./);
+
+    for (let i = 0; i < parts.length; i++) {
+        const part = parts[i];
+        if (st === undefined) {
+            return undefined;
+        }
+
+        st = st[part];
+    }
+
+    return st;
+}
+
+export function RequiredField(component, label, name, props = {}, helpText = null) {
     return <Form.Group as={Col}>
         <Form.Label className="required">{label}</Form.Label>
         <Form.Control
-            isInvalid={component.state[name] === ''}
+            isInvalid={stateProperty(component, name) === ''}
             name={name}
-            value={component.state[name]}
-            onChange={component.handleChange} 
+            value={stateProperty(component, name)}
+            onChange={component.handleChange}
             {...props} />
+        {helpText && <Form.Text className="text-muted">{helpText}</Form.Text>}
         <Form.Control.Feedback type="invalid">{label} Is Required</Form.Control.Feedback>
     </Form.Group>
 }
 
-export function OptionalField(component, label, name, props={}) {
+export function OptionalField(component, label, name, props = {}, helpText = null) {
     return <Form.Group as={Col}>
         <Form.Label>{label}</Form.Label>
         <Form.Control
             name={name}
-            value={component.state[name]}
-            onChange={component.handleChange} 
+            value={stateProperty(component, name)}
+            onChange={component.handleChange}
             {...props} />
+        {helpText && <Form.Text className="text-muted">{helpText}</Form.Text>}
+    </Form.Group>
+}
+
+function valueToNumber(t) {
+    if (t.value === "") {
+        return undefined;
+    }
+
+    const v = Number.parseInt(t.value);
+    if (isNaN(v)) {
+        return t.value + '';
+    }
+
+    return v;
+}
+
+function isInvalidNumber(v) {
+    if (v === undefined) {
+        return false
+    }
+
+    if (isNaN(Number.parseInt(v))) {
+        return true;
+    }
+
+    return false;
+}
+
+export function OptionalNumberField(component, label, name, props = {}) {
+    return <Form.Group as={Col}>
+        <Form.Label>{label}</Form.Label>
+        <Form.Control
+            name={name}
+            isInvalid={isInvalidNumber(stateProperty(component, name))}
+            value={stateProperty(component, name)}
+            onChange={e => component.handleChange(e, valueToNumber)}
+            {...props} />
+        <Form.Control.Feedback type="invalid">Must be a valid number or empty</Form.Control.Feedback>
+    </Form.Group>
+}
+
+function checkedToBool(t) {
+    if (t.checked) {
+        return true;
+    }
+
+    return undefined;
+}
+
+export function RequiredBoolean(component, label, name, defaultLabel) {
+    return <Form.Group as={Col}>
+        <Form.Check
+            label={label}
+            name={name}
+            checked={stateProperty(component, name)}
+            onChange={e => component.handleChange(e, checkedToBool)}
+            type="checkbox" />
+    </Form.Group>
+}
+
+function optionalBooleanValue(target) {
+    if (target.value === "true") {
+        return true;
+    }
+    if (target.value === "false") {
+        return false;
+    }
+
+    return undefined;
+}
+
+export function OptionalBoolean(component, label, name, defaultLabel) {
+    return <Form.Group as={Col}>
+        <Form.Label>{label}</Form.Label>
+        <Form.Control
+            name={name}
+            value={stateProperty(component, name)}
+            onChange={e => component.handleChange(e, optionalBooleanValue)}
+            as="select">
+            <option value="">{defaultLabel}</option>
+            <option value="true">yes</option>
+            <option value="false">no</option>
+        </Form.Control>
+    </Form.Group>
+}
+
+function listToMultilineString(v) {
+    if (v) {
+        return v.join("\n");
+    }
+
+    return "";
+}
+
+function multilineStringToList(target) {
+    const v = target.value;
+    if (v === "") {
+        return undefined;
+    }
+
+    return v.split(/\n/);
+}
+
+export function StringList(component, label, name, helpText) {
+    return <Form.Group as={Col}>
+        <Form.Label>{label}</Form.Label>
+        <Form.Control
+            name={name}
+            value={listToMultilineString(stateProperty(component, name))}
+            onChange={e => component.handleChange(e, multilineStringToList)}
+            as="textarea"
+            rows="5">
+        </Form.Control>
+        <Form.Text className="text-muted">{helpText}</Form.Text>
     </Form.Group>
 }
