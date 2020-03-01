4c057ea0f018b631cbcd05da2d058d72b143d509
Jarek Kowalski <jaak@jkowalski.net>
2020-03-01 14:14:30 -0800

    sftp: Fixed issues in SFTP provider, Fixes #216
    - did not work on windows due to use of filepath which uses backslash
      instead of slash
    - added support for embedding SFTP key
    - fixed UI controls
    - misc fixes for KopiaUI
    - added progress reporting

--- htmlui/src/SetupSFTP.js
+++ htmlui/src/SetupSFTP.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
-import { handleChange, RequiredField, validateRequiredFields, OptionalNumberField } from './forms';
+import { handleChange, OptionalField, OptionalNumberField, RequiredField, validateRequiredFields, hasExactlyOneOf } from './forms';
 
 export class SetupSFTP extends Component {
     constructor() {
@@ -13,7 +13,19 @@ export class SetupSFTP extends Component {
     }
 
     validate() {
-        return validateRequiredFields(this, ["host", "port", "path", "keyfile"])
+        if (!validateRequiredFields(this, ["host", "port", "username", "path"])) {
+            return false;
+        }
+
+        if (!hasExactlyOneOf(this, ["keyfile", "keyData"])) {
+            return false;
+        }
+
+        if (!hasExactlyOneOf(this, ["knownHostsFile", "knownHostsData"])) {
+            return false;
+        }
+
+        return true;
     }
 
     render() {
@@ -21,10 +33,28 @@ export class SetupSFTP extends Component {
             <Form.Row>
                 {RequiredField(this, "Host", "host", { placeholder: "host name" })}
                 {OptionalNumberField(this, "Port", "port", { placeholder: "port number (e.g. 22)" })}
+                {RequiredField(this, "User", "username", { placeholder: "user name" })}
+            </Form.Row>
+            <Form.Row>
                 {RequiredField(this, "Path", "path", { placeholder: "enter remote path" })}
             </Form.Row>
             <Form.Row>
-                {RequiredField(this, "Key File", "keyfile", { placeholder: "enter path to the key file" })}
+                {OptionalField(this, "Path to key file", "keyfile", { placeholder: "enter path to the key file" })}
+                {OptionalField(this, "Path to known_hosts File", "knownHostsFile", { placeholder: "enter path to known_hosts file" })}
+            </Form.Row>
+            <Form.Row>
+                {OptionalField(this, "Key Data", "keyData", { 
+                    placeholder: "paste contents of the key file", 
+                    as: "textarea", 
+                    rows: 5,
+                    isInvalid: !hasExactlyOneOf(this, ["keyfile", "keyData"]),
+                }, null, <>Either <b>Key File</b> or <b>Key Data</b> is required, but not both.</>)}
+                {OptionalField(this, "Known Hosts Data", "knownHostsData", { 
+                    placeholder: "paste contents of the known_hosts file", 
+                    as: "textarea", 
+                    rows: 5,
+                    isInvalid: !hasExactlyOneOf(this, ["knownHostsFile", "knownHostsData"]),
+                }, null, <>Either <b>Known Hosts File</b> or <b>Known Hosts Data</b> is required, but not both.</>)})}
             </Form.Row>
         </>;
     }
--- htmlui/src/tests/SetupSFTP.test.js
+++ htmlui/src/tests/SetupSFTP.test.js
@@ -12,12 +12,36 @@ it('can set fields', async () => {
   changeControlValue(getByTestId("control-host"), "some-host");
   changeControlValue(getByTestId("control-port"), "22");
   changeControlValue(getByTestId("control-path"), "some-path");
+  changeControlValue(getByTestId("control-username"), "some-username");
   changeControlValue(getByTestId("control-keyfile"), "some-keyfile");
+  changeControlValue(getByTestId("control-knownHostsFile"), "some-knownHostsFile");
+  expect(ref.current.validate()).toBe(true);
+  expect(ref.current.state).toStrictEqual({
+    "host": "some-host",
+    "username": "some-username",
+    "keyfile": "some-keyfile",
+    "knownHostsFile": "some-knownHostsFile",
+    "path": "some-path",
+    "port": 22,
+  });
+
+  // now enter key data instead of key file, make sure validation triggers along the way
+  changeControlValue(getByTestId("control-keyData"), "some-keyData");
+  expect(ref.current.validate()).toBe(false);
+  changeControlValue(getByTestId("control-keyfile"), "");
+  expect(ref.current.validate()).toBe(true);
+  changeControlValue(getByTestId("control-knownHostsData"), "some-knownHostsData");
+  expect(ref.current.validate()).toBe(false);
+  changeControlValue(getByTestId("control-knownHostsFile"), "");
   expect(ref.current.validate()).toBe(true);
 
   expect(ref.current.state).toStrictEqual({
     "host": "some-host",
-    "keyfile": "some-keyfile",
+    "username": "some-username",
+    "keyfile": "",
+    "keyData": "some-keyData",
+    "knownHostsFile": "",
+    "knownHostsData": "some-knownHostsData",
     "path": "some-path",
     "port": 22,
   });
