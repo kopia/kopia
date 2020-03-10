01c3739ea88b8794825bf032d48e18b5d3bd03ef
Nick <nick@kasten.io>
2020-03-10 14:56:29 -0700

    [robustness testing] Implement metadata store for test metadata using kopia
    Add an implementation of the metadata store using kopia snapshots and restores to manage persistence of walk metadata. Metadata are stored to and retrieved from the store, and a mechanism for persisting the store is implemented using kopia snapshots.

---
+++ tests/robustness/snapmeta/snapmeta.go
@@ -0,0 +1,23 @@
+// Package snapmeta describes entities that can accept
+// arbitrary metadata and flush it to a persistent repository.
+package snapmeta
+
+import "github.com/kopia/kopia/tests/robustness/snap"
+
+// Store describes the ability to store and retrieve
+// a buffer of metadata, indexed by a string key.
+type Store interface {
+	Store(key string, val []byte) error
+	Load(key string) ([]byte, error)
+	Delete(key string)
+	GetKeys() []string
+}
+
+// Persister describes the ability to flush metadata
+// to, and load it again, from a repository.
+type Persister interface {
+	Store
+	snap.RepoManager
+	LoadMetadata() error
+	FlushMetadata() error
+}
