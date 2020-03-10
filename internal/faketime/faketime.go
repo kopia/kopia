431a274a02e859de0ad60a6d7a405c9252a8ba31
Julio Lopez <julio+gh@kasten.io>
2020-03-09 17:54:23 -0700

    Remove mutex from faketime.TimeAdvance

--- internal/faketime/faketime.go
+++ internal/faketime/faketime.go
@@ -3,6 +3,7 @@ package faketime
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -34,32 +35,28 @@ func AutoAdvance(t time.Time, dt time.Duration) func() time.Time {
 // TimeAdvance allows controlling the passage of time. Intended to be used in
 // tests.
 type TimeAdvance struct {
-	mu sync.Mutex
-	t  time.Time
+	base  time.Time
+	delta time.Duration
 }
 
 // NewTimeAdvance creates a TimeAdvance with the given start time
 func NewTimeAdvance(start time.Time) *TimeAdvance {
-	return &TimeAdvance{t: start}
+	return &TimeAdvance{base: start}
 }
 
 // NowFunc returns a time provider function for t
 func (t *TimeAdvance) NowFunc() func() time.Time {
 	return func() time.Time {
-		t.mu.Lock()
-		defer t.mu.Unlock()
+		dt := time.Duration(atomic.LoadInt64((*int64)(&t.delta)))
 
-		return t.t
+		return t.base.Add(dt)
 	}
 }
 
 // Advance advances t by dt, such that the next call to t.NowFunc()() returns
 // current t + dt
 func (t *TimeAdvance) Advance(dt time.Duration) time.Time {
-	t.mu.Lock()
-	defer t.mu.Unlock()
+	advance := atomic.AddInt64((*int64)(&t.delta), int64(dt))
 
-	t.t = t.t.Add(dt)
-
-	return t.t
+	return t.base.Add(time.Duration(advance))
 }
