78a921ab2c55cfb0b706a89b41fdee602a4b01e9
Nick <nwright970@gmail.com>
2020-03-11 17:21:30 +0000

    Rename Simple mutex

--- tests/robustness/snapmeta/simple.go
+++ tests/robustness/snapmeta/simple.go
@@ -8,8 +8,8 @@ var _ Store = &Simple{}
 // snapshot metadata as a byte slice in a map in memory.
 // A Simple should not be copied.
 type Simple struct {
-	m map[string][]byte
-	l sync.Mutex
+	m  map[string][]byte
+	mu sync.Mutex
 }
 
 // NewSimple instantiates a new Simple snapstore and
@@ -25,8 +25,8 @@ func (s *Simple) Store(key string, val []byte) error {
 	buf := make([]byte, len(val))
 	_ = copy(buf, val)
 
-	s.l.Lock()
-	defer s.l.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
 	s.m[key] = buf
 
@@ -35,8 +35,8 @@ func (s *Simple) Store(key string, val []byte) error {
 
 // Load implements the Storer interface Load method
 func (s *Simple) Load(key string) ([]byte, error) {
-	s.l.Lock()
-	defer s.l.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
 	if buf, found := s.m[key]; found {
 		retBuf := make([]byte, len(buf))
@@ -50,16 +50,16 @@ func (s *Simple) Load(key string) ([]byte, error) {
 
 // Delete implements the Storer interface Delete method
 func (s *Simple) Delete(key string) {
-	s.l.Lock()
-	defer s.l.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
 	delete(s.m, key)
 }
 
 // GetKeys implements the Storer interface GetKeys method
 func (s *Simple) GetKeys() []string {
-	s.l.Lock()
-	defer s.l.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
 	ret := make([]string, 0, len(s.m))
 
