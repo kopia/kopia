d707f3615ab8342f2123e5cbac1732ffafc5d74e
Julio López <julio+gh@kasten.io>
2020-04-17 21:47:50 -0700

    GC content repair (#420)
    Add content.Manager.UndeleteContent()
    Implementation based on RewriteContent()
    
    Undelete re-referenced content in snapshot GC

--- snapshot/gc/stats.go
+++ snapshot/gc/stats.go
@@ -5,6 +5,6 @@ type Stats struct {
 	// Keep int64 fields first to ensure they get aligned to at least 64-bit
 	// boundaries which is required for atomic access on ARM and x86-32.
 	// Also results in a smaller struct size
-	UnusedBytes, InUseBytes, SystemBytes, TooRecentBytes int64
-	UnusedCount, InUseCount, SystemCount, TooRecentCount uint32
+	UnusedBytes, InUseBytes, SystemBytes, TooRecentBytes, UndeletedBytes int64
+	UnusedCount, InUseCount, SystemCount, TooRecentCount, UndeletedCount uint32
 }
