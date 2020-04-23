3f6221f296a487342a938ec17bd02759a80d4df2
Jarek Kowalski <jaak@jkowalski.net>
2020-04-22 20:38:07 -0700

    manifest: added JSON serialization attributes to EntryMetadata

--- repo/manifest/manifest_entry.go
+++ repo/manifest/manifest_entry.go
@@ -5,8 +5,8 @@ import "time"
 // EntryMetadata contains metadata about manifest item. Each manifest item has one or more labels
 // Including required "type" label.
 type EntryMetadata struct {
-	ID      ID
-	Length  int
-	Labels  map[string]string
-	ModTime time.Time
+	ID      ID                `json:"id"`
+	Length  int               `json:"length"`
+	Labels  map[string]string `json:"labels"`
+	ModTime time.Time         `json:"mtime"`
 }
