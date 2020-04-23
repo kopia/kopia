44053b9c1dc1a5771b9c9d3e567e6986392c0ca7
Jarek Kowalski <jaak@jkowalski.net>
2020-04-22 20:44:15 -0700

    cli: added 'kopia repository connect server'
    This sets up repository connection via the API server instead of
    directly-manipulated storage.

---
+++ cli/command_repository_connect_server.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/kopia/kopia/repo"
+)
+
+var (
+	connectAPIServerCommand = connectCommand.Command("server", "Connect to a repository API Server.")
+
+	connectAPIServerURL             = connectAPIServerCommand.Flag("url", "Server URL").Required().String()
+	connectAPIServerCertFingerprint = connectAPIServerCommand.Flag("server-cert-fingerprint", "Server certificate fingerprint").String()
+)
+
+func runConnectAPIServerCommand(ctx context.Context) error {
+	password, err := getPasswordFromFlags(ctx, false, false)
+	if err != nil {
+		return errors.Wrap(err, "getting password")
+	}
+
+	as := &repo.APIServerInfo{
+		BaseURL:                             *connectAPIServerURL,
+		TrustedServerCertificateFingerprint: *connectAPIServerCertFingerprint,
+	}
+
+	configFile := repositoryConfigFileName()
+	if err := repo.ConnectAPIServer(ctx, configFile, as, password, connectOptions()); err != nil {
+		return err
+	}
+
+	printStderr("Connected to repository API Server.\n")
+	maybeInitializeUpdateCheck(ctx)
+
+	return nil
+}
+
+func init() {
+	connectAPIServerCommand.Action(noRepositoryAction(runConnectAPIServerCommand))
+}
