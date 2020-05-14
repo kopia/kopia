ca28469706d4eeb34868dafc2ef65bab869c7534
Jarek Kowalski <jaak@jkowalski.net>
2020-05-13 23:43:45 -0700

    cli: improved 'snapshot delete' usage (#436)
    New usage:
    
    ```
    kopia snapshot delete manifestID... [--delete]
    kopia snapshot delete rootObjectID... [--delete]
    ```
    
    Fixes #435
    
    cli: added --unsafe-ignore-source as alias for `--delete`
    This is a hidden flag for backwards compatibility. It will be removed.

--- cli/command_snapshot_delete.go
+++ cli/command_snapshot_delete.go
@@ -2,66 +2,90 @@ package cli
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 
 	"github.com/kopia/kopia/repo"
 	"github.com/kopia/kopia/repo/manifest"
+	"github.com/kopia/kopia/repo/object"
+	"github.com/kopia/kopia/snapshot"
 )
 
 var (
-	snapshotDeleteCommand      = snapshotCommands.Command("delete", "Explicitly delete a snapshot by providing a snapshot ID.")
-	snapshotDeleteID           = snapshotDeleteCommand.Arg("id", "Snapshot ID to be deleted").Required().String()
-	snapshotDeletePath         = snapshotDeleteCommand.Flag("path", "Specify the path of the snapshot to be deleted").String()
-	snapshotDeleteHostname     = snapshotDeleteCommand.Flag("hostname", "Specify the hostname of the snapshot to be deleted").String()
-	snapshotDeleteUsername     = snapshotDeleteCommand.Flag("username", "Specify the username of the snapshot to be deleted").String()
-	snapshotDeleteIgnoreSource = snapshotDeleteCommand.Flag("unsafe-ignore-source", "Override the requirement to specify source info for the delete to succeed").Bool()
+	snapshotDeleteCommand = snapshotCommands.Command("delete", "Explicitly delete a snapshot by providing a snapshot ID.")
+	snapshotDeleteIDs     = snapshotDeleteCommand.Arg("id", "Snapshot ID or root object ID to be deleted").Required().Strings()
+	snapshotDeleteConfirm = snapshotDeleteCommand.Flag("delete", "Confirm deletion").Bool()
 )
 
 func runDeleteCommand(ctx context.Context, rep repo.Repository) error {
-	if !*snapshotDeleteIgnoreSource && *snapshotDeletePath == "" {
-		return errors.New("path is required")
+	for _, id := range *snapshotDeleteIDs {
+		if strings.HasPrefix(id, "k") {
+			if err := deleteSnapshotsByRootObjectID(ctx, rep, object.ID(id)); err != nil {
+				return errors.Wrapf(err, "error deleting snapshots by root ID %v", id)
+			}
+		} else {
+			m, err := snapshot.LoadSnapshot(ctx, rep, manifest.ID(id))
+			if err != nil {
+				return errors.Wrapf(err, "error loading snapshot %v", id)
+			}
+
+			if err := deleteSnapshot(ctx, rep, m); err != nil {
+				return errors.Wrapf(err, "error deleting %v", id)
+			}
+		}
 	}
 
-	manifestID := manifest.ID(*snapshotDeleteID)
+	return nil
+}
 
-	manifestMeta, err := rep.GetManifest(ctx, manifestID, nil)
+func deleteSnapshot(ctx context.Context, rep repo.Repository, m *snapshot.Manifest) error {
+	desc := fmt.Sprintf("snapshot %v of %v at %v", m.ID, m.Source, formatTimestamp(m.StartTime))
+
+	if !*snapshotDeleteConfirm {
+		printStderr("Would delete %v (pass --delete to confirm)\n", desc)
+		return nil
+	}
+
+	printStderr("Deleting %v...\n", desc)
+
+	return rep.DeleteManifest(ctx, m.ID)
+}
+
+func deleteSnapshotsByRootObjectID(ctx context.Context, rep repo.Repository, rootID object.ID) error {
+	ids, err := snapshot.ListSnapshotManifests(ctx, rep, nil)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error listing snapshot manifests")
 	}
 
-	labels := manifestMeta.Labels
-	if labels["type"] != "snapshot" {
-		return errors.Errorf("snapshot ID provided (%v) did not reference a snapshot", manifestID)
+	manifests, err := snapshot.LoadSnapshots(ctx, rep, ids)
+	if err != nil {
+		return errors.Wrap(err, "error loading snapshot manifests")
 	}
 
-	if !*snapshotDeleteIgnoreSource {
-		h := *snapshotDeleteHostname
-		if h == "" {
-			h = rep.Hostname()
-		}
+	cnt := 0
 
-		if labels["hostname"] != h {
-			return errors.Errorf("host name does not match for deleting requested snapshot ID (got %q, expected %q)", h, labels["hostname"])
-		}
+	for _, m := range manifests {
+		if m.RootObjectID() == rootID {
+			cnt++
 
-		u := *snapshotDeleteUsername
-		if u == "" {
-			u = rep.Username()
-		}
-
-		if labels["username"] != u {
-			return errors.Errorf("user name does not match for deleting requested snapshot ID (got %q, expected %q)", u, labels["username"])
-		}
-
-		if labels["path"] != *snapshotDeletePath {
-			return errors.New("path does not match for deleting requested snapshot ID")
+			if err := deleteSnapshot(ctx, rep, m); err != nil {
+				return errors.Wrap(err, "error deleting")
+			}
 		}
 	}
 
-	return rep.DeleteManifest(ctx, manifestID)
+	if cnt == 0 {
+		return errors.Errorf("no snapshots matched %v", rootID)
+	}
+
+	return nil
 }
 
 func init() {
 	snapshotDeleteCommand.Action(repositoryAction(runDeleteCommand))
+
+	// hidden flag for backwards compatibility
+	snapshotDeleteCommand.Flag("unsafe-ignore-source", "Alias for --delete").Hidden().BoolVar(snapshotDeleteConfirm)
 }
