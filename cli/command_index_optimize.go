1c2534199c02bc7afe6d461c0b393a31b5f2f121
Jarek Kowalski <jaak@jkowalski.net>
2020-06-23 23:12:36 -0700

    cli: fixed panic() during snapshot verification caused by refactoring to new Repository interface

--- cli/command_index_optimize.go
+++ cli/command_index_optimize.go
@@ -12,6 +12,7 @@ var (
 	optimizeCommand              = indexCommands.Command("optimize", "Optimize indexes blobs.")
 	optimizeMaxSmallBlobs        = optimizeCommand.Flag("max-small-blobs", "Maximum number of small index blobs that can be left after compaction.").Default("1").Int()
 	optimizeDropDeletedOlderThan = optimizeCommand.Flag("drop-deleted-older-than", "Drop deleted contents above given age").Duration()
+	optimizeDropContents         = optimizeCommand.Flag("drop-contents", "Drop contents with given IDs").Strings()
 	optimizeAllIndexes           = optimizeCommand.Flag("all", "Optimize all indexes, even those above maximum size.").Bool()
 )
 
@@ -19,6 +20,7 @@ func runOptimizeCommand(ctx context.Context, rep *repo.DirectRepository) error {
 	opt := content.CompactOptions{
 		MaxSmallBlobs: *optimizeMaxSmallBlobs,
 		AllIndexes:    *optimizeAllIndexes,
+		DropContents:  toContentIDs(*optimizeDropContents),
 	}
 
 	if age := *optimizeDropDeletedOlderThan; age > 0 {
