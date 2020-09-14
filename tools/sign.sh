44968af7b690b0fed1ef7d8339ef3ec76695307b
Jarek Kowalski <jaak@jkowalski.net>
2020-09-13 17:05:46 -0700

    release: fixed RPM signing by moving all of this to a single invocation of sign.sh

--- tools/sign.sh
+++ tools/sign.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
-input=$1
-signature=$2
 
-# add signature to RPMs
-if [ ${input: -4} == ".rpm" ]; then
-  rpm --define "%_gpg_name Kopia Builder" --addsign $input
-fi
+for f in dist/*rpm; do
+  # add signature to RPMs
+  rpm --define "%_gpg_name Kopia Builder" --addsign $f
+done
 
-if [ $input == "dist/checksums.txt" ]; then
-    # before signing checksums.txt, regenerate it since we've just signed some RPMs.
-    filenames=$(cut -f 2- -d " " dist/checksums.txt)
-    (cd dist && sha256sum $filenames > checksums.txt)
-    gpg --output dist/checksums.txt.sig --detach-sig dist/checksums.txt
-fi
+# before signing checksums.txt, regenerate it since we've just signed some RPMs.
+filenames=$(cut -f 2- -d " " dist/checksums.txt)
+(cd dist && sha256sum $filenames > checksums.txt)
+gpg --output dist/checksums.txt.sig --detach-sig dist/checksums.txt
