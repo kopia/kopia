fe05a6d653e0e67a85d26e54ce73b5168b90b755
Jarek Kowalski <jaak@jkowalski.net>
2021-02-04 20:16:32 -0800

    uitask: added package for managing and introspection into tasks running inside the process

---
+++ internal/uitask/uitask_manager.go
@@ -0,0 +1,208 @@
+// Package uitask provided management of in-process long-running tasks that are exposed to the UI.
+package uitask
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/kopia/kopia/internal/clock"
+	"github.com/kopia/kopia/repo/logging"
+)
+
+const (
+	maxFinishedTasks      = 50
+	maxLogMessagesPerTask = 1000
+)
+
+// Manager manages UI tasks.
+type Manager struct {
+	mu         sync.Mutex
+	nextTaskID int
+	running    map[string]*runningTaskInfo
+	finished   map[string]*Info
+
+	MaxFinishedTasks      int
+	MaxLogMessagesPerTask int
+}
+
+// Controller allows the task to communicate with task manager and receive signals.
+type Controller interface {
+	CurrentTaskID() string
+	OnCancel(cancelFunc context.CancelFunc)
+}
+
+// TaskFunc represents a task function.
+type TaskFunc func(ctx context.Context, ctrl Controller) error
+
+// Run executes the provided task in the current goroutine while allowing it to be externally examined and canceled.
+func (m *Manager) Run(ctx context.Context, kind, description string, task TaskFunc) error {
+	r := &runningTaskInfo{
+		Info: Info{
+			Kind:        kind,
+			Description: description,
+			Status:      StatusRunning,
+		},
+		maxLogMessages: m.MaxLogMessagesPerTask,
+	}
+
+	ctx = logging.WithLogger(ctx, r.loggerForModule)
+
+	m.startTask(r)
+	defer m.completeTask(r)
+
+	err := task(ctx, r)
+
+	if r.Status != StatusCanceled {
+		if err != nil {
+			r.Status = StatusFailed
+			r.ErrorMessage = err.Error()
+		} else {
+			r.Status = StatusSuccess
+		}
+	}
+
+	return err
+}
+
+// ListTasks lists all running and some recently-finished tasks up to configured limits.
+func (m *Manager) ListTasks() []Info {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	var res []Info
+
+	for _, v := range m.running {
+		res = append(res, v.Info)
+	}
+
+	for _, v := range m.finished {
+		res = append(res, *v)
+	}
+
+	// most recent first
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].StartTime.After(res[j].StartTime)
+	})
+
+	return res
+}
+
+// TaskSummary returns the summary (number of tasks by status).
+func (m *Manager) TaskSummary() map[Status]int {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	s := map[Status]int{
+		StatusRunning: len(m.running),
+	}
+
+	for _, v := range m.finished {
+		s[v.Status]++
+	}
+
+	return s
+}
+
+// TaskLog retrieves the log from the task.
+func (m *Manager) TaskLog(taskID string) []LogEntry {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if r := m.running[taskID]; r != nil {
+		return r.log()
+	}
+
+	if f, ok := m.finished[taskID]; ok {
+		return append([]LogEntry(nil), f.LogLines...)
+	}
+
+	return nil
+}
+
+// GetTask retrieves the task info.
+func (m *Manager) GetTask(taskID string) (Info, bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if r := m.running[taskID]; r != nil {
+		return r.Info, true
+	}
+
+	if f, ok := m.finished[taskID]; ok {
+		return *f, true
+	}
+
+	return Info{}, false
+}
+
+// CancelTask retrieves the log from the task.
+func (m *Manager) CancelTask(taskID string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	t := m.running[taskID]
+
+	if t.Status == StatusRunning {
+		for _, c := range t.taskCancel {
+			c()
+		}
+
+		t.taskCancel = nil
+		t.Status = StatusCanceled
+	}
+}
+
+func (m *Manager) startTask(r *runningTaskInfo) string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.nextTaskID++
+
+	taskID := fmt.Sprintf("%x", m.nextTaskID)
+	r.StartTime = clock.Now()
+	r.TaskID = taskID
+	m.running[taskID] = r
+
+	return taskID
+}
+
+func (m *Manager) completeTask(r *runningTaskInfo) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	now := clock.Now()
+	r.EndTime = &now
+
+	delete(m.running, r.TaskID)
+	m.finished[r.TaskID] = &r.Info
+
+	// delete oldest finished tasks up to configured limit.
+	for len(m.finished) > m.MaxFinishedTasks {
+		var (
+			oldestStartTime time.Time
+			oldestID        string
+		)
+
+		for _, v := range m.finished {
+			if oldestStartTime.IsZero() || v.StartTime.Before(oldestStartTime) {
+				oldestStartTime = v.StartTime
+				oldestID = v.TaskID
+			}
+		}
+
+		delete(m.finished, oldestID)
+	}
+}
+
+// NewManager creates new UI Task Manager.
+func NewManager() *Manager {
+	return &Manager{
+		running:  map[string]*runningTaskInfo{},
+		finished: map[string]*Info{},
+
+		MaxLogMessagesPerTask: maxLogMessagesPerTask,
+		MaxFinishedTasks:      maxFinishedTasks,
+	}
+}
---
+++ internal/uitask/uitask_test.go
@@ -0,0 +1,302 @@
+package uitask_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+
+	"github.com/kopia/kopia/internal/uitask"
+	"github.com/kopia/kopia/repo/content"
+	"github.com/kopia/kopia/repo/logging"
+)
+
+var (
+	log        = logging.GetContextLoggerFunc("uitasktest")
+	ignoredLog = logging.GetContextLoggerFunc(content.FormatLogModule)
+)
+
+func TestUITask(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	m := uitask.NewManager()
+
+	m.MaxLogMessagesPerTask = 3
+	m.MaxFinishedTasks = 3
+
+	verifyTaskList(t, m, nil)
+
+	var tid1a, tid1, tid2, tid3, tid4, tid5 string
+
+	m.Run(ctx, "some-kind", "test-1", func(ctx context.Context, ctrl uitask.Controller) error {
+		tid1a = ctrl.CurrentTaskID()
+
+		tsk, ok := m.GetTask(tid1a)
+		if !ok {
+			t.Fatalf("task not found")
+		}
+
+		if got, want := tsk.Description, "test-1"; got != want {
+			t.Fatalf("invalid task description %v, want %v", got, want)
+		}
+
+		if got, want := tsk.Status, uitask.StatusRunning; got != want {
+			t.Fatalf("invalid task status %v, want %v", got, want)
+		}
+
+		verifyTaskList(t, m, map[string]uitask.Status{
+			tid1a: uitask.StatusRunning,
+		})
+
+		verifyTaskLog(t, m, tid1a, nil)
+		log(ctx).Debugf("first")
+		ignoredLog(ctx).Debugf("this is ignored")
+		log(ctx).Infof("iii")
+		verifyTaskLog(t, m, tid1a, []string{
+			"first",
+			"iii",
+		})
+		log(ctx).Warningf("www")
+		log(ctx).Errorf("eee")
+
+		// 'first' has aged out
+		verifyTaskLog(t, m, tid1a, []string{
+			"iii",
+			"www",
+			"eee",
+		})
+
+		log(ctx).Noticef("nnn")
+		log(ctx).Fatalf("fff")
+		verifyTaskLog(t, m, tid1a, []string{
+			"eee",
+			"nnn",
+			"fff",
+		})
+
+		return nil
+	})
+
+	tid1 = getTaskID(t, m, "test-1")
+	if tid1 != tid1a {
+		t.Fatalf("task ID has changed after completion: %v vs %v", tid1, tid1a)
+	}
+
+	tsk, ok := m.GetTask(tid1)
+	if !ok {
+		t.Fatalf("task not found")
+	}
+
+	if got, want := tsk.Description, "test-1"; got != want {
+		t.Fatalf("invalid task description %v, want %v", got, want)
+	}
+
+	if got, want := tsk.Status, uitask.StatusSuccess; got != want {
+		t.Fatalf("invalid task status %v, want %v", got, want)
+	}
+
+	// get non-existent task.
+	if _, ok := m.GetTask(uuid.New().String()); ok {
+		t.Fatalf("task unexpectedly found")
+	}
+
+	verifyTaskLog(t, m, uuid.New().String(), nil)
+
+	// task log still available after task finished.
+	verifyTaskLog(t, m, tid1, []string{
+		"eee",
+		"nnn",
+		"fff",
+	})
+
+	m.Run(ctx, "some-kind", "test-2", func(ctx context.Context, ctrl uitask.Controller) error {
+		tid2 = ctrl.CurrentTaskID()
+		verifyTaskList(t, m, map[string]uitask.Status{
+			tid1: uitask.StatusSuccess,
+			tid2: uitask.StatusRunning,
+		})
+
+		return nil
+	})
+
+	m.Run(ctx, "some-kind", "test-3", func(ctx context.Context, ctrl uitask.Controller) error {
+		tid3 = ctrl.CurrentTaskID()
+		verifyTaskList(t, m, map[string]uitask.Status{
+			tid1: uitask.StatusSuccess,
+			tid2: uitask.StatusSuccess,
+			tid3: uitask.StatusRunning,
+		})
+
+		if diff := cmp.Diff(m.TaskSummary(), map[uitask.Status]int{
+			uitask.StatusRunning: 1,
+			uitask.StatusSuccess: 2,
+		}); diff != "" {
+			t.Fatalf("unexpected summary: %v", diff)
+		}
+
+		return errors.Errorf("some error")
+	})
+
+	verifyTaskList(t, m, map[string]uitask.Status{
+		tid1: uitask.StatusSuccess,
+		tid2: uitask.StatusSuccess,
+		tid3: uitask.StatusFailed,
+	})
+
+	m.Run(ctx, "some-kind", "test-4", func(ctx context.Context, ctrl uitask.Controller) error {
+		tid4 = ctrl.CurrentTaskID()
+		return nil
+	})
+
+	// test-1 is aged out
+	verifyTaskList(t, m, map[string]uitask.Status{
+		tid2: uitask.StatusSuccess,
+		tid3: uitask.StatusFailed,
+		tid4: uitask.StatusSuccess,
+	})
+
+	m.Run(ctx, "some-kind", "test-5", func(ctx context.Context, ctrl uitask.Controller) error {
+		tid5 = ctrl.CurrentTaskID()
+		return nil
+	})
+
+	// test-2 is aged out
+	verifyTaskList(t, m, map[string]uitask.Status{
+		tid3: uitask.StatusFailed,
+		tid4: uitask.StatusSuccess,
+		tid5: uitask.StatusSuccess,
+	})
+
+	if diff := cmp.Diff(m.TaskSummary(), map[uitask.Status]int{
+		uitask.StatusRunning: 0,
+		uitask.StatusFailed:  1,
+		uitask.StatusSuccess: 2,
+	}); diff != "" {
+		t.Fatalf("unexpected summary: %v", diff)
+	}
+}
+
+func verifyTaskList(t *testing.T, m *uitask.Manager, wantStatuses map[string]uitask.Status) {
+	t.Helper()
+
+	tasks := m.ListTasks()
+	if got, want := len(tasks), len(wantStatuses); got != want {
+		t.Fatalf("invalid task list length: %v, want %v", got, want)
+	}
+
+	for taskID, wantStatus := range wantStatuses {
+		if got := mustFindTask(t, tasks, taskID).Status; got != wantStatus {
+			t.Fatalf("task %v status was %v, wanted %v", taskID, got, wantStatus)
+		}
+	}
+}
+
+func TestUITaskCancel_AfterOnCancel(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	m := uitask.NewManager()
+
+	ch := make(chan string)
+
+	go func() {
+		childTaskID := <-ch
+
+		time.Sleep(time.Second)
+		m.CancelTask(childTaskID)
+	}()
+
+	m.Run(ctx, "some-kind", "test-1", func(ctx context.Context, ctrl uitask.Controller) error {
+		// send my task ID to the goroutine which will cancel our task
+		ch <- ctrl.CurrentTaskID()
+		canceled := make(chan struct{})
+		ctrl.OnCancel(func() {
+			close(canceled)
+		})
+
+		<-canceled
+
+		return nil
+	})
+}
+
+func TestUITaskCancel_BeforeOnCancel(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	m := uitask.NewManager()
+
+	ch := make(chan string)
+
+	go func() {
+		childTaskID := <-ch
+		m.CancelTask(childTaskID)
+	}()
+
+	m.Run(ctx, "some-kind", "test-1", func(ctx context.Context, ctrl uitask.Controller) error {
+		// send my task ID to the goroutine which will cancel our task
+		ch <- ctrl.CurrentTaskID()
+		time.Sleep(1 * time.Second)
+		canceled := make(chan struct{})
+		ctrl.OnCancel(func() {
+			close(canceled)
+		})
+
+		<-canceled
+
+		return nil
+	})
+}
+
+func getTaskID(t *testing.T, m *uitask.Manager, desc string) string {
+	t.Helper()
+
+	// nolint:gocritic
+	for _, tsk := range m.ListTasks() {
+		if tsk.Description == desc {
+			return tsk.TaskID
+		}
+	}
+
+	t.Fatalf("task with description %v was not found", desc)
+
+	return ""
+}
+
+func verifyTaskLog(t *testing.T, m *uitask.Manager, taskID string, want []string) {
+	t.Helper()
+
+	if got, want := logText(m.TaskLog(taskID)), strings.Join(want, "\n"); got != want {
+		t.Fatalf("invalid task log %v, want %v", got, want)
+	}
+}
+
+func logText(items []uitask.LogEntry) string {
+	var result []string
+
+	for _, it := range items {
+		result = append(result, it.Text)
+	}
+
+	return strings.Join(result, "\n")
+}
+
+func mustFindTask(t *testing.T, tasks []uitask.Info, tid string) uitask.Info {
+	t.Helper()
+
+	// nolint:gocritic
+	for _, tsk := range tasks {
+		if tsk.TaskID == tid {
+			return tsk
+		}
+	}
+
+	t.Fatalf("task %v not found", tid)
+
+	return uitask.Info{}
+}
