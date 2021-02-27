dbc070a17e899eaa24c9a8969c5911e66b6d0c09
Jarek Kowalski <jaak@jkowalski.net>
2021-02-27 06:49:44 -0800

    cli: get/set ignore-unknown-types policy flag

--- cli/command_policy_set_error_handling.go
+++ cli/command_policy_set_error_handling.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"context"
 
+	"github.com/pkg/errors"
+
 	"github.com/kopia/kopia/snapshot/policy"
 )
 
@@ -10,12 +12,21 @@ var (
 	// Error handling behavior.
 	policyIgnoreFileErrors      = policySetCommand.Flag("ignore-file-errors", "Ignore errors reading files while traversing ('true', 'false', 'inherit')").Enum(booleanEnumValues...)
 	policyIgnoreDirectoryErrors = policySetCommand.Flag("ignore-dir-errors", "Ignore errors reading directories while traversing ('true', 'false', 'inherit").Enum(booleanEnumValues...)
+	policyIgnoreUnknownTypes    = policySetCommand.Flag("ignore-unknown-types", "Ignore unknown entry types in directories ('true', 'false', 'inherit").Enum(booleanEnumValues...)
 )
 
 func setErrorHandlingPolicyFromFlags(ctx context.Context, fp *policy.ErrorHandlingPolicy, changeCount *int) error {
-	if err := applyPolicyBoolPtr(ctx, "ignore file read errors", &fp.IgnoreFileErrors, *policyIgnoreFileErrors, changeCount); err != nil {
-		return err
+	if err := applyPolicyBoolPtr(ctx, "ignore file errors", &fp.IgnoreFileErrors, *policyIgnoreFileErrors, changeCount); err != nil {
+		return errors.Wrap(err, "ignore file errors")
 	}
 
-	return applyPolicyBoolPtr(ctx, "ignore dir read errors", &fp.IgnoreDirectoryErrors, *policyIgnoreDirectoryErrors, changeCount)
+	if err := applyPolicyBoolPtr(ctx, "ignore directory errors", &fp.IgnoreDirectoryErrors, *policyIgnoreDirectoryErrors, changeCount); err != nil {
+		return errors.Wrap(err, "ignore directory errors")
+	}
+
+	if err := applyPolicyBoolPtr(ctx, "ignore unknown types", &fp.IgnoreUnknownTypes, *policyIgnoreUnknownTypes, changeCount); err != nil {
+		return errors.Wrap(err, "ignore unknown types")
+	}
+
+	return nil
 }
