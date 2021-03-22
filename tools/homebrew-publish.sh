e8d14af0dbb134f2dd486ec5e7b85d189847afd6
Jarek Kowalski <jaak@jkowalski.net>
2021-03-21 20:57:46 -0700

    ci: publish homebrew test builds to kopia/test-builds TAP

--- tools/homebrew-publish.sh
+++ tools/homebrew-publish.sh
@@ -3,9 +3,12 @@ set -e
 dist_dir=$1
 ver=$2
 
+target_repo=kopia/homebrew-kopia
+source_repo=kopia/kopia
+
 if [ "$CI_TAG" == "" ]; then
-  echo Not publishing Homebrew package on non-tagged releases.
-  exit 0
+    target_repo=kopia/homebrew-test-builds
+    source_repo=kopia/kopia-test-builds
 fi
 
 if [ "$GITHUB_TOKEN" == "" ]; then
@@ -18,7 +21,7 @@ if [ "$REPO_OWNER/$GITHUB_REF" != "kopia/refs/heads/master" ]; then
   exit 0
 fi
 
-echo Publishing Homebrew version $ver from $dist_dir...
+echo Publishing Homebrew version $source_repo version $ver to $target_repo from $dist_dir...
 
 HASH_MAC_AMD64=$(sha256sum $dist_dir/kopia-$ver-macOS-x64.tar.gz | cut -f 1 -d " ")
 HASH_MAC_ARM64=$(sha256sum $dist_dir/kopia-$ver-macOS-arm64.tar.gz | cut -f 1 -d " ")
@@ -26,10 +29,11 @@ HASH_LINUX_AMD64=$(sha256sum $dist_dir/kopia-$ver-linux-x64.tar.gz | cut -f 1 -d
 HASH_LINUX_ARM64=$(sha256sum $dist_dir/kopia-$ver-linux-arm64.tar.gz | cut -f 1 -d " ")
 HASH_LINUX_ARM=$(sha256sum $dist_dir/kopia-$ver-linux-arm.tar.gz | cut -f 1 -d " ")
 tmpdir=$(mktemp -d)
-git clone https://$GITHUB_TOKEN@github.com/kopia/homebrew-kopia.git $tmpdir
+git clone https://$GITHUB_TOKEN@github.com/$target_repo.git $tmpdir
 
 cat tools/kopia-homebrew.rs.template | \
    sed "s/VERSION/$ver/g" | \
+   sed "s!SOURCE_REPO!$source_repo!g" | \
    sed "s/HASH_MAC_AMD64/$HASH_MAC_AMD64/g" | \
    sed "s/HASH_MAC_ARM64/$HASH_MAC_ARM64/g" | \
    sed "s/HASH_LINUX_AMD64/$HASH_LINUX_AMD64/g" | \
