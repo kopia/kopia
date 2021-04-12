b1843eb67a1bc4697e06ead6b512b5b7f8020fa8
Robert Kroeger <rjkroege@liqui.org>
2021-04-12 06:43:52 -0400

    Better windows build fix.

--- snapshot/restore/long_paths_windows.go
+++ snapshot/restore/long_paths_windows.go
@@ -1,5 +1,12 @@
 package restore
 
+import (
+	"syscall"
+)
+
+// MaxFilenameLength is the maximum length of a filename.
+const MaxFilenameLength = syscall.MAX_PATH
+
 // SafelySuffixablePath returns true if path can be suffixed with the
 // placeholder suffix and written to the filesystem.
 func SafelySuffixablePath(path string) bool {
