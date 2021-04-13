3f9ec47d53faf5aa090ca0f24cb474cf4d9c559c
Julio Lopez <julio+gh@kasten.io>
2021-04-13 14:51:29 -0700

    blob/pit: Extend Versioned storage with listing versions functions

--- repo/blob/pit/versioned.go
+++ repo/blob/pit/versioned.go
@@ -16,8 +16,18 @@ type VersionMetadata struct {
 	Version        string
 }
 
-// VersionedStorage defines the API for accessing stores with object versioning
-// enabled.
+// VersionMetadataCallback is called when processing the metadata for each blob version.
+type VersionMetadataCallback func(VersionMetadata) error
+
+// VersionedStorage defines the API for accessing stores with object versioning enabled.
 type VersionedStorage interface {
-	IsVersioned(ctx context.Context) (bool, error)
+	// IsVersioned returns whether the backend storage is versioned. Often, this
+	// is a property of the blob store bucket.
+	IsVersioned(context.Context) (bool, error)
+
+	// GetBlobVersions lists all the versions for the blob with the given ID.
+	GetBlobVersions(context.Context, blob.ID, VersionMetadataCallback) error
+
+	// ListBlobVersions lists all versions for all the blobs with the given blob ID prefix.
+	ListBlobVersions(context.Context, blob.ID, VersionMetadataCallback) error
 }
