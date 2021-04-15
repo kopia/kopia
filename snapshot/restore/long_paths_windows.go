460e2da383ff43b041aa3357b08be6778d11bee3
Robert Kroeger <rjkroege@liqui.org>
2021-04-15 06:43:20 -0400

    Yet another attempted windows fix

--- snapshot/restore/long_paths_windows.go
+++ snapshot/restore/long_paths_windows.go
@@ -1,14 +1,18 @@
 package restore
 
 import (
+	"path/filepath"
 	"syscall"
+
+	"github.com/kopia/kopia/fs/localfs"
 )
 
 // MaxFilenameLength is the maximum length of a filename.
-const MaxFilenameLength = syscall.MAX_PATH
+// Go source suggests reducing this by 1.
+const MaxFilenameLength = syscall.MAX_PATH - 1
 
 // SafelySuffixablePath returns true if path can be suffixed with the
 // placeholder suffix and written to the filesystem.
 func SafelySuffixablePath(path string) bool {
-	return true
+	return len(filepath.Base(path))+len(localfs.ShallowEntrySuffix) <= MaxFilenameLength
 }
