fc5e405d3a657a75b1d887f20da50f71a800951a
Jarek Kowalski <jaak@jkowalski.net>
2021-05-02 20:27:20 -0700

    cli: fixed password_*.go build

--- cli/password_linux.go
+++ cli/password_linux.go
@@ -1,6 +1,10 @@
 package cli
 
-import "github.com/kopia/kopia/repo"
+import (
+	"github.com/alecthomas/kingpin"
+
+	"github.com/kopia/kopia/repo"
+)
 
 func (c *TheApp) setupOSSpecificKeychainFlags(app *kingpin.Application) {
 	app.Flag("use-keyring", "Use Gnome Keyring for storing repository password.").Default("false").BoolVar(&repo.KeyRingEnabled)
--- cli/password_windows.go
+++ cli/password_windows.go
@@ -1,6 +1,10 @@
 package cli
 
-import "github.com/kopia/kopia/repo"
+import (
+	"github.com/alecthomas/kingpin"
+
+	"github.com/kopia/kopia/repo"
+)
 
 func (c *TheApp) setupOSSpecificKeychainFlags(app *kingpin.Application) {
 	app.Flag("use-credential-manager", "Use Windows Credential Manager for storing repository password.").Default("true").BoolVar(&repo.KeyRingEnabled)
