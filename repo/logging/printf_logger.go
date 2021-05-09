b725401e9e89bd3a13cf276280692db26953aba4
Jarek Kowalski <jaak@jkowalski.net>
2021-05-09 14:40:59 -0700

    cli: added cli.RunSubcommand() which is used in testing to execute a subcommand in the same process

--- repo/logging/printf_logger.go
+++ repo/logging/printf_logger.go
@@ -1,5 +1,11 @@
 package logging
 
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
 type printfLogger struct {
 	printf func(msg string, args ...interface{})
 	prefix string
@@ -13,5 +19,22 @@ func (l *printfLogger) Errorf(msg string, args ...interface{}) { l.printf(l.pref
 func Printf(printf func(msg string, args ...interface{})) LoggerForModuleFunc {
 	return func(module string) Logger {
 		return &printfLogger{printf, "[" + module + "]"}
+	}
+}
+
+// Writer returns LoggerForModuleFunc that uses given writer for log output.
+func Writer(w io.Writer) LoggerForModuleFunc {
+	printf := func(msg string, args ...interface{}) {
+		msg = fmt.Sprintf(msg, args...)
+
+		if !strings.HasSuffix(msg, "\n") {
+			msg += "\n"
+		}
+
+		io.WriteString(w, msg) //nolint:errcheck
+	}
+
+	return func(module string) Logger {
+		return &printfLogger{printf, ""}
 	}
 }
