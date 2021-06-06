e71c2e1f1f66827ef9d9d8b993d1cb04f54a40ff
Robert Kroeger <rjkroege@liqui.org>
2021-06-06 17:58:34 -0400

    Fix spelling mistake.

--- snapshot/restore/long_paths_windows.go
+++ snapshot/restore/long_paths_windows.go
@@ -6,7 +6,7 @@ import (
 	"github.com/kopia/kopia/fs/localfs"
 )
 
-// MaxFilenameLength is set for compatability with Linux and MacOS.
+// MaxFilenameLength is set for compatibility with Linux and MacOS.
 const MaxFilenameLength = 255
 
 // SafelySuffixablePath returns true if path can be suffixed with the
