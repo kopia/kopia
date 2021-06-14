b8bdb7ca9713790a044635c96c2d744da674a6f1
Jarek Kowalski <jaak@jkowalski.net>
2021-06-13 17:26:02 -0700

    content: refactored ParseIndexBlob

--- repo/content/content_index_reader.go
+++ repo/content/content_index_reader.go
@@ -2,12 +2,9 @@ package content
 
 import (
 	"context"
-
-	"github.com/kopia/kopia/repo/blob"
 )
 
 // IndexBlobReader defines content read API.
 type IndexBlobReader interface {
-	ParseIndexBlob(ctx context.Context, blobID blob.ID) ([]Info, error)
 	IndexBlobs(ctx context.Context, includeInactive bool) ([]IndexBlobInfo, error)
 }
