1238a0db6056b28442d7e92d8bb870379d917e1d
Jarek Kowalski <jaak@jkowalski.net>
2021-08-06 21:54:38 -0700

    cli: additional 'index inspect' flags (#1231)
    * cli: fixed output of 'index inspect' to go to stdout
    
    * cli: additional flags for 'index inspect'

--- cli/command_index_inspect_test.go
+++ cli/command_index_inspect_test.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/kopia/kopia/tests/testenv"
 )
 
@@ -13,5 +15,18 @@ func TestIndexInspect(t *testing.T) {
 	env.RunAndExpectSuccess(t, "repo", "create", "filesystem", "--path", env.RepoDir)
 
 	someIndex := strings.Split(env.RunAndExpectSuccess(t, "index", "list")[0], " ")[0]
+	someContentID := env.RunAndExpectSuccess(t, "content", "list")[0]
 	env.RunAndExpectSuccess(t, "index", "inspect", someIndex)
+	env.RunAndExpectSuccess(t, "index", "inspect", "--active")
+	env.RunAndExpectSuccess(t, "index", "inspect", "--all")
+
+	require.Len(t, env.RunAndExpectSuccess(t, "index", "inspect", "--active", "--content-id", someContentID), 1)
+	require.Empty(t, env.RunAndExpectSuccess(t, "index", "inspect", "--active", "--content-id", "nosuchcontent"))
+
+	// now rewrite one content, making it appear in second index
+	env.RunAndExpectSuccess(t, "content", "rewrite", someContentID, "--safety=none")
+	require.Len(t, env.RunAndExpectSuccess(t, "index", "inspect", "--all", "--content-id", someContentID), 2)
+
+	// no targets specified
+	env.RunAndExpectFailure(t, "index", "inspect")
 }
