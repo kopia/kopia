59b5a44ca675805c703f90a88b186aeb8da12bf8
Jarek Kowalski <jaak@jkowalski.net>
2021-09-01 20:50:14 -0700

    Makefile: refactored build tools installation (#1274)
    Instead of using hardcore Makefile scripts and external tools, created a
    specialized Go tool that combines curl, sha256sum, gunzip, tar, unzip
    and automatically figures out the correct URL to download based on
    the current GOOS/GOARCH combination.

---
+++ tools/gettool/gettool.go
@@ -0,0 +1,232 @@
+// Package gettool combines and replaces curl, tar and gunzip, sha256sum and a bunch of Makefile scripts
+// to quickly download, verify and install OS-specific version of tools (typically from GitHub)
+// in a platform-agnostic manner without external tooling.
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"flag"
+	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
+	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/kopia/kopia/tools/gettool/autodownload"
+)
+
+// ToolInfo encapsulates all information required to download a tool.
+type ToolInfo struct {
+	urlTemplate         string
+	osMap               map[string]string
+	archMap             map[string]string
+	stripPathComponents int
+	darwinForceAMD64    bool
+	unsupportedArch     map[string]bool
+}
+
+func (ti ToolInfo) actualURL(version, goos, goarch string) string {
+	if ti.darwinForceAMD64 && goos == "darwin" {
+		goarch = "amd64"
+	}
+
+	if ti.unsupportedArch[goarch] {
+		return ""
+	}
+
+	u := ti.urlTemplate
+	u = strings.ReplaceAll(u, "VERSION", version)
+	u = strings.ReplaceAll(u, "GOARCH", replacementFromMap(goarch, ti.archMap))
+	u = strings.ReplaceAll(u, "GOOS", replacementFromMap(goos, ti.osMap))
+	u = strings.ReplaceAll(u, "EXT", replacementFromMap(goos, map[string]string{
+		"windows": "zip",
+		"linux":   "tar.gz",
+		"darwin":  "tar.gz",
+	}))
+
+	return u
+}
+
+var tools = map[string]ToolInfo{
+	"linter": {
+		urlTemplate: "https://github.com/golangci/golangci-lint/releases/download/vVERSION/golangci-lint-VERSION-GOOS-GOARCH.EXT",
+		archMap: map[string]string{
+			"arm": "armv6",
+		},
+		stripPathComponents: 1,
+	},
+	"hugo": {
+		urlTemplate: "https://github.com/gohugoio/hugo/releases/download/vVERSION/hugo_extended_VERSION_GOOS-GOARCH.EXT",
+		archMap: map[string]string{
+			"amd64": "64bit", "arm": "ARM", "arm64": "ARM64",
+		},
+		osMap: map[string]string{
+			"linux": "Linux", "darwin": "macOS",
+		},
+		unsupportedArch: map[string]bool{
+			"arm":   true,
+			"arm64": true,
+		},
+	},
+	"gotestsum": {
+		urlTemplate: "https://github.com/gotestyourself/gotestsum/releases/download/vVERSION/gotestsum_VERSION_GOOS_GOARCH.tar.gz",
+		archMap: map[string]string{
+			"arm": "armv6",
+		},
+	},
+	"rclone": {
+		urlTemplate: "https://github.com/rclone/rclone/releases/download/vVERSION/rclone-vVERSION-GOOS-GOARCH.zip",
+		osMap:       map[string]string{"darwin": "osx"},
+	},
+	"goreleaser": {
+		urlTemplate: "https://github.com/goreleaser/goreleaser/releases/download/VERSION/goreleaser_GOOS_GOARCH.EXT",
+		archMap: map[string]string{
+			"amd64": "x86_64",
+			"arm":   "armv6",
+		},
+		osMap: map[string]string{
+			"darwin":  "Darwin",
+			"linux":   "Linux",
+			"windows": "Windows",
+		},
+	},
+	"node": {
+		urlTemplate:         "https://nodejs.org/dist/vVERSION/node-vVERSION-GOOS-GOARCH.EXT",
+		osMap:               map[string]string{"windows": "win"},
+		archMap:             map[string]string{"arm": "armv7l", "amd64": "x64"},
+		stripPathComponents: 1,
+		darwinForceAMD64:    true,
+	},
+}
+
+var (
+	tool      = flag.String("tool", "", "Name of the tool:version")
+	outputDir = flag.String("output-dir", "", "Output directory")
+
+	testAll = flag.Bool("test-all", false, "Unpacks the package for all GOOS/ARCH combinations")
+)
+
+var buildArchitectures = []struct {
+	goos   string
+	goarch string
+}{
+	{"linux", "amd64"},
+	{"linux", "arm64"},
+	{"linux", "arm"},
+	{"darwin", "amd64"},
+	{"darwin", "arm64"},
+	{"windows", "amd64"},
+}
+
+func replacementFromMap(defaultValue string, m map[string]string) string {
+	if v, ok := m[defaultValue]; ok {
+		return v
+	}
+
+	return defaultValue
+}
+
+//go:embed checksums.txt
+var checksumsFileContents string
+
+func parseEmbeddedChecksums() map[string]string {
+	m := map[string]string{}
+
+	s := bufio.NewScanner(strings.NewReader(checksumsFileContents))
+	for s.Scan() {
+		p := strings.Split(s.Text(), ": ")
+
+		m[p[0]] = p[1]
+	}
+
+	return m
+}
+
+func main() {
+	flag.Parse()
+
+	if *outputDir == "" {
+		log.Fatalf("--output-dir must be set")
+	}
+
+	checksums := parseEmbeddedChecksums()
+
+	var errorCount int
+
+	for _, toolNameVersion := range strings.Split(*tool, ",") {
+		parts := strings.Split(toolNameVersion, ":")
+
+		// nolint:gomnd
+		if len(parts) != 2 {
+			log.Fatalf("invalid tool spec, must be tool:version[,tool:version]")
+		}
+
+		toolName := parts[0]
+		toolVersion := parts[1]
+
+		if err := downloadTool(toolName, toolVersion, checksums, &errorCount); err != nil {
+			log.Fatalf("unable to download %v version %v: %v", toolName, toolVersion, err)
+		}
+	}
+
+	// all good
+	if errorCount == 0 {
+		return
+	}
+
+	// on failure print current checksums, so they can be copy/pasted as the new baseline
+	var lines []string
+
+	for k, v := range checksums {
+		lines = append(lines, fmt.Sprintf("%v: %v", k, v))
+	}
+
+	sort.Strings(lines)
+
+	for _, l := range lines {
+		fmt.Println(l)
+	}
+
+	log.Fatalf("Error(s) encountered, see log messages above.")
+}
+
+func downloadTool(toolName, toolVersion string, checksums map[string]string, errorCount *int) error {
+	t, ok := tools[toolName]
+	if !ok {
+		return errors.Errorf("unsupported tool: %q", toolName)
+	}
+
+	if *testAll {
+		for _, ba := range buildArchitectures {
+			u := t.actualURL(toolVersion, ba.goos, ba.goarch)
+			if u == "" {
+				continue
+			}
+
+			if err := autodownload.Download(u, filepath.Join(*outputDir, ba.goos, ba.goarch), checksums, t.stripPathComponents); err != nil {
+				log.Printf("ERROR %v: %v", u, err)
+
+				*errorCount++
+			}
+		}
+
+		return nil
+	}
+
+	u := t.actualURL(toolVersion, runtime.GOOS, runtime.GOARCH)
+	if u == "" {
+		log.Fatalf("Tool '%v' is not supported on %v/%v", toolName, runtime.GOOS, runtime.GOARCH)
+	}
+
+	fmt.Printf("Downloading %v version %v from %v...\n", toolName, toolVersion, u)
+
+	if err := autodownload.Download(u, *outputDir, checksums, t.stripPathComponents); err != nil {
+		return errors.Wrap(err, "unable to download")
+	}
+
+	return nil
+}
--- tools/tools.mk
+++ tools/tools.mk
@@ -99,24 +99,21 @@ retry:=$(SELF_DIR)/retry.sh
 
 # tool versions
 GOLANGCI_LINT_VERSION=1.42.0
-NODE_VERSION=14.15.4
-HUGO_VERSION=0.82.0
-GOTESTSUM_VERSION=0.5.3
-GORELEASER_VERSION=v0.158.0
-RCLONE_VERSION=1.53.4
+NODE_VERSION=14.17.6
+HUGO_VERSION=0.87.0
+GOTESTSUM_VERSION=1.7.0
+GORELEASER_VERSION=v0.176.0
+RCLONE_VERSION=1.56.0
 
 # nodejs / npm
 node_base_dir=$(TOOLS_DIR)$(slash)node-$(NODE_VERSION)
-node_dir=$(node_base_dir)$(slash)node$(slash)bin
+ifeq ($(GOOS),windows)
+node_dir=$(node_base_dir)
+else
+node_dir=$(node_base_dir)$(slash)bin
+endif
 npm=$(node_dir)$(slash)npm$(cmd_suffix)
 npm_flags=--scripts-prepend-node-path=auto
-node_arch_name=x64
-ifeq ($(raw_arch),aarch64)
-	node_arch_name=arm64
-endif
-ifeq ($(raw_arch),armv7l)
-	node_arch_name=armv7l
-endif
 
 # put NPM in the path
 PATH:=$(node_dir)$(path_separator)$(PATH)
@@ -132,104 +129,41 @@ else ifeq ($(GOOS),freebsd)
 	@echo Use pkg to install npm
 	@exit 1
 else
-	@echo Downloading Node v$(NODE_VERSION) with NPM path $(npm)
-	$(mkdir) $(node_base_dir)$(slash)node
-
-ifeq ($(GOOS),windows)
-	curl -Ls -o $(node_base_dir).zip https://nodejs.org/dist/v$(NODE_VERSION)/node-v$(NODE_VERSION)-win-x64.zip
-	unzip -q $(node_base_dir).zip -d $(node_base_dir)
-	$(move) $(node_base_dir)\\node-v$(NODE_VERSION)-win-x64 $(node_base_dir)\\node\\bin
-else
-
-ifeq ($(GOOS),linux)
-	curl -LsS https://nodejs.org/dist/v$(NODE_VERSION)/node-v$(NODE_VERSION)-linux-$(node_arch_name).tar.gz | tar zx -C $(node_base_dir)
-else
-	curl -LsS https://nodejs.org/dist/v$(NODE_VERSION)/node-v$(NODE_VERSION)-darwin-x64.tar.gz | tar zx -C $(node_base_dir)
-endif
-	mv $(node_base_dir)/node-v$(NODE_VERSION)*/* $(node_base_dir)/node
-endif
+	go run github.com/kopia/kopia/tools/gettool -tool node:$(NODE_VERSION) --output-dir $(node_base_dir)
 endif
 
 # linter
 linter_dir=$(TOOLS_DIR)$(slash)golangci-lint-$(GOLANGCI_LINT_VERSION)
 linter=$(linter_dir)$(slash)golangci-lint$(exe_suffix)
+
 linter_flags=
-linter_arch_name=amd64
-ifeq ($(raw_arch),aarch64)
-	linter_arch_name=arm64
-endif
-
-ifeq ($(raw_arch),armv7l)
-	linter_arch_name=armv7
-endif
-
-
 ifeq ($(GOOS),windows)
 linter_flags=-D gofmt -D goimports
 endif
 
 $(linter):
-	@echo Downloading GolangCI-lint v$(GOLANGCI_LINT_VERSION) to $(linter)
-ifeq ($(GOOS),windows)
-	-$(mkdir) $(linter_dir)
-	curl -LsS -o $(linter_dir).zip https://github.com/golangci/golangci-lint/releases/download/v$(GOLANGCI_LINT_VERSION)/golangci-lint-$(GOLANGCI_LINT_VERSION)-windows-amd64.zip
-	unzip -q $(linter_dir).zip -d $(linter_dir)
-	$(move) $(linter_dir)\golangci-lint-$(GOLANGCI_LINT_VERSION)-windows-amd64\golangci-lint.exe $(linter)
-else
-	mkdir -p $(linter_dir)
-ifeq ($(GOOS),linux)
-	curl -LsS https://github.com/golangci/golangci-lint/releases/download/v$(GOLANGCI_LINT_VERSION)/golangci-lint-$(GOLANGCI_LINT_VERSION)-linux-$(linter_arch_name).tar.gz | tar zxv --strip=1 -C $(linter_dir)
-else
-	curl -LsS https://github.com/golangci/golangci-lint/releases/download/v$(GOLANGCI_LINT_VERSION)/golangci-lint-$(GOLANGCI_LINT_VERSION)-darwin-amd64.tar.gz | tar zxv --strip=1 -C $(linter_dir)
-endif
-
-endif
+	go run github.com/kopia/kopia/tools/gettool --tool linter:$(GOLANGCI_LINT_VERSION) --output-dir $(linter_dir)
 
 # hugo
-hugo_dir=$(TOOLS_DIR)$(slash)hugo-$(HUGO_VERSION)
-hugo=$(hugo_dir)$(slash)hugo$(exe_suffix)
+hugo_dir=$(TOOLS_DIR)$(slash)hugo-$(GOTESTSUM_VERSION)
+hugo=$(hugo_dir)/hugo$(exe_suffix)
 
 $(hugo):
-	@echo Downloading Hugo v$(HUGO_VERSION) to $(hugo)
-	-$(mkdir) $(TOOLS_DIR)$(slash)hugo-$(HUGO_VERSION)
-
-ifeq ($(GOOS),windows)
-	curl -LsS -o $(hugo_dir).zip https://github.com/gohugoio/hugo/releases/download/v$(HUGO_VERSION)/hugo_extended_$(HUGO_VERSION)_Windows-64bit.zip
-	unzip -q $(hugo_dir).zip -d $(hugo_dir)
-else
-
-ifeq ($(GOOS),linux)
-	curl -LsS https://github.com/gohugoio/hugo/releases/download/v$(HUGO_VERSION)/hugo_extended_$(HUGO_VERSION)_Linux-64bit.tar.gz | tar zxv -C $(hugo_dir)
-else
-	curl -LsS https://github.com/gohugoio/hugo/releases/download/v$(HUGO_VERSION)/hugo_extended_$(HUGO_VERSION)_macOS-64bit.tar.gz | tar zxv -C $(hugo_dir)
-endif
-
-endif
+	go run github.com/kopia/kopia/tools/gettool --tool hugo:$(HUGO_VERSION) --output-dir $(hugo_dir)
 
 # rclone
-rclone_os_name:=$(GOOS)
-ifeq ($(GOOS),darwin)
-rclone_os_name=osx
-endif
-
 rclone_dir=$(TOOLS_DIR)$(slash)rclone-$(RCLONE_VERSION)
 rclone=$(rclone_dir)$(slash)rclone$(exe_suffix)
-
+ 
 $(rclone):
-	@echo Downloading RCLONE_VERSION v$(RCLONE_VERSION) to $(rclone)
-	-$(mkdir) $(TOOLS_DIR)$(slash)rclone-$(RCLONE_VERSION)
-
-	curl -LsS -o $(rclone_dir).zip https://github.com/rclone/rclone/releases/download/v$(RCLONE_VERSION)/rclone-v$(RCLONE_VERSION)-$(rclone_os_name)-$(GOARCH).zip
-	unzip -j -q $(rclone_dir).zip -d $(rclone_dir)
+	go run github.com/kopia/kopia/tools/gettool --tool rclone:$(RCLONE_VERSION) --output-dir $(rclone_dir)
 
 # gotestsum
-gotestsum=$(TOOLS_DIR)/bin/gotestsum$(exe_suffix)
+gotestsum_dir=$(TOOLS_DIR)$(slash)gotestsum-$(GOTESTSUM_VERSION)
+gotestsum=$(gotestsum_dir)$(slash)gotestsum$(exe_suffix)
 
-$(gotestsum): export GO111MODULE=off
-$(gotestsum): export GOPATH=$(TOOLS_DIR)
 $(gotestsum):
-	-$(mkdir) $(TOOLS_DIR)
-	go get gotest.tools/gotestsum
+	go run github.com/kopia/kopia/tools/gettool --tool gotestsum:$(GOTESTSUM_VERSION) --output-dir $(gotestsum_dir)
 
 MINIO_MC_PATH=$(TOOLS_DIR)/bin/mc$(exe_suffix)
 
@@ -247,24 +181,8 @@ $(wwhrd):
 goreleaser_dir=$(TOOLS_DIR)$(slash)goreleaser-$(GORELEASER_VERSION)
 goreleaser=$(goreleaser_dir)$(slash)goreleaser$(exe_suffix)
 
-goreleaser_arch_name=x86_64
-ifeq ($(raw_arch),aarch64)
-	goreleaser_arch_name=arm64
-endif
-
-ifeq ($(raw_arch),armv7l)
-	goreleaser_arch_name=armv6
-endif
-
 $(goreleaser):
-	@echo Downloading GoReleaser $(GORELEASER_VERSION) to $(goreleaser)
-	-$(mkdir) $(goreleaser_dir)
-ifeq ($(GOOS),windows)
-	curl -LsS -o $(goreleaser_dir).zip https://github.com/goreleaser/goreleaser/releases/download/$(GORELEASER_VERSION)/goreleaser_Windows_x86_64.zip
-	unzip -q $(goreleaser_dir).zip -d $(goreleaser_dir)
-else
-	curl -LsS https://github.com/goreleaser/goreleaser/releases/download/$(GORELEASER_VERSION)/goreleaser_$$(uname -s)_$(goreleaser_arch_name).tar.gz | tar zx -C $(TOOLS_DIR)/goreleaser-$(GORELEASER_VERSION)
-endif
+	go run github.com/kopia/kopia/tools/gettool --tool goreleaser:$(GORELEASER_VERSION) --output-dir $(goreleaser_dir)
 
 ifeq ($(IS_PULL_REQUEST),false)
 
@@ -340,6 +258,11 @@ maybehugo=$(hugo)
 else
 maybehugo=
 endif
+
+verify-all-tool-checksums:
+	go run github.com/kopia/kopia/tools/gettool --test-all \
+	  --output-dir /tmp/all-tools \
+	  --tool node:$(NODE_VERSION),linter:$(GOLANGCI_LINT_VERSION),hugo:$(HUGO_VERSION),rclone:$(RCLONE_VERSION),gotestsum:$(GOTESTSUM_VERSION),goreleaser:$(GORELEASER_VERSION)
 
 all-tools: $(gotestsum) $(npm) $(linter) $(maybehugo)
 
