kopia/kopia

commit c3f4c4159198a2039f0cfcba8121b17179358291

refactor: move from ioutil.ReadDir to os.ReadDir (#1361)
* refactor: move from ioutil.ReadDir to os.ReadDir

This commit is an addition to PR #1360. According to
`ioutil.ReadDir` documentation (https://pkg.go.dev/io/ioutil#ReadDir),
`os.ReadDir` should be preferred as it is a more efficient and correct
implementation.

Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

* perf: optimize localfs scan performance

Reference: https://github.com/kopia/kopia/pull/1361#issuecomment-937345195
Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

cli/command_repository_status.go
@@ -3,7 +3,7 @@ package cli
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
+	"os"
 	"path/filepath"
 	"reflect"
 
@@ -120,14 +120,19 @@ func (c *commandRepositoryStatus) run(ctx context.Context, rep repo.Repository) 
 }
 
 func scanCacheDir(dirname string) (fileCount int, totalFileLength int64, err error) {
-	entries, err := ioutil.ReadDir(dirname)
+	entries, err := os.ReadDir(dirname)
 	if err != nil {
 		return 0, 0, errors.Wrap(err, "unable to read cache directory")
 	}
 
 	for _, e := range entries {
-		if e.IsDir() {
-			subdir := filepath.Join(dirname, e.Name())
+		fi, err := e.Info()
+		if err != nil {
+			return 0, 0, errors.Wrap(err, "unable to read file info")
+		}
+
+		if fi.IsDir() {
+			subdir := filepath.Join(dirname, fi.Name())
 
 			c, l, err2 := scanCacheDir(subdir)
 			if err2 != nil {
@@ -142,8 +147,8 @@ func scanCacheDir(dirname string) (fileCount int, totalFileLength int64, err err
 
 		fileCount++
 
-		totalFileLength += e.Size()
+		totalFileLength += fi.Size()
 	}
 
-	return
+	return fileCount, totalFileLength, nil
 }

internal/logfile/logfile.go
@@ -4,7 +4,6 @@ package logfile
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
 	"path/filepath"
@@ -269,31 +268,43 @@ func sweepLogDir(ctx context.Context, dirname string, maxCount int, maxAge time.
 		maxCount = math.MaxInt32
 	}
 
-	entries, err := ioutil.ReadDir(dirname)
+	entries, err := os.ReadDir(dirname)
 	if err != nil {
 		log(ctx).Errorf("unable to read log directory: %v", err)
 		return
 	}
 
-	sort.Slice(entries, func(i, j int) bool {
-		return entries[i].ModTime().After(entries[j].ModTime())
+	fileInfos := make([]os.FileInfo, 0, len(entries))
+
+	for _, e := range entries {
+		info, err2 := e.Info()
+		if err2 != nil {
+			log(ctx).Errorf("unable to read file info: %v", err2)
+			return
+		}
+
+		fileInfos = append(fileInfos, info)
+	}
+
+	sort.Slice(fileInfos, func(i, j int) bool {
+		return fileInfos[i].ModTime().After(fileInfos[j].ModTime())
 	})
 
 	cnt := 0
 
-	for _, e := range entries {
-		if !strings.HasPrefix(e.Name(), logFileNamePrefix) {
+	for _, fi := range fileInfos {
+		if !strings.HasPrefix(fi.Name(), logFileNamePrefix) {
 			continue
 		}
 
-		if !strings.HasSuffix(e.Name(), logFileNameSuffix) {
+		if !strings.HasSuffix(fi.Name(), logFileNameSuffix) {
 			continue
 		}
 
 		cnt++
 
-		if cnt > maxCount || e.ModTime().Before(timeCutoff) {
-			if err = os.Remove(filepath.Join(dirname, e.Name())); err != nil && !os.IsNotExist(err) {
+		if cnt > maxCount || fi.ModTime().Before(timeCutoff) {
+			if err = os.Remove(filepath.Join(dirname, fi.Name())); err != nil && !os.IsNotExist(err) {
 				log(ctx).Errorf("unable to remove log file: %v", err)
 			}
 		}

repo/blob/filesystem/filesystem_storage.go
@@ -6,7 +6,6 @@ import (
 	"crypto/rand"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -221,7 +220,7 @@ func (fs *fsImpl) DeleteBlobInPath(ctx context.Context, dirPath, path string) er
 
 func (fs *fsImpl) ReadDir(ctx context.Context, dirname string) ([]os.FileInfo, error) {
 	v, err := retry.WithExponentialBackoff(ctx, "ReadDir:"+dirname, func() (interface{}, error) {
-		v, err := ioutil.ReadDir(dirname)
+		v, err := os.ReadDir(dirname)
 		// nolint:wrapcheck
 		return v, err
 	}, isRetriable)
@@ -230,7 +229,19 @@ func (fs *fsImpl) ReadDir(ctx context.Context, dirname string) ([]os.FileInfo, e
 		return nil, err
 	}
 
-	return v.([]os.FileInfo), nil
+	fileInfos := make([]os.FileInfo, 0, len(v.([]os.DirEntry)))
+
+	for _, e := range v.([]os.DirEntry) {
+		fi, err := e.Info()
+		if err != nil {
+			// nolint:wrapcheck
+			return nil, err
+		}
+
+		fileInfos = append(fileInfos, fi)
+	}
+
+	return fileInfos, nil
 }
 
 // SetTime updates file modification time to the provided time.

repo/content/committed_content_index_disk_cache.go
@@ -2,7 +2,6 @@ package content
 
 import (
 	"context"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -140,7 +139,7 @@ func writeTempFileAtomic(dirname string, data []byte) (string, error) {
 func (c *diskCommittedContentIndexCache) expireUnused(ctx context.Context, used []blob.ID) error {
 	c.log.Debugf("expireUnused (except %v)", used)
 
-	entries, err := ioutil.ReadDir(c.dirname)
+	entries, err := os.ReadDir(c.dirname)
 	if err != nil {
 		return errors.Wrap(err, "can't list cache")
 	}
@@ -148,9 +147,14 @@ func (c *diskCommittedContentIndexCache) expireUnused(ctx context.Context, used 
 	remaining := map[blob.ID]os.FileInfo{}
 
 	for _, ent := range entries {
+		fi, err := ent.Info()
+		if err != nil {
+			return errors.Wrap(err, "failed to read file info")
+		}
+
 		if strings.HasSuffix(ent.Name(), simpleIndexSuffix) {
 			n := strings.TrimSuffix(ent.Name(), simpleIndexSuffix)
-			remaining[blob.ID(n)] = ent
+			remaining[blob.ID(n)] = fi
 		}
 	}
 

.golangci.yml
@@ -9,6 +9,7 @@ linters-settings:
     - ioutil.Discard # use io.Discard
     - ioutil.NopCloser # use io.NopCloser
     - ioutil.ReadAll # use io.ReadAll
+    - ioutil.ReadDir # use os.ReadDir
     - ioutil.ReadFile # use os.ReadFile
     - ioutil.TempDir # use os.MkdirTemp
     - ioutil.TempFile # use os.CreateTemp

cli/command_cache_info.go
@@ -3,7 +3,7 @@ package cli
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	"github.com/pkg/errors"
@@ -39,7 +39,7 @@ func (c *commandCacheInfo) run(ctx context.Context, rep repo.Repository) error {
 		return nil
 	}
 
-	entries, err := ioutil.ReadDir(opts.CacheDirectory)
+	entries, err := os.ReadDir(opts.CacheDirectory)
 	if err != nil {
 		return errors.Wrap(err, "unable to scan cache directory")
 	}

fs/localfs/local_fs.go
@@ -128,8 +128,8 @@ type entryWithError struct {
 	err   error
 }
 
-func toDirEntryOrNil(basename, prefix string) (fs.Entry, error) {
-	fi, err := os.Lstat(prefix + basename)
+func toDirEntryOrNil(dirEntry os.DirEntry, prefix string) (fs.Entry, error) {
+	fi, err := os.Lstat(prefix + dirEntry.Name())
 	if err != nil {
 		if os.IsNotExist(err) {
 			return nil, nil
@@ -153,7 +153,7 @@ func (fsd *filesystemDirectory) Readdir(ctx context.Context) (fs.Entries, error)
 	var entries fs.Entries
 
 	// read first batch of directory entries using Readdir() before parallelization.
-	firstBatch, firstBatchErr := f.Readdirnames(numEntriesToReadFirst)
+	firstBatch, firstBatchErr := f.ReadDir(numEntriesToReadFirst)
 	if firstBatchErr != nil && !errors.Is(firstBatchErr, io.EOF) {
 		return nil, errors.Wrap(firstBatchErr, "unable to read directory entries")
 	}
@@ -180,7 +180,7 @@ func (fsd *filesystemDirectory) Readdir(ctx context.Context) (fs.Entries, error)
 
 	// first batch was shorter than expected, perform another read to make sure we get EOF.
 	if len(firstBatch) < numEntriesToRead {
-		secondBatch, secondBatchErr := f.Readdirnames(numEntriesToRead)
+		secondBatch, secondBatchErr := f.ReadDir(numEntriesToRead)
 		if secondBatchErr != nil && !errors.Is(secondBatchErr, io.EOF) {
 			return nil, errors.Wrap(secondBatchErr, "unable to read directory entries")
 		}
@@ -210,7 +210,7 @@ func (fsd *filesystemDirectory) Readdir(ctx context.Context) (fs.Entries, error)
 
 func (fsd *filesystemDirectory) readRemainingDirEntriesInParallel(childPrefix string, entries fs.Entries, f *os.File) (fs.Entries, error) {
 	// start feeding directory entries to dirEntryCh
-	dirEntryCh := make(chan string, dirListingPrefetch)
+	dirEntryCh := make(chan os.DirEntry, dirListingPrefetch)
 
 	var readDirErr error
 
@@ -218,7 +218,7 @@ func (fsd *filesystemDirectory) readRemainingDirEntriesInParallel(childPrefix st
 		defer close(dirEntryCh)
 
 		for {
-			des, err := f.Readdirnames(numEntriesToRead)
+			des, err := f.ReadDir(numEntriesToRead)
 			for _, de := range des {
 				dirEntryCh <- de
 			}

internal/testutil/tmpdir.go
@@ -2,7 +2,6 @@ package testutil
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -102,7 +101,7 @@ func TempLogDirectory(t *testing.T) string {
 func dumpLogs(t *testing.T, dirname string) {
 	t.Helper()
 
-	entries, err := ioutil.ReadDir(dirname)
+	entries, err := os.ReadDir(dirname)
 	if err != nil {
 		t.Errorf("unable to read %v: %v", dirname, err)
 

commit b05c694a0653c2b1a864b0193579c5eac38229cd

fixed directory read race condition
This was introduced by a refactoring in #1361 - unlike
ioutil.ReadDir() which internally handles list/delete race and always
returns os.FileInfo, Info() on os.DirEntry can fail if a file
is deleted right after listing it.

Fixes #1486

cli/command_repository_status.go
@@ -127,6 +127,11 @@ func scanCacheDir(dirname string) (fileCount int, totalFileLength int64, err err
 
 	for _, e := range entries {
 		fi, err := e.Info()
+		if os.IsNotExist(err) {
+			// we lost the race, the file was deleted since it was listed.
+			continue
+		}
+
 		if err != nil {
 			return 0, 0, errors.Wrap(err, "unable to read file info")
 		}

internal/logfile/logfile.go
@@ -278,6 +278,11 @@ func sweepLogDir(ctx context.Context, dirname string, maxCount int, maxAge time.
 
 	for _, e := range entries {
 		info, err2 := e.Info()
+		if os.IsNotExist(err2) {
+			// we lost the race, the file was deleted since it was listed.
+			continue
+		}
+
 		if err2 != nil {
 			log(ctx).Errorf("unable to read file info: %v", err2)
 			return

repo/blob/filesystem/filesystem_storage.go
@@ -233,6 +233,12 @@ func (fs *fsImpl) ReadDir(ctx context.Context, dirname string) ([]os.FileInfo, e
 
 	for _, e := range v.([]os.DirEntry) {
 		fi, err := e.Info()
+
+		if os.IsNotExist(err) {
+			// we lost the race, the file was deleted since it was listed.
+			continue
+		}
+
 		if err != nil {
 			// nolint:wrapcheck
 			return nil, err

repo/content/committed_content_index_disk_cache.go
@@ -148,6 +148,11 @@ func (c *diskCommittedContentIndexCache) expireUnused(ctx context.Context, used 
 
 	for _, ent := range entries {
 		fi, err := ent.Info()
+		if os.IsNotExist(err) {
+			// we lost the race, the file was deleted since it was listed.
+			continue
+		}
+
 		if err != nil {
 			return errors.Wrap(err, "failed to read file info")
 		}