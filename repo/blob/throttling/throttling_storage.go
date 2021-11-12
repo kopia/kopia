250e0dd0abee5f95cb4edcd523d957ed8f4a04ce
Jarek Kowalski <jaak@jkowalski.net>
2021-11-11 18:09:18 -0800

    blob: added throttling wrapper (#1510)
    Throttler is only mocked for unit test purposes and not implemented yet.

---
+++ repo/blob/throttling/throttling_storage.go
@@ -0,0 +1,97 @@
+// Package throttling implements wrapper around blob.Storage that adds throttling to all calls.
+// Throttling is performed for both operations and byte counts (upload and download separately).
+package throttling
+
+import (
+	"context"
+	"time"
+
+	"github.com/kopia/kopia/repo/blob"
+)
+
+// assume we will need to download ~20 MB for blobs of unknown length, we will refund the difference
+// if we guess wrong or acquire more.
+const unknownBlobAcquireLength = 20000000
+
+// Throttler implements throttling policy by blocking before certain operations are
+// attempted to ensure we don't exceed the desired rate of operations/bytes uploaded/downloaded.
+type Throttler interface {
+	BeforeOperation(ctx context.Context, op string)
+
+	// BeforeDownload acquires the specified number of downloaded bytes
+	// possibly blocking until enough are available.
+	BeforeDownload(ctx context.Context, numBytes int64)
+
+	// BeforeUpload acquires the specified number of upload bytes
+	// possibly blocking until enough are available.
+	BeforeUpload(ctx context.Context, numBytes int64)
+
+	// ReturnUnusedDownloadBytes returns the specified number of unused download bytes.
+	ReturnUnusedDownloadBytes(ctx context.Context, numBytes int64)
+}
+
+// throttlingStorage.
+type throttlingStorage struct {
+	blob.Storage
+	throttler Throttler
+}
+
+func (s *throttlingStorage) GetBlob(ctx context.Context, id blob.ID, offset, length int64, output blob.OutputBuffer) error {
+	acquired := length
+	if acquired < 0 {
+		acquired = unknownBlobAcquireLength
+	}
+
+	s.throttler.BeforeOperation(ctx, "GetBlob")
+	s.throttler.BeforeDownload(ctx, acquired)
+
+	output.Reset()
+
+	err := s.Storage.GetBlob(ctx, id, offset, length, output)
+	downloaded := int64(output.Length())
+
+	if acquired != downloaded {
+		if downloaded > acquired {
+			// we downloaded more than initially acquired, acquire more which may pause for a bit.
+			s.throttler.BeforeDownload(ctx, downloaded-acquired)
+		} else {
+			// we downloaded less than initially acquired, release extra
+			s.throttler.ReturnUnusedDownloadBytes(ctx, acquired-downloaded)
+		}
+	}
+
+	return err // nolint:wrapcheck
+}
+
+func (s *throttlingStorage) GetMetadata(ctx context.Context, id blob.ID) (blob.Metadata, error) {
+	s.throttler.BeforeOperation(ctx, "GetMetadata")
+
+	return s.Storage.GetMetadata(ctx, id) // nolint:wrapcheck
+}
+
+func (s *throttlingStorage) ListBlobs(ctx context.Context, blobIDPrefix blob.ID, cb func(bm blob.Metadata) error) error {
+	s.throttler.BeforeOperation(ctx, "ListBlobs")
+	return s.Storage.ListBlobs(ctx, blobIDPrefix, cb) // nolint:wrapcheck
+}
+
+func (s *throttlingStorage) SetTime(ctx context.Context, id blob.ID, t time.Time) error {
+	s.throttler.BeforeOperation(ctx, "SetTime")
+	return s.Storage.SetTime(ctx, id, t) // nolint:wrapcheck
+}
+
+func (s *throttlingStorage) PutBlob(ctx context.Context, id blob.ID, data blob.Bytes) error {
+	s.throttler.BeforeOperation(ctx, "PutBlob")
+	s.throttler.BeforeUpload(ctx, int64(data.Length()))
+
+	return s.Storage.PutBlob(ctx, id, data) // nolint:wrapcheck
+}
+
+func (s *throttlingStorage) DeleteBlob(ctx context.Context, id blob.ID) error {
+	s.throttler.BeforeOperation(ctx, "DeleteBlob")
+	return s.Storage.DeleteBlob(ctx, id) // nolint:wrapcheck
+}
+
+// NewWrapper returns a Storage wrapper that adds retry loop around all operations of the underlying storage.
+func NewWrapper(wrapped blob.Storage, throttler Throttler) blob.Storage {
+	return &throttlingStorage{wrapped, throttler}
+}
---
+++ repo/blob/throttling/throttling_storage_test.go
@@ -0,0 +1,146 @@
+package throttling_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/kopia/kopia/internal/blobtesting"
+	"github.com/kopia/kopia/internal/clock"
+	"github.com/kopia/kopia/internal/gather"
+	"github.com/kopia/kopia/internal/testlogging"
+	"github.com/kopia/kopia/repo/blob"
+	"github.com/kopia/kopia/repo/blob/logging"
+	"github.com/kopia/kopia/repo/blob/throttling"
+)
+
+type mockThrottler struct {
+	activity []string
+}
+
+func (m *mockThrottler) Reset() {
+	m.activity = nil
+}
+
+func (m *mockThrottler) BeforeOperation(ctx context.Context, op string) {
+	m.activity = append(m.activity, fmt.Sprintf("BeforeOperation(%v)", op))
+}
+
+func (m *mockThrottler) BeforeDownload(ctx context.Context, numBytes int64) {
+	m.activity = append(m.activity, fmt.Sprintf("BeforeDownload(%v)", numBytes))
+}
+
+func (m *mockThrottler) BeforeUpload(ctx context.Context, numBytes int64) {
+	m.activity = append(m.activity, fmt.Sprintf("BeforeUpload(%v)", numBytes))
+}
+
+func (m *mockThrottler) ReturnUnusedDownloadBytes(ctx context.Context, numBytes int64) {
+	m.activity = append(m.activity, fmt.Sprintf("ReturnUnusedDownloadBytes(%v)", numBytes))
+}
+
+func (m *mockThrottler) Debugw(msg string, args ...interface{}) {
+	m.activity = append(m.activity, msg)
+}
+
+func (m *mockThrottler) Debugf(msg string, args ...interface{}) {}
+func (m *mockThrottler) Infof(msg string, args ...interface{})  {}
+func (m *mockThrottler) Warnf(msg string, args ...interface{})  {}
+func (m *mockThrottler) Errorf(msg string, args ...interface{}) {}
+
+func TestThrottling(t *testing.T) {
+	ctx := testlogging.Context(t)
+	m := &mockThrottler{}
+	st := blobtesting.NewMapStorage(blobtesting.DataMap{}, nil, nil)
+	l := logging.NewWrapper(st, m, "inner.")
+	wrapped := throttling.NewWrapper(l, m)
+
+	var tmp gather.WriteBuffer
+
+	m.Reset()
+	require.ErrorIs(t, wrapped.GetBlob(ctx, "blob1", 0, -1, &tmp), blob.ErrBlobNotFound)
+	require.Equal(t, []string{
+		"BeforeOperation(GetBlob)",
+		"BeforeDownload(20000000)",
+		"inner.GetBlob",
+		"ReturnUnusedDownloadBytes(20000000)",
+	}, m.activity)
+
+	// upload blob of 7 bytes
+	m.Reset()
+	require.NoError(t, wrapped.PutBlob(ctx, "blob1", gather.FromSlice([]byte{1, 2, 3, 4, 5, 6, 7})))
+	require.Equal(t, []string{
+		"BeforeOperation(PutBlob)",
+		"BeforeUpload(7)",
+		"inner.PutBlob",
+	}, m.activity)
+
+	// upload another blob of 30MB
+	m.Reset()
+	require.NoError(t, wrapped.PutBlob(ctx, "blob2", gather.FromSlice(bytes.Repeat([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 3000000))))
+	require.Equal(t, []string{
+		"BeforeOperation(PutBlob)",
+		"BeforeUpload(30000000)",
+		"inner.PutBlob",
+	}, m.activity)
+
+	m.Reset()
+	require.NoError(t, wrapped.GetBlob(ctx, "blob1", 0, -1, &tmp))
+	require.Equal(t, []string{
+		"BeforeOperation(GetBlob)",
+		"BeforeDownload(20000000)", // length is unknown, we assume 20MB
+		"inner.GetBlob",
+		"ReturnUnusedDownloadBytes(19999993)", // refund all but 7 bytes
+	}, m.activity)
+
+	m.Reset()
+	require.NoError(t, wrapped.GetBlob(ctx, "blob2", 0, -1, &tmp))
+	require.Equal(t, []string{
+		"BeforeOperation(GetBlob)",
+		"BeforeDownload(20000000)", // length is unknown, we assume 20MB
+		"inner.GetBlob",
+		"BeforeDownload(10000000)", // we downloaded more than expected, acquire more
+	}, m.activity)
+
+	m.Reset()
+	require.NoError(t, wrapped.GetBlob(ctx, "blob1", 3, 4, &tmp))
+	require.Equal(t, []string{
+		"BeforeOperation(GetBlob)",
+		"BeforeDownload(4)",
+		"inner.GetBlob",
+	}, m.activity)
+
+	m.Reset()
+
+	_, err := wrapped.GetMetadata(ctx, "blob1")
+	require.NoError(t, err, blob.ErrBlobNotFound)
+	require.Equal(t, []string{
+		"BeforeOperation(GetMetadata)",
+		"inner.GetMetadata",
+	}, m.activity)
+
+	m.Reset()
+	require.NoError(t, wrapped.SetTime(ctx, "blob1", clock.Now()))
+	require.Equal(t, []string{
+		"BeforeOperation(SetTime)",
+		"inner.SetTime",
+	}, m.activity)
+
+	m.Reset()
+	require.NoError(t, wrapped.DeleteBlob(ctx, "blob1"))
+	require.Equal(t, []string{
+		"BeforeOperation(DeleteBlob)",
+		"inner.DeleteBlob",
+	}, m.activity)
+
+	m.Reset()
+
+	_, err = blob.ListAllBlobs(ctx, wrapped, "")
+	require.NoError(t, err)
+	require.Equal(t, []string{
+		"BeforeOperation(ListBlobs)",
+		"inner.ListBlobs",
+	}, m.activity)
+}
