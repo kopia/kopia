fad50dbe0e3695b70d36ea4a3627289260cc89b2
daniel-eys <47071433+daniel-eys@users.noreply.github.com>
2021-12-12 18:46:17 +0100

    localfs: adjust build flags to support more archs (#1183)

--- fs/localfs/local_fs_64bit.go
+++ fs/localfs/local_fs_64bit.go
@@ -1,8 +1,8 @@
-//go:build !windows && !openbsd && !darwin && (amd64 || arm64 || arm)
+//go:build !windows && !openbsd && !darwin && (amd64 || arm64 || arm || ppc64 || ppc64le || s390x || 386 || riscv64)
 // +build !windows
 // +build !openbsd
 // +build !darwin
-// +build amd64 arm64 arm
+// +build amd64 arm64 arm ppc64 ppc64le s390x 386 riscv64
 
 package localfs
 
