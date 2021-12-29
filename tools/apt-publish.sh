a223fb8bdc0fb0e1daf10f5c9fead80f427f8c68
Jarek Kowalski <jaak@jkowalski.net>
2021-12-29 10:43:51 -0800

    ci: do not upload armv7 deb packages (kopia-ui) into the apt repository bucket

--- tools/apt-publish.sh
+++ tools/apt-publish.sh
@@ -81,6 +81,12 @@ for f in $deb_files; do
       dists="unstable"
     fi
 
+
+    if [[ "$architectures" != *"$arch"* ]]; then
+      echo "ignoring unsupported architecture: $f arch: $arch dists: $dists"
+      continue
+    fi
+
     echo "$f $arch $dists"
 
     bn=$(basename $f)
@@ -112,7 +118,7 @@ echo Generating Release files
 # generate Release/InRelease/Release.gpg files for all distributions
 for d in $distributions; do
   echo Generating $WORK_DIR/dists/$d/Release...
-  docker run -i --rm -v $WORK_DIR/dists/$d:/root marshallofsound/apt-ftparchive release \
+  apt-ftparchive release \
     -o APT::FTPArchive::Release::Architectures="$architectures" \
     -o APT::FTPArchive::Release::Codename="$d" \
     -o APT::FTPArchive::Release::Suite="$d" \
