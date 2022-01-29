kopia/kopia

commit 7ca8b85a5758c796d85e74e33ad8f062343a0392

feat(providers): expand PutBlob API to allow for idempotent puts (#1654)
* Add a new PutBlob option and blob error type

When `DoNotRecreate` is set as true, the blob put operation should
only succeed if no blob with the given blob ID already exists.
Othwerwise, `ErrBlobAlreadyExists` is returned.

* Validate default storage providers' support

By default, storage providers should not support idempotent creates.
This commit adds error handling to exit early if `DoNotRecreate` is
set to true. The commit also verifies this behavior in the provider
validation test.

* Implement support for new option in GCS storage

* Push PutBlob option handling down to Impl

When PutBlob options were introduced, error handling logic for them
was implemented for the Sharded storage interface. However, the
behavior of different providers that implement Sharded can be
different, so it's better to push the options down to be processed in
the provider implementations.

* Introduce new error type for unsupported put opts

To unify error handling code and make it more maintainable, introduce
a new error type `blob.ErrUnsupportedPutBlobOption`, which is to be
returned whenever a storage provider implementation is given put
options it does not support.

repo/blob/retrying/retrying_storage.go
@@ -71,6 +71,9 @@ func isRetriable(err error) bool {
 	case errors.Is(err, blob.ErrSetTimeUnsupported):
 		return false
 
+	case errors.Is(err, blob.ErrBlobAlreadyExists):
+		return false
+
 	default:
 		return true
 	}

cli/command_repository_validate_provider.go
@@ -23,6 +23,7 @@ func (c *commandRepositoryValidateProvider) setup(svc advancedAppServices, paren
 	cmd.Flag("put-blob-workers", "Number of PutBlob workers").IntVar(&c.opt.NumPutBlobWorkers)
 	cmd.Flag("get-blob-workers", "Number of GetBlob workers").IntVar(&c.opt.NumGetBlobWorkers)
 	cmd.Flag("get-metadata-workers", "Number of GetMetadata workers").IntVar(&c.opt.NumGetMetadataWorkers)
+	cmd.Flag("support-idempotent-creates", "Whether store supports idempotent blob creates").BoolVar(&c.opt.SupportIdempotentCreates)
 	c.out.setup(svc)
 
 	cmd.Action(c.out.svc.directRepositoryWriteAction(c.run))

internal/blobtesting/map.go
@@ -76,8 +76,11 @@ func (s *mapStorage) GetMetadata(ctx context.Context, id blob.ID) (blob.Metadata
 }
 
 func (s *mapStorage) PutBlob(ctx context.Context, id blob.ID, data blob.Bytes, opts blob.PutOptions) error {
-	if opts.HasRetentionOptions() {
-		return errors.New("setting blob-retention is not supported")
+	switch {
+	case opts.HasRetentionOptions():
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "blob-retention")
+	case opts.DoNotRecreate:
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "do-not-recreate")
 	}
 
 	s.mutex.Lock()

internal/blobtesting/verify.go
@@ -108,14 +108,21 @@ func VerifyStorage(ctx context.Context, t *testing.T, r blob.Storage, opts blob.
 	})
 
 	t.Run("OverwriteBlobs", func(t *testing.T) {
+		newContents := []byte{99}
+
 		for _, b := range blocks {
 			b := b
 
 			t.Run(string(b.blk), func(t *testing.T) {
 				t.Parallel()
-
-				require.NoErrorf(t, r.PutBlob(ctx, b.blk, gather.FromSlice(b.contents), blob.PutOptions{}), "can't put blob: %v", b)
-				AssertGetBlob(ctx, t, r, b.blk, b.contents)
+				err := r.PutBlob(ctx, b.blk, gather.FromSlice(newContents), opts)
+				if opts.DoNotRecreate {
+					require.ErrorIsf(t, err, blob.ErrBlobAlreadyExists, "overwrote blob: %v", b)
+					AssertGetBlob(ctx, t, r, b.blk, b.contents)
+				} else {
+					require.NoErrorf(t, err, "can't put blob: %v", b)
+					AssertGetBlob(ctx, t, r, b.blk, newContents)
+				}
 			})
 		}
 	})
@@ -209,11 +216,12 @@ func AssertConnectionInfoRoundTrips(ctx context.Context, t *testing.T, s blob.St
 // TestValidationOptions is the set of options used when running providing validation from tests.
 // nolint:gomnd
 var TestValidationOptions = providervalidation.Options{
-	MaxClockDrift:           3 * time.Minute,
-	ConcurrencyTestDuration: 15 * time.Second,
-	NumPutBlobWorkers:       3,
-	NumGetBlobWorkers:       3,
-	NumGetMetadataWorkers:   3,
-	NumListBlobsWorkers:     3,
-	MaxBlobLength:           10e6,
+	MaxClockDrift:            3 * time.Minute,
+	ConcurrencyTestDuration:  15 * time.Second,
+	NumPutBlobWorkers:        3,
+	NumGetBlobWorkers:        3,
+	NumGetMetadataWorkers:    3,
+	NumListBlobsWorkers:      3,
+	MaxBlobLength:            10e6,
+	SupportIdempotentCreates: false,
 }

internal/providervalidation/providervalidation.go
@@ -31,18 +31,21 @@ type Options struct {
 	NumGetMetadataWorkers int
 	NumListBlobsWorkers   int
 	MaxBlobLength         int
+
+	SupportIdempotentCreates bool
 }
 
 // DefaultOptions is the default set of options.
 // nolint:gomnd,gochecknoglobals
 var DefaultOptions = Options{
-	MaxClockDrift:           3 * time.Minute,
-	ConcurrencyTestDuration: 30 * time.Second,
-	NumPutBlobWorkers:       3,
-	NumGetBlobWorkers:       3,
-	NumGetMetadataWorkers:   3,
-	NumListBlobsWorkers:     3,
-	MaxBlobLength:           10e6,
+	MaxClockDrift:            3 * time.Minute,
+	ConcurrencyTestDuration:  30 * time.Second,
+	NumPutBlobWorkers:        3,
+	NumGetBlobWorkers:        3,
+	NumGetMetadataWorkers:    3,
+	NumListBlobsWorkers:      3,
+	MaxBlobLength:            10e6,
+	SupportIdempotentCreates: false,
 }
 
 const blobIDLength = 16
@@ -51,7 +54,7 @@ var log = logging.Module("providervalidation")
 
 // ValidateProvider runs a series of tests against provided storage to validate that
 // it can be used with Kopia.
-// nolint:gomnd,funlen,gocyclo
+// nolint:gomnd,funlen,gocyclo,cyclop
 func ValidateProvider(ctx context.Context, st blob.Storage, opt Options) error {
 	if os.Getenv("KOPIA_SKIP_PROVIDER_VALIDATION") != "" {
 		return nil
@@ -181,6 +184,15 @@ func ValidateProvider(ctx context.Context, st blob.Storage, opt Options) error {
 
 	if err := ct.run(ctx); err != nil {
 		return errors.Wrap(err, "error validating concurrency")
+	}
+
+	log(ctx).Infof("Validating blob idempontent creates...")
+
+	if !opt.SupportIdempotentCreates {
+		err := st.PutBlob(ctx, "dummy_id", gather.FromSlice([]byte{99}), blob.PutOptions{DoNotRecreate: true})
+		if !errors.As(err, &blob.ErrUnsupportedPutBlobOption) {
+			return errors.Errorf("expected error 'unsupported put-blob option', but got %v", err)
+		}
 	}
 
 	log(ctx).Infof("All good.")

repo/blob/azure/azure_storage.go
@@ -111,8 +111,11 @@ func translateError(err error) error {
 }
 
 func (az *azStorage) PutBlob(ctx context.Context, b blob.ID, data blob.Bytes, opts blob.PutOptions) error {
-	if opts.HasRetentionOptions() {
-		return errors.New("setting blob-retention is not supported")
+	switch {
+	case opts.HasRetentionOptions():
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "blob-retention")
+	case opts.DoNotRecreate:
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "do-not-recreate")
 	}
 
 	ctx, cancel := context.WithCancel(ctx)

repo/blob/filesystem/filesystem_storage.go
@@ -150,8 +150,15 @@ func (fs *fsImpl) GetMetadataFromPath(ctx context.Context, dirPath, path string)
 	return v.(blob.Metadata), nil //nolint:forcetypeassert
 }
 
+//nolint:wrapcheck,gocyclo
 func (fs *fsImpl) PutBlobInPath(ctx context.Context, dirPath, path string, data blob.Bytes, opts blob.PutOptions) error {
-	// nolint:wrapcheck
+	switch {
+	case opts.HasRetentionOptions():
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "blob-retention")
+	case opts.DoNotRecreate:
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "do-not-recreate")
+	}
+
 	return retry.WithExponentialBackoffNoValue(ctx, "PutBlobInPath:"+path, func() error {
 		randSuffix := make([]byte, tempFileRandomSuffixLen)
 		if _, err := rand.Read(randSuffix); err != nil {

repo/blob/gcs/gcs_storage.go
@@ -84,8 +84,11 @@ func translateError(err error) error {
 	var ae *googleapi.Error
 
 	if errors.As(err, &ae) {
-		if ae.Code == http.StatusRequestedRangeNotSatisfiable {
+		switch ae.Code {
+		case http.StatusRequestedRangeNotSatisfiable:
 			return blob.ErrInvalidRange
+		case http.StatusPreconditionFailed:
+			return blob.ErrBlobAlreadyExists
 		}
 	}
 
@@ -100,13 +103,17 @@ func translateError(err error) error {
 }
 
 func (gcs *gcsStorage) PutBlob(ctx context.Context, b blob.ID, data blob.Bytes, opts blob.PutOptions) error {
-	if opts.HasRetentionOptions() {
-		return errors.New("setting blob-retention is not supported")
+	switch {
+	case opts.HasRetentionOptions():
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "blob-retention")
+	case opts.DoNotRecreate:
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "do-not-recreate")
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	obj := gcs.bucket.Object(gcs.getObjectNameString(b))
+	conds := gcsclient.Conditions{DoesNotExist: opts.DoNotRecreate}
+	obj := gcs.bucket.Object(gcs.getObjectNameString(b)).If(conds)
 	writer := obj.NewWriter(ctx)
 	writer.ChunkSize = writerChunkSize
 	writer.ContentType = "application/x-kopia"

commit 5749a4ee5ba9869b689460bc6499b22ac5237708

fix(provider): fixed b2 provider failure
There was a small regression in #1654 - only in negative tests.

Also removed unnecessary retries on ErrUnsupportedPutBlobOption
+ switched provider test to run once per day, instead of 12 times/day.

repo/blob/b2/b2_storage.go
@@ -146,6 +146,13 @@ func translateError(err error) error {
 }
 
 func (s *b2Storage) PutBlob(ctx context.Context, id blob.ID, data blob.Bytes, opts blob.PutOptions) error {
+	switch {
+	case opts.HasRetentionOptions():
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "blob-retention")
+	case opts.DoNotRecreate:
+		return errors.Wrap(blob.ErrUnsupportedPutBlobOption, "do-not-recreate")
+	}
+
 	fileName := s.getObjectNameString(id)
 
 	// Backblaze always expects Content-Length to be set, even in http.Request ContentLength==0

repo/blob/retrying/retrying_storage.go
@@ -71,6 +71,9 @@ func isRetriable(err error) bool {
 	case errors.Is(err, blob.ErrSetTimeUnsupported):
 		return false
 
+	case errors.Is(err, blob.ErrUnsupportedPutBlobOption):
+		return false
+
 	case errors.Is(err, blob.ErrBlobAlreadyExists):
 		return false
 

.github/workflows/provider-tests.yml
@@ -5,8 +5,8 @@ on:
     tags:
       - v*
   schedule:
-    # run every 2 hours
-    - cron:  '19 */2 * * *'
+    # run once per day
+    - cron:  '19 7 * * *'
 jobs:
   endurance-test:
     name: Provider Test