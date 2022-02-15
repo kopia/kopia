ed327c869f4a42d5241160a656f744f007824cd5
Shikhar Mall <small@kopia.io>
2022-02-15 13:14:25 -0800

    rollback from multiple owners

--- repo/upgrade_lock_test.go
+++ repo/upgrade_lock_test.go
@@ -2,11 +2,14 @@ package repo_test
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/kopia/kopia/internal/repotesting"
+	"github.com/kopia/kopia/repo"
+	"github.com/kopia/kopia/repo/blob"
 	"github.com/kopia/kopia/repo/content"
 )
 
@@ -117,5 +120,62 @@ func TestFormatUpgradeRollback(t *testing.T) {
 	env.MustReopen(t)
 
 	// verify that commit after rollback fails
+	require.EqualError(t, env.RepositoryWriter.CommitUpgrade(ctx), "no upgrade in progress")
+}
+
+func TestFormatMultipleLocksRollback(t *testing.T) {
+	ctx, env := repotesting.NewEnvironment(t, content.FormatVersion1)
+	formatBlockCacheDuration := env.Repository.ClientOptions().FormatBlobCacheDuration
+
+	l := &content.UpgradeLock{
+		OwnerID:                "upgrade-owner",
+		CreationTime:           env.Repository.Time(),
+		AdvanceNoticeDuration:  0,
+		IODrainTimeout:         formatBlockCacheDuration * 2,
+		StatusPollInterval:     formatBlockCacheDuration,
+		Message:                "upgrading from format version 2 -> 3",
+		MaxPermittedClockDrift: formatBlockCacheDuration / 3,
+		OldFormatVersion:       env.RepositoryWriter.ContentManager().ContentFormat().MutableParameters.Version,
+	}
+
+	secondWriter := env.MustOpenAnother(t)
+
+	// first lock by primary creator
+	_, err := env.RepositoryWriter.SetUpgradeLockIntent(ctx, *l)
+	require.NoError(t, err)
+
+	// second lock from a random owner
+	secondL := l.Clone()
+	secondL.OwnerID = "another-upgrade-owner"
+	_, err = secondWriter.(repo.DirectRepositoryWriter).SetUpgradeLockIntent(ctx, *secondL)
+	require.NoError(t, err)
+
+	// verify that we have two repository backups, the second one will contain
+	// the first owner's lock
+	{
+		var backups []string
+		require.NoError(t, env.RootStorage().ListBlobs(ctx, repo.FormatBlobBackupIDPrefix, func(bm blob.Metadata) error {
+			backups = append(backups, string(bm.BlobID))
+			return nil
+		}))
+		sort.Strings(backups)
+		require.Equal(t, []string{string(repo.FormatBlobBackupID(*secondL)), string(repo.FormatBlobBackupID(*l))},
+			backups, "invalid backups list")
+	}
+
+	require.NoError(t, env.RepositoryWriter.RollbackUpgrade(ctx))
+
+	// verify that we have no repository backups pending
+	require.NoError(t, env.RootStorage().ListBlobs(ctx, repo.FormatBlobBackupIDPrefix, func(bm blob.Metadata) error {
+		t.Fatalf("found unexpected backup: %s", bm.BlobID)
+		return nil
+	}))
+
+	// reopen the repo because we still have the lock in-memory
+	env.MustReopen(t)
+
+	// verify that commit after rollback fails, this ensures that the correct
+	// backup got restored because if the second backup was restored then we'd
+	// still get a lock to be committed without any error
 	require.EqualError(t, env.RepositoryWriter.CommitUpgrade(ctx), "no upgrade in progress")
 }
