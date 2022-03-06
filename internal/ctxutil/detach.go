b8d8854ad1071e68e1bab39cadfd38402719df30
Jarek Kowalski <jaak@jkowalski.net>
2022-03-06 15:45:20 -0800

    refactor(repository): better context cancelation handling
    Instead of ignoring context cancelation in Open(), ensure we don't
    spawn goroutines that might be canceled.

--- internal/ctxutil/detach.go
+++ internal/ctxutil/detach.go
@@ -16,6 +16,13 @@ func Detach(ctx context.Context) context.Context {
 	return detachedContext{context.Background(), ctx}
 }
 
+// GoDetached invokes the provided function in a goroutine where the context is detached.
+func GoDetached(ctx context.Context, fun func(ctx context.Context)) {
+	go func() {
+		fun(Detach(ctx))
+	}()
+}
+
 func (d detachedContext) Value(key interface{}) interface{} {
 	return d.wrapped.Value(key)
 }
