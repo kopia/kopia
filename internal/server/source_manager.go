kopia/kopia

commit 9cba4a97bed7c5375bfb6eeb429e3ab512a359e8

refactor(repository): major server code refactoring (#1837)
This removes big shared lock held for for the duration of each request
and replaces it with trivially short lock to capture the current
state of the server/repository before passing it to handlers.

Handlers are now limited to only accessing a small subset of Server
functionality to be able to better reason about them.

internal/server/source_manager.go
@@ -38,31 +38,42 @@ const (
 type sourceManager struct {
 	snapshotfs.NullUploadProgress
 
-	server           *Server
+	server *Server
+
 	src              snapshot.SourceInfo
+	rep              repo.Repository
 	closed           chan struct{}
 	snapshotRequests chan struct{}
 	refreshRequested chan struct{} // tickled externally to trigger refresh
 	wg               sync.WaitGroup
 
-	mu                                 sync.RWMutex
-	uploader                           *snapshotfs.Uploader
-	pol                                policy.SchedulingPolicy
-	state                              string
-	nextSnapshotTime                   *time.Time
-	lastSnapshot                       *snapshot.Manifest
-	lastCompleteSnapshot               *snapshot.Manifest
+	sourceMutex sync.RWMutex
+	// +checklocks:sourceMutex
+	uploader *snapshotfs.Uploader
+	// +checklocks:sourceMutex
+	pol policy.SchedulingPolicy
+	// +checklocks:sourceMutex
+	state string
+	// +checklocks:sourceMutex
+	nextSnapshotTime *time.Time
+	// +checklocks:sourceMutex
+	lastSnapshot *snapshot.Manifest
+	// +checklocks:sourceMutex
+	lastCompleteSnapshot *snapshot.Manifest
+	// +checklocks:sourceMutex
 	manifestsSinceLastCompleteSnapshot []*snapshot.Manifest
-	paused                             bool
-	isReadOnly                         bool
-
-	progress    *snapshotfs.CountingUploadProgress
+	// +checklocks:sourceMutex
+	paused bool
+	// +checklocks:sourceMutex
 	currentTask string
+
+	isReadOnly bool
+	progress   *snapshotfs.CountingUploadProgress
 }
 
 func (s *sourceManager) Status() *serverapi.SourceStatus {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
+	s.sourceMutex.RLock()
+	defer s.sourceMutex.RUnlock()
 
 	st := &serverapi.SourceStatus{
 		Source:           s.src,
@@ -84,26 +95,59 @@ func (s *sourceManager) Status() *serverapi.SourceStatus {
 }
 
 func (s *sourceManager) setStatus(stat string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.sourceMutex.Lock()
+	defer s.sourceMutex.Unlock()
+
 	s.state = stat
 }
 
+func (s *sourceManager) isPaused() bool {
+	s.sourceMutex.RLock()
+	defer s.sourceMutex.RUnlock()
+
+	return s.paused
+}
+
+func (s *sourceManager) getNextSnapshotTime() (time.Time, bool) {
+	s.sourceMutex.RLock()
+	defer s.sourceMutex.RUnlock()
+
+	if s.nextSnapshotTime == nil {
+		return time.Time{}, false
+	}
+
+	return *s.nextSnapshotTime, true
+}
+
+func (s *sourceManager) setCurrentTaskID(taskID string) {
+	s.sourceMutex.Lock()
+	defer s.sourceMutex.Unlock()
+
+	s.currentTask = taskID
+}
+
+func (s *sourceManager) setNextSnapshotTime(t time.Time) {
+	s.sourceMutex.Lock()
+	defer s.sourceMutex.Unlock()
+
+	s.nextSnapshotTime = &t
+}
+
 func (s *sourceManager) currentUploader() *snapshotfs.Uploader {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.sourceMutex.RLock()
+	defer s.sourceMutex.RUnlock()
 
 	return s.uploader
 }
 
 func (s *sourceManager) setUploader(u *snapshotfs.Uploader) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.sourceMutex.Lock()
+	defer s.sourceMutex.Unlock()
 
 	s.uploader = u
 }
 
-func (s *sourceManager) run(ctx context.Context) {
+func (s *sourceManager) start(ctx context.Context, rep repo.Repository) {
 	// make sure we run in a detached context, which ignores outside cancelation and deadline.
 	ctx = ctxutil.Detach(ctx)
 
@@ -113,12 +157,14 @@ func (s *sourceManager) run(ctx context.Context) {
 	s.wg.Add(1)
 	defer s.wg.Done()
 
-	if s.server.rep.ClientOptions().Hostname == s.src.Host && !s.server.rep.ClientOptions().ReadOnly {
+	if rep.ClientOptions().Hostname == s.src.Host && !rep.ClientOptions().ReadOnly {
 		log(ctx).Debugf("starting local source manager for %v", s.src)
-		s.runLocal(ctx)
+
+		go s.runLocal(ctx)
 	} else {
 		log(ctx).Debugf("starting read-only source manager for %v", s.src)
-		s.runReadOnly(ctx)
+
+		go s.runReadOnly(ctx)
 	}
 }
 
@@ -128,13 +174,14 @@ func (s *sourceManager) runLocal(ctx context.Context) {
 	for {
 		var waitTime time.Duration
 
-		if s.nextSnapshotTime != nil {
-			waitTime = s.nextSnapshotTime.Sub(clock.Now())
+		nst, ok := s.getNextSnapshotTime()
+		if ok {
+			waitTime = nst.Sub(clock.Now())
 		} else {
 			waitTime = oneDay
 		}
 
-		if s.paused {
+		if s.isPaused() {
 			s.setStatus("PAUSED")
 		} else {
 			s.setStatus("IDLE")
@@ -145,8 +192,7 @@ func (s *sourceManager) runLocal(ctx context.Context) {
 			return
 
 		case <-s.snapshotRequests:
-			nt := clock.Now()
-			s.nextSnapshotTime = &nt
+			s.setNextSnapshotTime(clock.Now())
 
 			continue
 
@@ -172,12 +218,11 @@ func (s *sourceManager) runLocal(ctx context.Context) {
 }
 
 func (s *sourceManager) backoffBeforeNextSnapshot() {
-	if s.nextSnapshotTime == nil {
+	if _, ok := s.getNextSnapshotTime(); !ok {
 		return
 	}
 
-	t := clock.Now().Add(failedSnapshotRetryInterval)
-	s.nextSnapshotTime = &t
+	s.setNextSnapshotTime(clock.Now().Add(failedSnapshotRetryInterval))
 }
 
 func (s *sourceManager) runReadOnly(ctx context.Context) {
@@ -223,9 +268,9 @@ func (s *sourceManager) cancel(ctx context.Context) serverapi.SourceActionRespon
 func (s *sourceManager) pause(ctx context.Context) serverapi.SourceActionResponse {
 	log(ctx).Debugw("pause triggered via API", "source", s.src)
 
-	s.mu.Lock()
+	s.sourceMutex.Lock()
 	s.paused = true
-	s.mu.Unlock()
+	s.sourceMutex.Unlock()
 
 	if u := s.currentUploader(); u != nil {
 		log(ctx).Infof("canceling current upload")
@@ -243,9 +288,9 @@ func (s *sourceManager) pause(ctx context.Context) serverapi.SourceActionRespons
 func (s *sourceManager) resume(ctx context.Context) serverapi.SourceActionResponse {
 	log(ctx).Debugw("resume triggered via API", "source", s.src)
 
-	s.mu.Lock()
+	s.sourceMutex.Lock()
 	s.paused = false
-	s.mu.Unlock()
+	s.sourceMutex.Unlock()
 
 	select {
 	case s.refreshRequested <- struct{}{}:
@@ -274,7 +319,10 @@ func (s *sourceManager) waitUntilStopped(ctx context.Context) {
 func (s *sourceManager) snapshot(ctx context.Context) error {
 	s.setStatus("PENDING")
 
-	s.server.beginUpload(ctx, s.src)
+	if !s.server.beginUpload(ctx, s.src) {
+		return nil
+	}
+
 	defer s.server.endUpload(ctx, s.src)
 
 	// nolint:wrapcheck
@@ -287,9 +335,8 @@ func (s *sourceManager) snapshot(ctx context.Context) error {
 func (s *sourceManager) snapshotInternal(ctx context.Context, ctrl uitask.Controller) error {
 	s.setStatus("UPLOADING")
 
-	s.currentTask = ctrl.CurrentTaskID()
-
-	defer func() { s.currentTask = "" }()
+	s.setCurrentTaskID(ctrl.CurrentTaskID())
+	defer s.setCurrentTaskID("")
 
 	// check if we got closed while waiting on semaphore
 	select {
@@ -307,8 +354,12 @@ func (s *sourceManager) snapshotInternal(ctx context.Context, ctrl uitask.Contro
 
 	onUpload := func(int64) {}
 
+	s.sourceMutex.RLock()
+	manifestsSinceLastCompleteSnapshot := append([]*snapshot.Manifest(nil), s.manifestsSinceLastCompleteSnapshot...)
+	s.sourceMutex.RUnlock()
+
 	// nolint:wrapcheck
-	return repo.WriteSession(ctx, s.server.rep, repo.WriteSessionOptions{
+	return repo.WriteSession(ctx, s.rep, repo.WriteSessionOptions{
 		Purpose: "Source Manager Uploader",
 		OnUpload: func(numBytes int64) {
 			// extra indirection to allow changing onUpload function later
@@ -336,7 +387,7 @@ func (s *sourceManager) snapshotInternal(ctx context.Context, ctrl uitask.Contro
 		log(ctx).Debugf("starting upload of %v", s.src)
 		s.setUploader(u)
 
-		manifest, err := u.Upload(ctx, localEntry, policyTree, s.src, s.manifestsSinceLastCompleteSnapshot...)
+		manifest, err := u.Upload(ctx, localEntry, policyTree, s.src, manifestsSinceLastCompleteSnapshot...)
 		prog.report(true)
 
 		s.setUploader(nil)
@@ -359,7 +410,8 @@ func (s *sourceManager) snapshotInternal(ctx context.Context, ctrl uitask.Contro
 	})
 }
 
-func (s *sourceManager) findClosestNextSnapshotTime() *time.Time {
+// +checklocksread:s.sourceMutex
+func (s *sourceManager) findClosestNextSnapshotTimeReadLocked() *time.Time {
 	var lastCompleteSnapshotTime time.Time
 	if lcs := s.lastCompleteSnapshot; lcs != nil {
 		lastCompleteSnapshotTime = lcs.StartTime
@@ -377,20 +429,20 @@ func (s *sourceManager) refreshStatus(ctx context.Context) {
 	ctx, cancel := context.WithTimeout(ctx, refreshTimeout)
 	defer cancel()
 
-	pol, _, _, err := policy.GetEffectivePolicy(ctx, s.server.rep, s.src)
+	pol, _, _, err := policy.GetEffectivePolicy(ctx, s.rep, s.src)
 	if err != nil {
 		s.setStatus("FAILED")
 		return
 	}
 
-	snapshots, err := snapshot.ListSnapshots(ctx, s.server.rep, s.src)
+	snapshots, err := snapshot.ListSnapshots(ctx, s.rep, s.src)
 	if err != nil {
 		s.setStatus("FAILED")
 		return
 	}
 
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.sourceMutex.Lock()
+	defer s.sourceMutex.Unlock()
 
 	s.pol = pol.SchedulingPolicy
 	s.manifestsSinceLastCompleteSnapshot = nil
@@ -415,11 +467,12 @@ func (s *sourceManager) refreshStatus(ctx context.Context) {
 	if s.paused {
 		s.nextSnapshotTime = nil
 	} else {
-		s.nextSnapshotTime = s.findClosestNextSnapshotTime()
+		s.nextSnapshotTime = s.findClosestNextSnapshotTimeReadLocked()
 	}
 }
 
 type uitaskProgress struct {
+	// +checkatomic
 	nextReportTimeNanos int64 // must be aligned due to atomic access
 	p                   *snapshotfs.CountingUploadProgress
 	ctrl                uitask.Controller
@@ -520,9 +573,10 @@ func (t *uitaskProgress) EstimatedDataSize(fileCount int, totalBytes int64) {
 	t.maybeReport()
 }
 
-func newSourceManager(src snapshot.SourceInfo, server *Server) *sourceManager {
+func newSourceManager(src snapshot.SourceInfo, server *Server, rep repo.Repository) *sourceManager {
 	m := &sourceManager{
 		src:              src,
+		rep:              rep,
 		server:           server,
 		state:            "UNKNOWN",
 		closed:           make(chan struct{}),

internal/server/api_cli.go
@@ -2,21 +2,20 @@ package server
 
 import (
 	"context"
-	"net/http"
 	"os"
 	"strings"
 
 	"github.com/kopia/kopia/internal/serverapi"
 )
 
-func (s *Server) handleCLIInfo(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
+func handleCLIInfo(ctx context.Context, rc requestContext) (interface{}, *apiError) {
 	executable, err := os.Executable()
 	if err != nil {
 		executable = "kopia"
 	}
 
 	return &serverapi.CLIInfo{
-		Executable: maybeQuote(executable) + " --config-file=" + maybeQuote(s.options.ConfigFile) + "",
+		Executable: maybeQuote(executable) + " --config-file=" + maybeQuote(rc.srv.getOptions().ConfigFile) + "",
 	}, nil
 }
 

internal/server/api_content.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"net/http"
 	"strconv"
 	"strings"
-
-	"github.com/gorilla/mux"
 
 	"github.com/kopia/kopia/internal/gather"
 	"github.com/kopia/kopia/internal/remoterepoapi"
@@ -19,13 +16,13 @@ import (
 	"github.com/kopia/kopia/repo/manifest"
 )
 
-func (s *Server) handleContentGet(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	dr, ok := s.rep.(repo.DirectRepository)
+func handleContentGet(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	dr, ok := rc.rep.(repo.DirectRepository)
 	if !ok {
 		return nil, notFoundError("content not found")
 	}
 
-	cid := content.ID(mux.Vars(r)["contentID"])
+	cid := content.ID(rc.muxVar("contentID"))
 
 	data, err := dr.ContentReader().GetContent(ctx, cid)
 	if errors.Is(err, content.ErrContentNotFound) {
@@ -35,10 +32,10 @@ func (s *Server) handleContentGet(ctx context.Context, r *http.Request, body []b
 	return data, nil
 }
 
-func (s *Server) handleContentInfo(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	cid := content.ID(mux.Vars(r)["contentID"])
+func handleContentInfo(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	cid := content.ID(rc.muxVar("contentID"))
 
-	ci, err := s.rep.ContentInfo(ctx, cid)
+	ci, err := rc.rep.ContentInfo(ctx, cid)
 
 	switch {
 	case err == nil:
@@ -52,13 +49,13 @@ func (s *Server) handleContentInfo(ctx context.Context, r *http.Request, body []
 	}
 }
 
-func (s *Server) handleContentPut(ctx context.Context, r *http.Request, data []byte) (interface{}, *apiError) {
-	dr, ok := s.rep.(repo.DirectRepositoryWriter)
+func handleContentPut(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	dr, ok := rc.rep.(repo.DirectRepositoryWriter)
 	if !ok {
 		return nil, repositoryNotWritableError()
 	}
 
-	cid := content.ID(mux.Vars(r)["contentID"])
+	cid := content.ID(rc.muxVar("contentID"))
 	prefix := cid.Prefix()
 
 	if strings.HasPrefix(string(prefix), manifest.ContentPrefix) {
@@ -68,7 +65,7 @@ func (s *Server) handleContentPut(ctx context.Context, r *http.Request, data []b
 
 	var comp compression.HeaderID
 
-	if c := r.URL.Query().Get("compression"); c != "" {
+	if c := rc.queryParam("compression"); c != "" {
 		// nolint:gomnd
 		v, err := strconv.ParseInt(c, 16, 32)
 		if err != nil {
@@ -81,7 +78,7 @@ func (s *Server) handleContentPut(ctx context.Context, r *http.Request, data []b
 		}
 	}
 
-	actualCID, err := dr.ContentManager().WriteContent(ctx, gather.FromSlice(data), prefix, comp)
+	actualCID, err := dr.ContentManager().WriteContent(ctx, gather.FromSlice(rc.body), prefix, comp)
 	if err != nil {
 		return nil, internalServerError(err)
 	}
@@ -93,14 +90,14 @@ func (s *Server) handleContentPut(ctx context.Context, r *http.Request, data []b
 	return &serverapi.Empty{}, nil
 }
 
-func (s *Server) handleContentPrefetch(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
+func handleContentPrefetch(ctx context.Context, rc requestContext) (interface{}, *apiError) {
 	var req remoterepoapi.PrefetchContentsRequest
 
-	if err := json.Unmarshal(body, &req); err != nil {
+	if err := json.Unmarshal(rc.body, &req); err != nil {
 		return nil, requestError(serverapi.ErrorMalformedRequest, "malformed request")
 	}
 
 	return &remoterepoapi.PrefetchContentsResponse{
-		ContentIDs: s.rep.PrefetchContents(ctx, req.ContentIDs, req.Hint),
+		ContentIDs: rc.rep.PrefetchContents(ctx, req.ContentIDs, req.Hint),
 	}, nil
 }

internal/server/api_estimate.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"path/filepath"
+	"sync/atomic"
 
 	"github.com/pkg/errors"
 
@@ -38,14 +38,14 @@ func (p estimateTaskProgress) Error(ctx context.Context, dirname string, err err
 
 func (p estimateTaskProgress) Stats(ctx context.Context, st *snapshot.Stats, included, excluded snapshotfs.SampleBuckets, excludedDirs []string, final bool) {
 	p.ctrl.ReportCounters(map[string]uitask.CounterValue{
-		"Bytes":                uitask.BytesCounter(st.TotalFileSize),
-		"Excluded Bytes":       uitask.BytesCounter(st.ExcludedTotalFileSize),
-		"Files":                uitask.SimpleCounter(int64(st.TotalFileCount)),
-		"Directories":          uitask.SimpleCounter(int64(st.TotalDirectoryCount)),
-		"Excluded Files":       uitask.SimpleCounter(int64(st.ExcludedFileCount)),
-		"Excluded Directories": uitask.SimpleCounter(int64(st.ExcludedDirCount)),
-		"Errors":               uitask.ErrorCounter(int64(st.ErrorCount)),
-		"Ignored Errors":       uitask.ErrorCounter(int64(st.IgnoredErrorCount)),
+		"Bytes":                uitask.BytesCounter(atomic.LoadInt64(&st.TotalFileSize)),
+		"Excluded Bytes":       uitask.BytesCounter(atomic.LoadInt64(&st.ExcludedTotalFileSize)),
+		"Files":                uitask.SimpleCounter(int64(atomic.LoadInt32(&st.TotalFileCount))),
+		"Directories":          uitask.SimpleCounter(int64(atomic.LoadInt32(&st.TotalDirectoryCount))),
+		"Excluded Files":       uitask.SimpleCounter(int64(atomic.LoadInt32(&st.ExcludedFileCount))),
+		"Excluded Directories": uitask.SimpleCounter(int64(atomic.LoadInt32(&st.ExcludedDirCount))),
+		"Errors":               uitask.ErrorCounter(int64(atomic.LoadInt32(&st.ErrorCount))),
+		"Ignored Errors":       uitask.ErrorCounter(int64(atomic.LoadInt32(&st.IgnoredErrorCount))),
 	})
 
 	if final {
@@ -96,14 +96,14 @@ func logBucketSamples(ctx context.Context, buckets snapshotfs.SampleBuckets, pre
 
 var _ snapshotfs.EstimateProgress = estimateTaskProgress{}
 
-func (s *Server) handleEstimate(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
+func handleEstimate(ctx context.Context, rc requestContext) (interface{}, *apiError) {
 	var req serverapi.EstimateRequest
 
-	if err := json.Unmarshal(body, &req); err != nil {
+	if err := json.Unmarshal(rc.body, &req); err != nil {
 		return nil, requestError(serverapi.ErrorMalformedRequest, "malformed request body")
 	}
 
-	rep := s.rep
+	rep := rc.rep
 
 	resolvedRoot := filepath.Clean(ospath.ResolveUserFriendlyPath(req.Root, true))
 
@@ -119,10 +119,19 @@ func (s *Server) handleEstimate(ctx context.Context, r *http.Request, body []byt
 
 	taskIDChan := make(chan string)
 
+	policyTree, err := policy.TreeForSourceWithOverride(ctx, rc.rep, snapshot.SourceInfo{
+		Host:     rc.rep.ClientOptions().Hostname,
+		UserName: rc.rep.ClientOptions().Username,
+		Path:     resolvedRoot,
+	}, req.PolicyOverride)
+	if err != nil {
+		return nil, internalServerError(errors.Wrap(err, "unable to get policy tree"))
+	}
+
 	// launch a goroutine that will continue the estimate and can be observed in the Tasks UI.
 
 	// nolint:errcheck
-	go s.taskmgr.Run(ctx, "Estimate", resolvedRoot, func(ctx context.Context, ctrl uitask.Controller) error {
+	go rc.srv.taskManager().Run(ctx, "Estimate", resolvedRoot, func(ctx context.Context, ctrl uitask.Controller) error {
 		taskIDChan <- ctrl.CurrentTaskID()
 
 		estimatectx, cancel := context.WithCancel(ctx)
@@ -130,22 +139,13 @@ func (s *Server) handleEstimate(ctx context.Context, r *http.Request, body []byt
 
 		ctrl.OnCancel(cancel)
 
-		policyTree, err := policy.TreeForSourceWithOverride(ctx, s.rep, snapshot.SourceInfo{
-			Host:     s.rep.ClientOptions().Hostname,
-			UserName: s.rep.ClientOptions().Username,
-			Path:     resolvedRoot,
-		}, req.PolicyOverride)
-		if err != nil {
-			return errors.Wrap(err, "unable to get policy tree")
-		}
-
 		// nolint:wrapcheck
 		return snapshotfs.Estimate(estimatectx, rep, dir, policyTree, estimateTaskProgress{ctrl}, req.MaxExamplesPerBucket)
 	})
 
 	taskID := <-taskIDChan
 
-	task, ok := s.taskmgr.GetTask(taskID)
+	task, ok := rc.srv.taskManager().GetTask(taskID)
 	if !ok {
 		return nil, internalServerError(errors.Errorf("task not found"))
 	}

internal/server/api_manifest.go
@@ -3,9 +3,7 @@ package server
 import (
 	"context"
 	"encoding/json"
-	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
 
 	"github.com/kopia/kopia/internal/auth"
@@ -15,12 +13,12 @@ import (
 	"github.com/kopia/kopia/repo/manifest"
 )
 
-func (s *Server) handleManifestGet(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	mid := manifest.ID(mux.Vars(r)["manifestID"])
+func handleManifestGet(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	mid := manifest.ID(rc.muxVar("manifestID"))
 
 	var data json.RawMessage
 
-	md, err := s.rep.GetManifest(ctx, mid, &data)
+	md, err := rc.rep.GetManifest(ctx, mid, &data)
 	if errors.Is(err, manifest.ErrNotFound) {
 		return nil, notFoundError("manifest not found")
 	}
@@ -29,7 +27,7 @@ func (s *Server) handleManifestGet(ctx context.Context, r *http.Request, body []
 		return nil, internalServerError(err)
 	}
 
-	if !hasManifestAccess(ctx, s, r, md.Labels, auth.AccessLevelRead) {
+	if !hasManifestAccess(ctx, rc, md.Labels, auth.AccessLevelRead) {
 		return nil, accessDeniedError()
 	}
 
@@ -39,17 +37,17 @@ func (s *Server) handleManifestGet(ctx context.Context, r *http.Request, body []
 	}, nil
 }
 
-func (s *Server) handleManifestDelete(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	rw, ok := s.rep.(repo.RepositoryWriter)
+func handleManifestDelete(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	rw, ok := rc.rep.(repo.RepositoryWriter)
 	if !ok {
 		return nil, repositoryNotWritableError()
 	}
 
-	mid := manifest.ID(mux.Vars(r)["manifestID"])
+	mid := manifest.ID(rc.muxVar("manifestID"))
 
 	var data json.RawMessage
 
-	em, err := s.rep.GetManifest(ctx, mid, &data)
+	em, err := rc.rep.GetManifest(ctx, mid, &data)
 	if errors.Is(err, manifest.ErrNotFound) {
 		return nil, notFoundError("manifest not found")
 	}
@@ -58,7 +56,7 @@ func (s *Server) handleManifestDelete(ctx context.Context, r *http.Request, body
 		return nil, internalServerError(err)
 	}
 
-	if !hasManifestAccess(ctx, s, r, em.Labels, auth.AccessLevelFull) {
+	if !hasManifestAccess(ctx, rc, em.Labels, auth.AccessLevelFull) {
 		return nil, accessDeniedError()
 	}
 
@@ -74,20 +72,20 @@ func (s *Server) handleManifestDelete(ctx context.Context, r *http.Request, body
 	return &serverapi.Empty{}, nil
 }
 
-func (s *Server) handleManifestList(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
+func handleManifestList(ctx context.Context, rc requestContext) (interface{}, *apiError) {
 	// password already validated by a wrapper, no need to check here.
 	labels := map[string]string{}
 
-	for k, v := range r.URL.Query() {
+	for k, v := range rc.req.URL.Query() {
 		labels[k] = v[0]
 	}
 
-	m, err := s.rep.FindManifests(ctx, labels)
+	m, err := rc.rep.FindManifests(ctx, labels)
 	if err != nil {
 		return nil, internalServerError(err)
 	}
 
-	return filterManifests(m, s.httpAuthorizationInfo(ctx, r)), nil
+	return filterManifests(m, httpAuthorizationInfo(ctx, rc)), nil
 }
 
 func filterManifests(manifests []*manifest.EntryMetadata, authz auth.AuthorizationInfo) []*manifest.EntryMetadata {
@@ -102,19 +100,19 @@ func filterManifests(manifests []*manifest.EntryMetadata, authz auth.Authorizati
 	return result
 }
 
-func (s *Server) handleManifestCreate(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	rw, ok := s.rep.(repo.RepositoryWriter)
+func handleManifestCreate(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	rw, ok := rc.rep.(repo.RepositoryWriter)
 	if !ok {
 		return nil, repositoryNotWritableError()
 	}
 
 	var req remoterepoapi.ManifestWithMetadata
 
-	if err := json.Unmarshal(body, &req); err != nil {
+	if err := json.Unmarshal(rc.body, &req); err != nil {
 		return nil, requestError(serverapi.ErrorMalformedRequest, "malformed request")
 	}
 
-	if !hasManifestAccess(ctx, s, r, req.Metadata.Labels, auth.AccessLevelAppend) {
+	if !hasManifestAccess(ctx, rc, req.Metadata.Labels, auth.AccessLevelAppend) {
 		return nil, accessDeniedError()
 	}
 

internal/server/api_mount.go
@@ -3,19 +3,14 @@ package server
 import (
 	"context"
 	"encoding/json"
-	"net/http"
 
-	"github.com/gorilla/mux"
-
-	"github.com/kopia/kopia/internal/mount"
 	"github.com/kopia/kopia/internal/serverapi"
 	"github.com/kopia/kopia/repo/object"
-	"github.com/kopia/kopia/snapshot/snapshotfs"
 )
 
-func (s *Server) handleMountCreate(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
+func handleMountCreate(ctx context.Context, rc requestContext) (interface{}, *apiError) {
 	req := &serverapi.MountSnapshotRequest{}
-	if err := json.Unmarshal(body, req); err != nil {
+	if err := json.Unmarshal(rc.body, req); err != nil {
 		return nil, requestError(serverapi.ErrorMalformedRequest, "malformed request body")
 	}
 
@@ -24,25 +19,9 @@ func (s *Server) handleMountCreate(ctx context.Context, r *http.Request, body []
 		return nil, requestError(serverapi.ErrorMalformedRequest, "unable to parse OID")
 	}
 
-	var c mount.Controller
-
-	v, ok := s.mounts.Load(oid)
-	if !ok {
-		log(ctx).Debugf("mount controller for %v not found, starting", oid)
-
-		var err error
-		c, err = mount.Directory(ctx, snapshotfs.DirectoryEntry(s.rep, oid, nil), "*", mount.Options{})
-
-		if err != nil {
-			return nil, internalServerError(err)
-		}
-
-		if actual, loaded := s.mounts.LoadOrStore(oid, c); loaded {
-			c.Unmount(ctx)                // nolint:errcheck
-			c = actual.(mount.Controller) // nolint:forcetypeassert
-		}
-	} else {
-		c = v.(mount.Controller) // nolint:forcetypeassert
+	c, err := rc.srv.getMountController(ctx, rc.rep, oid, true)
+	if err != nil {
+		return nil, internalServerError(err)
 	}
 
 	log(ctx).Debugf("mount for %v => %v", oid, c.MountPath())
@@ -53,15 +32,17 @@ func (s *Server) handleMountCreate(ctx context.Context, r *http.Request, body []
 	}, nil
 }
 
-func (s *Server) handleMountGet(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	oid := object.ID(mux.Vars(r)["rootObjectID"])
+func handleMountGet(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	oid := object.ID(rc.muxVar("rootObjectID"))
 
-	v, ok := s.mounts.Load(oid)
-	if !ok {
-		return nil, notFoundError("mount point not found")
+	c, err := rc.srv.getMountController(ctx, rc.rep, oid, false)
+	if err != nil {
+		return nil, internalServerError(err)
 	}
 
-	c := v.(mount.Controller) // nolint:forcetypeassert
+	if c == nil {
+		return nil, notFoundError("mount point not found")
+	}
 
 	return &serverapi.MountedSnapshot{
 		Path: c.MountPath(),
@@ -69,55 +50,38 @@ func (s *Server) handleMountGet(ctx context.Context, r *http.Request, body []byt
 	}, nil
 }
 
-func (s *Server) handleMountDelete(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
-	oid := object.ID(mux.Vars(r)["rootObjectID"])
+func handleMountDelete(ctx context.Context, rc requestContext) (interface{}, *apiError) {
+	oid := object.ID(rc.muxVar("rootObjectID"))
 
-	v, ok := s.mounts.Load(oid)
-	if !ok {
-		return nil, notFoundError("mount point not found")
+	c, err := rc.srv.getMountController(ctx, rc.rep, oid, false)
+	if err != nil {
+		return nil, internalServerError(err)
 	}
 
-	c := v.(mount.Controller) // nolint:forcetypeassert
+	if c == nil {
+		return nil, notFoundError("mount point not found")
+	}
 
 	if err := c.Unmount(ctx); err != nil {
 		return nil, internalServerError(err)
 	}
 
-	s.mounts.Delete(oid)
+	rc.srv.deleteMount(oid)
 
 	return &serverapi.Empty{}, nil
 }
 
-func (s *Server) handleMountList(ctx context.Context, r *http.Request, body []byte) (interface{}, *apiError) {
+func handleMountList(ctx context.Context, rc requestContext) (interface{}, *apiError) {
 	res := &serverapi.MountedSnapshots{
 		Items: []*serverapi.MountedSnapshot{},
 	}
 
-	s.mounts.Range(func(key, val interface{}) bool {
-		oid := key.(object.ID)      // nolint:forcetypeassert
-		c := val.(mount.Controller) // nolint:forcetypeassert
-
+	for oid, c := range rc.srv.listMounts() {
 		res.Items = append(res.Items, &serverapi.MountedSnapshot{
 			Path: c.MountPath(),
 			Root: oid,
 		})
-		return true
-	})
+	}
 
 	return res, nil
-}
-
-func (s *Server) unmountAll(ctx context.Context) {
-	s.mounts.Range(func(key, val interface{}) bool {
-		c := val.(mount.Controller) // nolint:forcetypeassert
-
-		log(ctx).Debugf("unmounting %v from %v", key, c.MountPath())
-
-		if err := c.Unmount(ctx); err != nil {
-			log(ctx).Errorf("unable to unmount %v", key)
-		}
-
-		s.mounts.Delete(key)
-		return true
-	})
 }

commit 19e8945b4611daf1130042c108b539840582c8b8

fix(server): fixed server startup race condition
This fixes a regression introduced in #1837

internal/server/source_manager.go
@@ -148,6 +148,12 @@ func (s *sourceManager) setUploader(u *snapshotfs.Uploader) {
 }
 
 func (s *sourceManager) start(ctx context.Context, rep repo.Repository) {
+	isLocal := rep.ClientOptions().Hostname == s.src.Host && !rep.ClientOptions().ReadOnly
+
+	go s.run(ctx, isLocal)
+}
+
+func (s *sourceManager) run(ctx context.Context, isLocal bool) {
 	// make sure we run in a detached context, which ignores outside cancelation and deadline.
 	ctx = ctxutil.Detach(ctx)
 
@@ -157,14 +163,12 @@ func (s *sourceManager) start(ctx context.Context, rep repo.Repository) {
 	s.wg.Add(1)
 	defer s.wg.Done()
 
-	if rep.ClientOptions().Hostname == s.src.Host && !rep.ClientOptions().ReadOnly {
+	if isLocal {
 		log(ctx).Debugf("starting local source manager for %v", s.src)
-
-		go s.runLocal(ctx)
+		s.runLocal(ctx)
 	} else {
 		log(ctx).Debugf("starting read-only source manager for %v", s.src)
-
-		go s.runReadOnly(ctx)
+		s.runReadOnly(ctx)
 	}
 }
 