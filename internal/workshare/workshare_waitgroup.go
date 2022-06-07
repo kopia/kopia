672280648890323e781f0f580af5a29b34a88316
Jarek Kowalski <jaak@jkowalski.net>
2022-06-06 22:20:24 -0700

    linter fix

--- internal/workshare/workshare_waitgroup.go
+++ internal/workshare/workshare_waitgroup.go
@@ -108,9 +108,8 @@ func (g *AsyncGroup) CanShareWork(w *Pool) bool {
 		// we successfully added token to the channel, because we have exactly the same number
 		// of workers as the capacity of the channel, one worker will wake up to process
 		// item from w.work, which will be added by RunAsync().
-
-		// double check we're not closed
 		select {
+		// double check we're not closed
 		case <-w.closed:
 			panic("invalid usage - CanShareWork() after workshare.AsyncGroup has been closed")
 		default:
