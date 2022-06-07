86e4a45de99f9348bd8f67808ce0265945d6a358
Ashlie Martinez <ashmrtnz@alcion.ai>
2022-06-07 14:16:34 -0700

    Add better test for estimator ignoring stream dir
    Previous test in uploader had a race condition, meaning it may not catch
    all cases.

--- fs/entry.go
+++ fs/entry.go
@@ -58,6 +58,8 @@ type Directory interface {
 	Entry
 	Child(ctx context.Context, name string) (Entry, error)
 	IterateEntries(ctx context.Context, cb func(context.Context, Entry) error) error
+	// MultipleIterations returns true if the Directory supports iterating through
+	// the entries multiple times. Otherwise it returns false.
 	MultipleIterations() bool
 }
 
---
+++ snapshot/snapshotfs/estimate_test.go
@@ -0,0 +1,73 @@
+package snapshotfs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kopia/kopia/fs"
+	"github.com/kopia/kopia/fs/virtualfs"
+	"github.com/kopia/kopia/internal/mockfs"
+	"github.com/kopia/kopia/snapshot"
+	"github.com/kopia/kopia/snapshot/policy"
+)
+
+type fakeProgress struct {
+	t                   *testing.T
+	expectedFiles       int32
+	expectedDirectories int32
+	expectedErrors      int32
+}
+
+func (p *fakeProgress) Processing(context.Context, string) {}
+
+func (p *fakeProgress) Error(context.Context, string, error, bool) {}
+
+func (p *fakeProgress) Stats(
+	ctx context.Context,
+	s *snapshot.Stats,
+	includedFiles, excludedFiles SampleBuckets,
+	excludedDirs []string,
+	final bool,
+) {
+	if !final {
+		return
+	}
+
+	if got := s.ErrorCount; got != p.expectedErrors {
+		p.t.Errorf("unexpected errors encountered: (actual) %v != %v (expected)", got, p.expectedErrors)
+	}
+
+	if got := s.TotalFileCount; got != p.expectedFiles {
+		p.t.Errorf("unexpected files counted: (actual) %v != %v (expected)", got, p.expectedFiles)
+	}
+
+	if got := s.TotalDirectoryCount; got != p.expectedDirectories {
+		p.t.Errorf("unexpected directory count: (actual) %v != %v (expected)", got, p.expectedDirectories)
+	}
+}
+
+func TestEstimate_SkipsStreamingDirectory(t *testing.T) {
+	f := mockfs.NewFile("f1", []byte{1, 2, 3}, 0o777)
+
+	rootDir := virtualfs.NewStaticDirectory("root", []fs.Entry{
+		virtualfs.NewStreamingDirectory(
+			"a-dir",
+			func(ctx context.Context, callback func(context.Context, fs.Entry) error) error {
+				return callback(ctx, f)
+			},
+		),
+	})
+
+	policyTree := policy.BuildTree(nil, policy.DefaultPolicy)
+	p := &fakeProgress{
+		t:                   t,
+		expectedFiles:       0,
+		expectedDirectories: 2,
+		expectedErrors:      0,
+	}
+
+	err := Estimate(context.TODO(), nil, rootDir, policyTree, p, 1)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
