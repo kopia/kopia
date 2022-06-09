kopia/kopia

commit 9bf9cac7fbaa8084c56e1204f3978cbdc9137733

refactor(repository): ensure we always parse content.ID and object.ID (#1960)
* refactor(repository): ensure we always parse content.ID and object.ID

This changes the types to be incompatible with string to prevent direct
conversion to and from string.

This has the additional benefit of reducing number of memory allocations
and bytes for all IDs.

content.ID went from 2 allocations to 1:
   typical case 32 characters + 16 bytes per-string overhead
   worst-case 65 characters + 16 bytes per-string overhead
   now: 34 bytes

object.ID went from 2 allocations to 1:
   typical case 32 characters + 16 bytes per-string overhead
   worst-case 65 characters + 16 bytes per-string overhead
   now: 36 bytes

* move index.{ID,IDRange} methods to separate files

* replaced index.IDFromHash with content.IDFromHash externally

* minor tweaks and additional tests

* Update repo/content/index/id_test.go

Co-authored-by: Julio Lopez <1953782+julio-lopez@users.noreply.github.com>

* Update repo/content/index/id_test.go

Co-authored-by: Julio Lopez <1953782+julio-lopez@users.noreply.github.com>

* pr feedback

* post-merge fixes

* pr feedback

* pr feedback

* fixed subtle regression in sortedContents()

This was actually not producing invalid results because of how base36
works, just not sorting as efficiently as it could.

Co-authored-by: Julio Lopez <1953782+julio-lopez@users.noreply.github.com>

repo/content/content_manager.go
@@ -4,7 +4,6 @@ package content
 import (
 	"context"
 	cryptorand "crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"math/rand"
 	"sync"
@@ -744,20 +743,23 @@ func (bm *WriteManager) SupportsContentCompression() bool {
 
 // WriteContent saves a given content of data to a pack group with a provided name and returns a contentID
 // that's based on the contents of data written.
-func (bm *WriteManager) WriteContent(ctx context.Context, data gather.Bytes, prefix ID, comp compression.HeaderID) (ID, error) {
+func (bm *WriteManager) WriteContent(ctx context.Context, data gather.Bytes, prefix index.IDPrefix, comp compression.HeaderID) (ID, error) {
 	if err := bm.maybeRetryWritingFailedPacksUnlocked(ctx); err != nil {
-		return "", err
+		return EmptyID, err
 	}
 
 	reportContentWriteBytes(int64(data.Length()))
 
-	if err := ValidatePrefix(prefix); err != nil {
-		return "", err
+	if err := prefix.ValidateSingle(); err != nil {
+		return EmptyID, errors.Wrap(err, "invalid prefix")
 	}
 
 	var hashOutput [hashing.MaxHashSize]byte
 
-	contentID := prefix + ID(hex.EncodeToString(bm.hashData(hashOutput[:0], data)))
+	contentID, err := IDFromHash(prefix, bm.hashData(hashOutput[:0], data))
+	if err != nil {
+		return EmptyID, errors.Wrap(err, "invalid hash")
+	}
 
 	previousWriteTime := int64(-1)
 

repo/content/content_manager_lock_free.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/aes"
 	cryptorand "crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"io"
 
@@ -23,10 +22,7 @@ const indexBlobCompactionWarningThreshold = 1000
 func (sm *SharedManager) maybeCompressAndEncryptDataForPacking(data gather.Bytes, contentID ID, comp compression.HeaderID, output *gather.WriteBuffer) (compression.HeaderID, error) {
 	var hashOutput [hashing.MaxHashSize]byte
 
-	iv, err := getPackedContentIV(hashOutput[:], contentID)
-	if err != nil {
-		return NoCompression, errors.Wrapf(err, "unable to get packed content IV for %q", contentID)
-	}
+	iv := getPackedContentIV(hashOutput[:0], contentID)
 
 	// If the content is prefixed (which represents Kopia's own metadata as opposed to user data),
 	// and we're on V2 format or greater, enable internal compression even when not requested.
@@ -50,7 +46,7 @@ func (sm *SharedManager) maybeCompressAndEncryptDataForPacking(data gather.Bytes
 			return NoCompression, errors.Errorf("unsupported compressor %x", comp)
 		}
 
-		if err = c.Compress(&tmp, data.Reader()); err != nil {
+		if err := c.Compress(&tmp, data.Reader()); err != nil {
 			return NoCompression, errors.Wrap(err, "compression error")
 		}
 
@@ -81,20 +77,6 @@ func writeRandomBytesToBuffer(b *gather.WriteBuffer, count int) error {
 	b.Append(rnd[0:count])
 
 	return nil
-}
-
-// ValidatePrefix returns an error if a given prefix is invalid.
-func ValidatePrefix(prefix ID) error {
-	switch len(prefix) {
-	case 0:
-		return nil
-	case 1:
-		if prefix[0] >= 'g' && prefix[0] <= 'z' {
-			return nil
-		}
-	}
-
-	return errors.Errorf("invalid prefix, must be empty or a single letter between 'g' and 'z'")
 }
 
 func contentCacheKeyForInfo(bi Info) string {
@@ -163,13 +145,10 @@ func (bm *WriteManager) preparePackDataContent(pp *pendingPackInfo) (index.Build
 	return packFileIndex, err
 }
 
-func getPackedContentIV(output []byte, contentID ID) ([]byte, error) {
-	n, err := hex.Decode(output, []byte(contentID[len(contentID)-(aes.BlockSize*2):])) //nolint:gomnd
-	if err != nil {
-		return nil, errors.Wrapf(err, "error decoding content IV from %v", contentID)
-	}
+func getPackedContentIV(output []byte, contentID ID) []byte {
+	h := contentID.Hash()
 
-	return output[0:n], nil
+	return append(output, h[len(h)-aes.BlockSize:]...)
 }
 
 func (bm *WriteManager) writePackFileNotLocked(ctx context.Context, packFile blob.ID, data gather.Bytes) error {

repo/content/index/id.go
@@ -1,0 +1,191 @@
+package index
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/kopia/kopia/repo/hashing"
+)
+
+// IDPrefix represents a content ID prefix (empty string or single character between 'g' and 'z').
+type IDPrefix string
+
+// ValidateSingle returns an error if a given prefix is invalid.
+func (p IDPrefix) ValidateSingle() error {
+	switch len(p) {
+	case 0:
+		return nil
+	case 1:
+		if p[0] >= 'g' && p[0] <= 'z' {
+			return nil
+		}
+	}
+
+	return errors.Errorf("invalid prefix, must be empty or a single letter between 'g' and 'z'")
+}
+
+// ID is an identifier of content in content-addressable storage.
+type ID struct {
+	data [hashing.MaxHashSize]byte
+
+	// those 2 could be packed into one byte, but that seems like overkill
+	prefix byte
+	idLen  byte
+}
+
+// MarshalJSON implements JSON serialization.
+func (i ID) MarshalJSON() ([]byte, error) {
+	s := i.String()
+
+	// nolint:wrapcheck
+	return json.Marshal(s)
+}
+
+// UnmarshalJSON implements JSON deserialization.
+func (i *ID) UnmarshalJSON(v []byte) error {
+	var s string
+
+	if err := json.Unmarshal(v, &s); err != nil {
+		return errors.Wrap(err, "unable to unmarshal ID")
+	}
+
+	tmp, err := ParseID(s)
+	if err != nil {
+		return errors.Wrap(err, "invalid ID")
+	}
+
+	*i = tmp
+
+	return nil
+}
+
+// Hash returns the hash part of content ID.
+func (i ID) Hash() []byte {
+	return i.data[0:i.idLen]
+}
+
+// EmptyID represents empty content ID.
+var EmptyID = ID{} // nolint:gochecknoglobals
+
+// prefixStrings contains precomputed single-character strings for all valid prefixes 'g'..'z'
+// nolint:gochecknoglobals
+var prefixStrings [256]IDPrefix
+
+func init() {
+	for i := 'g'; i <= 'z'; i++ {
+		prefixStrings[i] = IDPrefix([]byte{byte(i)})
+	}
+}
+
+func (i ID) less(other ID) bool {
+	if i.prefix != other.prefix {
+		if other.prefix == 0 {
+			// value is g..z, other is a..f, so i > other
+			return false
+		}
+
+		return i.prefix < other.prefix
+	}
+
+	return bytes.Compare(i.data[:i.idLen], other.data[:other.idLen]) < 0
+}
+
+// String returns a string representation of ID.
+func (i ID) String() string {
+	return string(i.Prefix()) + hex.EncodeToString(i.data[:i.idLen])
+}
+
+// Prefix returns a one-character prefix of a content ID or an empty string.
+func (i ID) Prefix() IDPrefix {
+	return prefixStrings[i.prefix]
+}
+
+// comparePrefix returns the value of strings.Compare(i.String(), p) in an optimized manner.
+func (i ID) comparePrefix(p IDPrefix) int {
+	switch len(p) {
+	case 0:
+		// empty ID == "", otherwise greater
+		if i == EmptyID {
+			return 0
+		}
+
+		return 1
+
+	default:
+		// slow path
+		return strings.Compare(i.String(), string(p))
+	}
+}
+
+// HasPrefix determines if the given ID has a non-empty prefix.
+func (i ID) HasPrefix() bool {
+	return i.prefix != 0
+}
+
+// IDFromHash constructs content ID from a prefix and a hash.
+func IDFromHash(prefix IDPrefix, hash []byte) (ID, error) {
+	var id ID
+
+	if len(hash) > len(id.data) {
+		return EmptyID, errors.Errorf("hash too long")
+	}
+
+	if len(hash) == 0 {
+		return EmptyID, errors.Errorf("hash too short")
+	}
+
+	if err := prefix.ValidateSingle(); err != nil {
+		return EmptyID, errors.Wrap(err, "invalid prefix")
+	}
+
+	if len(prefix) > 0 {
+		id.prefix = prefix[0]
+	}
+
+	id.idLen = byte(len(hash))
+	copy(id.data[:], hash)
+
+	return id, nil
+}
+
+// ParseID parses and validates the content ID.
+func ParseID(s string) (ID, error) {
+	s0 := s
+
+	if s == "" {
+		return ID{}, nil
+	}
+
+	var id ID
+
+	if len(s)%2 == 1 {
+		id.prefix = s[0]
+
+		if id.prefix < 'g' || id.prefix > 'z' {
+			return id, errors.Errorf("invalid content prefix")
+		}
+
+		s = s[1:]
+	}
+
+	if len(s) > 2*len(id.data) {
+		return id, errors.Errorf("hash too long")
+	}
+
+	n, err := hex.Decode(id.data[:], []byte(s))
+	if err != nil {
+		return id, errors.Wrap(err, "invalid content hash")
+	}
+
+	if n == 0 {
+		return id, errors.Errorf("id too short: %q", s0)
+	}
+
+	id.idLen = byte(n)
+
+	return id, nil
+}

repo/content/index/id_test.go
@@ -1,0 +1,136 @@
+package index
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIDValid(t *testing.T) {
+	validIDsOrdered := []string{
+		"",
+		"0012abcd",
+		"12345678",
+		"ffffffff",
+		"g01234567",
+		"g01234568",
+		"yffffffff",
+		"z00000000",
+		"z00000001",
+		"zffffffff",
+	}
+
+	var validContentIDsOrdered []ID
+
+	for _, s := range validIDsOrdered {
+		cid, err := ParseID(s)
+		require.NoError(t, err)
+
+		require.Equal(t, s, cid.String())
+
+		validContentIDsOrdered = append(validContentIDsOrdered, cid)
+	}
+
+	for i, v1 := range validContentIDsOrdered {
+		jsonData, err := json.Marshal(v1)
+		require.NoError(t, err)
+
+		var v3 ID
+
+		require.NoError(t, json.Unmarshal(jsonData, &v3))
+		require.Equal(t, v1, v3)
+
+		for j, v2 := range validContentIDsOrdered {
+			switch {
+			case i < j:
+				require.True(t, v1.less(v2))
+				require.Negative(t, v1.comparePrefix(IDPrefix(v2.String())))
+			case i == j:
+				require.Equal(t, v1, v2)
+				require.Equal(t, 0, v1.comparePrefix(IDPrefix(v2.String())))
+			case i > j:
+				require.False(t, v1.less(v2))
+				require.Positive(t, v1.comparePrefix(IDPrefix(v2.String())))
+			}
+		}
+	}
+}
+
+func TestIDFromHash(t *testing.T) {
+	cid, err := IDFromHash("", []byte{0x12, 0x34})
+	require.NoError(t, err)
+	require.Equal(t, "1234", cid.String())
+
+	cid, err = IDFromHash("x", []byte{0x12, 0x34})
+	require.NoError(t, err)
+	require.Equal(t, "x1234", cid.String())
+
+	_, err = IDFromHash("xx", []byte{0x12, 0x34})
+	require.ErrorContains(t, err, "invalid prefix, must be empty or a single letter between 'g' and 'z'")
+
+	cid, err = IDFromHash("x", []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})
+	require.NoError(t, err)
+	require.Equal(t, "x000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f", cid.String())
+
+	_, err = IDFromHash("x", []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0})
+	require.ErrorContains(t, err, "hash too long")
+}
+
+func TestParseInvalid(t *testing.T) {
+	cases := map[string]string{
+		"x":  "id too short",
+		"x1": "invalid content hash",
+		"xx": "invalid content hash",
+		"x000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f11": "hash too long",
+		"A00":   "invalid content prefix",
+		"@abcd": "invalid content prefix",
+	}
+
+	for cid, wantErr := range cases {
+		_, err := ParseID(cid)
+		require.ErrorContains(t, err, wantErr)
+	}
+}
+
+func TestIDPrefix(t *testing.T) {
+	require.NoError(t, IDPrefix("").ValidateSingle())
+	require.NoError(t, IDPrefix("x").ValidateSingle())
+	require.ErrorContains(t, IDPrefix("@").ValidateSingle(), "invalid prefix, must be empty or a single letter between 'g' and 'z'")
+	require.ErrorContains(t, IDPrefix("x12").ValidateSingle(), "invalid prefix, must be empty or a single letter between 'g' and 'z'")
+}
+
+func TestIDHash(t *testing.T) {
+	prefixes := []IDPrefix{"", "g", "z"}
+	hashes := [][]byte{
+		{1, 2},
+		{1, 2, 3, 4},
+	}
+
+	for _, prefix := range prefixes {
+		for _, h := range hashes {
+			cid, err := IDFromHash(prefix, h)
+			require.NoError(t, err)
+			require.Equal(t, h, cid.Hash())
+
+			require.Equal(t, prefix != "", cid.HasPrefix())
+			require.Equal(t, prefix, cid.Prefix())
+		}
+	}
+
+	_, err := IDFromHash("", nil)
+	require.ErrorContains(t, err, "hash too short")
+}
+
+func TestIDInvalidJSON(t *testing.T) {
+	cases := map[string]string{
+		`"x"`: "invalid ID: id too short: \"x\"",
+		`123`: "cannot unmarshal number",
+	}
+
+	for jsonString, wantErr := range cases {
+		var v ID
+
+		require.ErrorContains(t, v.UnmarshalJSON([]byte(jsonString)), wantErr)
+	}
+}

commit fa52892c892ca8651c54105537bb86576d2b8f4e

chore(repository): reduced memory allocations when logging content.ID
This fixes regression introduced in #1960.

repo/content/content_manager.go
@@ -783,6 +783,12 @@ func (bm *WriteManager) WriteContent(ctx context.Context, data gather.Bytes, pre
 	_, bi, err := bm.getContentInfoReadLocked(ctx, contentID)
 	bm.mu.RUnlock()
 
+	logbuf := logging.GetBuffer()
+	defer logbuf.Release()
+
+	logbuf.AppendString("write-content ")
+	contentID.AppendToLogBuffer(logbuf)
+
 	// content already tracked
 	if err == nil {
 		if !bi.GetDeleted() {
@@ -791,10 +797,11 @@ func (bm *WriteManager) WriteContent(ctx context.Context, data gather.Bytes, pre
 
 		previousWriteTime = bi.GetTimestampSeconds()
 
-		bm.log.Debugf("write-content %v previously-deleted", contentID)
-	} else {
-		bm.log.Debugf("write-content %v new", contentID)
+		logbuf.AppendString(" previously-deleted:")
+		logbuf.AppendInt64(previousWriteTime)
 	}
+
+	bm.log.Debugf(logbuf.String())
 
 	return contentID, bm.addToPackUnlocked(ctx, contentID, data, false, comp, previousWriteTime)
 }

repo/content/content_manager_lock_free.go
@@ -18,6 +18,7 @@ import (
 	"github.com/kopia/kopia/repo/content/index"
 	"github.com/kopia/kopia/repo/encryption"
 	"github.com/kopia/kopia/repo/hashing"
+	"github.com/kopia/kopia/repo/logging"
 )
 
 const indexBlobCompactionWarningThreshold = 1000
@@ -109,12 +110,26 @@ func (bm *WriteManager) preparePackDataContent(pp *pendingPackInfo) (index.Build
 	packFileIndex := index.Builder{}
 	haveContent := false
 
+	sb := logging.GetBuffer()
+	defer sb.Release()
+
 	for _, info := range pp.currentPackItems {
 		if info.GetPackBlobID() == pp.packBlobID {
 			haveContent = true
 		}
 
-		bm.log.Debugf("add-to-pack %v %v p:%v %v d:%v", pp.packBlobID, info.GetContentID(), info.GetPackBlobID(), info.GetPackedLength(), info.GetDeleted())
+		sb.Reset()
+		sb.AppendString("add-to-pack ")
+		sb.AppendString(string(pp.packBlobID))
+		sb.AppendString(" ")
+		info.GetContentID().AppendToLogBuffer(sb)
+		sb.AppendString(" p:")
+		sb.AppendString(string(info.GetPackBlobID()))
+		sb.AppendString(" ")
+		sb.AppendUint32(info.GetPackedLength())
+		sb.AppendString(" d:")
+		sb.AppendBoolean(info.GetDeleted())
+		bm.log.Debugf(sb.String())
 
 		packFileIndex.Add(info)
 	}

repo/content/index/id.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/kopia/kopia/repo/hashing"
+	"github.com/kopia/kopia/repo/logging"
 )
 
 // IDPrefix represents a content ID prefix (empty string or single character between 'g' and 'z').
@@ -92,6 +93,18 @@ func (i ID) less(other ID) bool {
 	}
 
 	return bytes.Compare(i.data[:i.idLen], other.data[:other.idLen]) < 0
+}
+
+// AppendToLogBuffer appends content ID to log buffer.
+func (i ID) AppendToLogBuffer(sb *logging.Buffer) {
+	var buf [128]byte
+
+	if i.prefix != 0 {
+		sb.AppendByte(i.prefix)
+	}
+
+	hex.Encode(buf[0:i.idLen*2], i.data[0:i.idLen])
+	sb.AppendBytes(buf[0 : i.idLen*2])
 }
 
 // String returns a string representation of ID.

repo/logging/logging_buf.go
@@ -1,0 +1,148 @@
+package logging
+
+import (
+	"reflect"
+	"strconv"
+	"sync"
+	"time"
+	"unsafe"
+)
+
+// Buffer is a specialized buffer that can be kept in a pool used
+// for constructing logging messages without allocation.
+type Buffer struct {
+	buf      [1024]byte
+	validLen int // valid length
+}
+
+// nolint:gochecknoglobals
+var bufPool = &sync.Pool{
+	New: func() interface{} {
+		return &Buffer{}
+	},
+}
+
+// GetBuffer gets a logging buffer.
+func GetBuffer() *Buffer {
+	// nolint:forcetypeassert
+	return bufPool.Get().(*Buffer)
+}
+
+// Release releases logging buffer back to the pool.
+func (b *Buffer) Release() {
+	b.Reset()
+
+	bufPool.Put(b)
+}
+
+// Reset resets logging buffer back to zero length.
+func (b *Buffer) Reset() {
+	b.validLen = 0
+}
+
+// AppendByte appends a single byte/character.
+func (b *Buffer) AppendByte(val byte) *Buffer {
+	if b.validLen < len(b.buf) {
+		b.buf[b.validLen] = val
+		b.validLen++
+	}
+
+	return b
+}
+
+// AppendString appends a string of characters.
+func (b *Buffer) AppendString(val string) *Buffer {
+	vl := len(val)
+
+	if b.validLen+vl > len(b.buf) {
+		vl = len(b.buf) - b.validLen
+	}
+
+	if vl > 0 {
+		copy(b.buf[b.validLen:b.validLen+vl], val)
+		b.validLen += vl
+	}
+
+	return b
+}
+
+// AppendTime appends a time representation.
+func (b *Buffer) AppendTime(val time.Time, layout string) *Buffer {
+	var buf [64]byte
+
+	return b.AppendBytes(val.AppendFormat(buf[:0], layout))
+}
+
+// AppendBytes appends a slice of bytes.
+func (b *Buffer) AppendBytes(val []byte) *Buffer {
+	vl := len(val)
+
+	if b.validLen+vl > len(b.buf) {
+		vl = len(b.buf) - b.validLen
+	}
+
+	if vl > 0 {
+		copy(b.buf[b.validLen:b.validLen+vl], val)
+		b.validLen += vl
+	}
+
+	return b
+}
+
+// AppendBoolean appends boolean string ("true" or "false").
+func (b *Buffer) AppendBoolean(val bool) *Buffer {
+	if val {
+		return b.AppendString("true")
+	}
+
+	return b.AppendString("false")
+}
+
+// AppendInt32 appends int32 value formatted as a decimal string.
+func (b *Buffer) AppendInt32(val int32) *Buffer {
+	return b.AppendInt(int64(val), 10) //nolint:gomnd
+}
+
+// AppendInt64 appends int64 value formatted as a decimal string.
+func (b *Buffer) AppendInt64(val int64) *Buffer {
+	return b.AppendInt(val, 10) //nolint:gomnd
+}
+
+// AppendInt appends integer value formatted as a string in a given base.
+func (b *Buffer) AppendInt(val int64, base int) *Buffer {
+	var buf [64]byte
+
+	return b.AppendBytes(strconv.AppendInt(buf[:0], val, base))
+}
+
+// AppendUint32 appends uint32 value formatted as a decimal string.
+func (b *Buffer) AppendUint32(val uint32) *Buffer {
+	return b.AppendUint(uint64(val), 10) //nolint:gomnd
+}
+
+// AppendUint64 appends uint64 value formatted as a decimal string.
+func (b *Buffer) AppendUint64(val uint64) *Buffer {
+	return b.AppendUint(val, 10) //nolint:gomnd
+}
+
+// AppendUint appends unsigned integer value formatted as a string in a given base.
+func (b *Buffer) AppendUint(val uint64, base int) *Buffer {
+	var buf [64]byte
+
+	return b.AppendBytes(strconv.AppendUint(buf[:0], val, base))
+}
+
+// String returns a string value of a buffer. The value is valud as long as
+// string remains allocated and no Append*() methods have been called.
+func (b *Buffer) String() (s string) {
+	if b.validLen == 0 {
+		return ""
+	}
+
+	// *reflect.StringHeader can't be constructed, so we refer to output variable here
+	shdr := (*reflect.StringHeader)(unsafe.Pointer(&s)) //nolint:gosec
+	shdr.Data = uintptr(unsafe.Pointer(&b.buf))         //nolint:gosec
+	shdr.Len = b.validLen
+
+	return
+}

repo/content/index/id_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/kopia/kopia/repo/logging"
 )
 
 func TestIDValid(t *testing.T) {
@@ -23,11 +25,18 @@ func TestIDValid(t *testing.T) {
 
 	var validContentIDsOrdered []ID
 
+	sb := logging.GetBuffer()
+	defer sb.Release()
+
 	for _, s := range validIDsOrdered {
 		cid, err := ParseID(s)
 		require.NoError(t, err)
 
 		require.Equal(t, s, cid.String())
+
+		sb.Reset()
+		cid.AppendToLogBuffer(sb)
+		require.Equal(t, s, sb.String())
 
 		validContentIDsOrdered = append(validContentIDsOrdered, cid)
 	}

repo/logging/logging_buf_test.go
@@ -1,0 +1,77 @@
+package logging_test
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/kopia/kopia/repo/logging"
+)
+
+func TestLoggingBuffer_ReusesMemory(t *testing.T) {
+	b := logging.GetBuffer()
+	defer b.Release()
+
+	b.AppendString("xx")
+
+	s := b.String()
+	require.Equal(t, "xx", s)
+
+	// ensure we're reusing memory
+	b.Reset()
+	b.AppendString("yy")
+
+	require.Equal(t, "yy", s)
+}
+
+func TestLoggingBuffer_Overflow(t *testing.T) {
+	b := logging.GetBuffer()
+	defer b.Release()
+
+	filler := strings.Repeat("x", 1020)
+	b.AppendString(filler)
+	b.AppendString("foobarbaz")
+
+	// only room for 4 more characters
+	require.Equal(t, filler+"foob", b.String())
+
+	b.Reset()
+
+	b.AppendString(filler)
+	b.AppendBytes([]byte{65, 66, 67, 68, 69})
+
+	// only room for 4 more characters
+	require.Equal(t, filler+"ABCD", b.String())
+}
+
+func TestLoggingBuffer_Append(t *testing.T) {
+	b := logging.GetBuffer()
+	defer b.Release()
+
+	require.Equal(t, "", b.String())
+
+	require.Equal(t,
+		"xx ABC D -42 -23 true 42 false 23 2000-01-02T03:04:05Z",
+		b.AppendString("xx").
+			AppendString(" ").
+			AppendBytes([]byte{65, 66, 67}).
+			AppendString(" ").
+			AppendByte('D').
+			AppendString(" ").
+			AppendInt32(-42).
+			AppendString(" ").
+			AppendInt64(-23).
+			AppendString(" ").
+			AppendBoolean(true).
+			AppendString(" ").
+			AppendUint32(42).
+			AppendString(" ").
+			AppendBoolean(false).
+			AppendString(" ").
+			AppendUint64(23).
+			AppendString(" ").
+			AppendTime(time.Date(2000, 1, 2, 3, 4, 5, 6, time.UTC), time.RFC3339).
+			String())
+}