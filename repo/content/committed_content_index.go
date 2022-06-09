kopia/kopia

commit 680e3f29bba59e76d118d877afea8c70fd782dcf

feat(repository): switched to using go-mmap for indexes (#1980)
* feat(repository): switched to using go-mmap for indexes

Previous mmap implementation only allowed io.ReaderAt API
this one allows direct access to the underlying bytes which helps
remove a bunch of buffers, copying and allocation in index parsing.

* let the compiler do bounds check

* removed one more unnecessary allocation

* pr feedback

repo/content/committed_content_index.go
@@ -248,7 +248,7 @@ func (c *committedContentIndex) combineSmallIndexes(m index.Merged) (index.Merge
 		return nil, errors.Wrap(err, "error building combined in-memory index")
 	}
 
-	combined, err := index.Open(bytes.NewReader(buf.Bytes()), c.v1PerContentOverhead)
+	combined, err := index.Open(buf.Bytes(), nil, c.v1PerContentOverhead)
 	if err != nil {
 		return nil, errors.Wrap(err, "error opening combined in-memory index")
 	}

repo/content/committed_content_index_disk_cache.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/edsrzf/mmap-go"
 	"github.com/pkg/errors"
-	"golang.org/x/exp/mmap"
 
 	"github.com/kopia/kopia/internal/cache"
 	"github.com/kopia/kopia/internal/gather"
@@ -36,14 +36,14 @@ func (c *diskCommittedContentIndexCache) indexBlobPath(indexBlobID blob.ID) stri
 func (c *diskCommittedContentIndexCache) openIndex(ctx context.Context, indexBlobID blob.ID) (index.Index, error) {
 	fullpath := c.indexBlobPath(indexBlobID)
 
-	f, err := c.mmapOpenWithRetry(fullpath)
+	f, closeMmap, err := c.mmapOpenWithRetry(fullpath)
 	if err != nil {
 		return nil, err
 	}
 
-	ndx, err := index.Open(f, c.v1PerContentOverhead)
+	ndx, err := index.Open(f, closeMmap, c.v1PerContentOverhead)
 	if err != nil {
-		f.Close() // nolint:errcheck
+		closeMmap() // nolint:errcheck
 		return nil, errors.Wrapf(err, "error openind index from %v", indexBlobID)
 	}
 
@@ -52,14 +52,14 @@ func (c *diskCommittedContentIndexCache) openIndex(ctx context.Context, indexBlo
 
 // mmapOpenWithRetry attempts mmap.Open() with exponential back-off to work around rare issue specific to Windows where
 // we can't open the file right after it has been written.
-func (c *diskCommittedContentIndexCache) mmapOpenWithRetry(path string) (*mmap.ReaderAt, error) {
+func (c *diskCommittedContentIndexCache) mmapOpenWithRetry(path string) (mmap.MMap, func() error, error) {
 	const (
 		maxRetries    = 8
 		startingDelay = 10 * time.Millisecond
 	)
 
 	// retry milliseconds: 10, 20, 40, 80, 160, 320, 640, 1280, total ~2.5s
-	f, err := mmap.Open(path)
+	f, err := os.Open(path) // nolint:gosec
 	nextDelay := startingDelay
 
 	retryCount := 0
@@ -68,10 +68,28 @@ func (c *diskCommittedContentIndexCache) mmapOpenWithRetry(path string) (*mmap.R
 		c.log.Debugf("retry #%v unable to mmap.Open(): %v", retryCount, err)
 		time.Sleep(nextDelay)
 		nextDelay *= 2
-		f, err = mmap.Open(path)
+
+		f, err = os.Open(path) // nolint:gosec
 	}
 
-	return f, errors.Wrap(err, "mmap() error")
+	mm, err := mmap.Map(f, mmap.RDONLY, 0)
+	if err != nil {
+		f.Close() // nolint:errcheck
+
+		return nil, nil, errors.Wrap(err, "mmap error")
+	}
+
+	return mm, func() error {
+		if err2 := mm.Unmap(); err2 != nil {
+			return errors.Wrapf(err2, "error unmapping index %v", path)
+		}
+
+		if err2 := f.Close(); err2 != nil {
+			return errors.Wrapf(err2, "error closing index %v", path)
+		}
+
+		return nil
+	}, errors.Wrap(err, "mmap() error")
 }
 
 func (c *diskCommittedContentIndexCache) hasIndexBlobID(ctx context.Context, indexBlobID blob.ID) (bool, error) {

go.mod
@@ -116,6 +116,7 @@ require (
 	cloud.google.com/go/iam v0.3.0 // indirect
 	github.com/alessio/shellescape v1.4.1 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/edsrzf/mmap-go v1.1.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect

go.sum
@@ -180,6 +180,8 @@ github.com/eapache/go-resiliency v1.1.0/go.mod h1:kFI+JgMyC7bLPUVY133qvEBtVayf5m
 github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21/go.mod h1:+020luEh2TKB4/GOp8oxxtq0Daoen/Cii55CzbTV6DU=
 github.com/eapache/queue v1.1.0/go.mod h1:6eCeP0CKFpHLu8blIFXhExK/dRa7WDZfr6jVFPTqq+I=
 github.com/edsrzf/mmap-go v1.0.0/go.mod h1:YO35OhQPt3KJa3ryjFM5Bs14WD66h8eGKpfaBNrHW5M=
+github.com/edsrzf/mmap-go v1.1.0 h1:6EUwBLQ/Mcr1EYLE4Tn1VdW1A4ckqCQWZBw8Hr0kjpQ=
+github.com/edsrzf/mmap-go v1.1.0/go.mod h1:19H/e8pUPLicwkyNgOykDXkJ9F0MHE+Z52B8EIth78Q=
 github.com/envoyproxy/go-control-plane v0.6.9/go.mod h1:SBwIajubJHhxtWwsL9s8ss4safvEdbitLhGGK48rN6g=
 github.com/envoyproxy/go-control-plane v0.9.0/go.mod h1:YTl/9mNaCwkRvm6d1a2C3ymFceY/DCBVvsKhRF0iEA4=
 github.com/envoyproxy/go-control-plane v0.9.1-0.20191026205805-5f8ba28d4473/go.mod h1:YTl/9mNaCwkRvm6d1a2C3ymFceY/DCBVvsKhRF0iEA4=

repo/content/committed_content_index_mem_cache.go
@@ -1,7 +1,6 @@
 package content
 
 import (
-	"bytes"
 	"context"
 	"sync"
 
@@ -32,7 +31,7 @@ func (m *memoryCommittedContentIndexCache) addContentToCache(ctx context.Context
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	ndx, err := index.Open(bytes.NewReader(data.ToByteSlice()), m.v1PerContentOverhead)
+	ndx, err := index.Open(data.ToByteSlice(), nil, m.v1PerContentOverhead)
 	if err != nil {
 		return errors.Wrapf(err, "error opening index blob %v", indexBlobID)
 	}

repo/content/content_index_recovery.go
@@ -22,11 +22,10 @@ func (bm *WriteManager) RecoverIndexFromPackBlob(ctx context.Context, packFile b
 		return nil, err
 	}
 
-	ndx, err := index.Open(localIndexBytes.Bytes(), uint32(bm.crypter.Encryptor.Overhead()))
+	ndx, err := index.Open(localIndexBytes.Bytes().ToByteSlice(), nil, uint32(bm.crypter.Encryptor.Overhead()))
 	if err != nil {
 		return nil, errors.Errorf("unable to open index in file %v", packFile)
 	}
-	defer ndx.Close() //nolint:errcheck
 
 	var recovered []Info
 

repo/content/content_manager_indexes.go
@@ -78,7 +78,7 @@ func ParseIndexBlob(ctx context.Context, blobID blob.ID, encrypted gather.Bytes,
 		return nil, errors.Wrap(err, "unable to decrypt index blob")
 	}
 
-	ndx, err := index.Open(data.Bytes(), uint32(crypter.Encryptor.Overhead()))
+	ndx, err := index.Open(data.Bytes().ToByteSlice(), nil, uint32(crypter.Encryptor.Overhead()))
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to open index blob")
 	}

repo/content/index/index.go
@@ -17,7 +17,6 @@ const (
 // Index is a read-only index of packed contents.
 type Index interface {
 	io.Closer
-
 	ApproximateCount() int
 	GetInfo(contentID ID) (Info, error)
 
@@ -26,33 +25,42 @@ type Index interface {
 }
 
 // Open reads an Index from a given reader. The caller must call Close() when the index is no longer used.
-func Open(readerAt io.ReaderAt, v1PerContentOverhead uint32) (Index, error) {
-	h, err := v1ReadHeader(readerAt)
+func Open(data []byte, closer func() error, v1PerContentOverhead uint32) (Index, error) {
+	h, err := v1ReadHeader(data)
 	if err != nil {
 		return nil, errors.Wrap(err, "invalid header")
 	}
 
 	switch h.version {
 	case Version1:
-		return openV1PackIndex(h, readerAt, v1PerContentOverhead)
+		return openV1PackIndex(h, data, closer, v1PerContentOverhead)
 
 	case Version2:
-		return openV2PackIndex(readerAt)
+		return openV2PackIndex(data, closer)
 
 	default:
 		return nil, errors.Errorf("invalid header format: %v", h.version)
 	}
 }
 
-func readAtAll(ra io.ReaderAt, p []byte, offset int64) error {
-	n, err := ra.ReadAt(p, offset)
-	if n != len(p) {
-		return errors.Errorf("incomplete read at offset %v, got %v bytes, expected %v", offset, n, len(p))
-	}
+func safeSlice(data []byte, offset int64, length int) (v []byte, err error) {
+	defer func() {
+		if recover() != nil {
+			v = nil
+			err = errors.Errorf("invalid slice offset=%v, length=%v, data=%v bytes", offset, length, len(data))
+		}
+	}()
 
-	if err == nil || errors.Is(err, io.EOF) {
-		return nil
-	}
+	return data[offset : offset+int64(length)], nil
+}
 
-	return errors.Wrapf(err, "invalid read at offset %v (%v bytes)", offset, len(p))
+func safeSliceString(data []byte, offset int64, length int) (s string, err error) {
+	defer func() {
+		if recover() != nil {
+			s = ""
+			err = errors.Errorf("invalid slice offset=%v, length=%v, data=%v bytes", offset, length, len(data))
+		}
+	}()
+
+	return string(data[offset : offset+int64(length)]), nil
 }

commit bc88a8c7057237a94403f4533182d01ab7a265fd

fix(repository): fixed stress test caused by index leaking file handles (#2026)
Fixed regression caused by #1980

Switching to new mmap library uncovered a bug where we were not properly
closing indices that are no longer in use, since previous mmap had
a finalizer defined to do so.

repo/content/committed_content_index.go
@@ -153,36 +153,43 @@ func (c *committedContentIndex) indexFilesChanged(indexFiles []blob.ID) bool {
 	return false
 }
 
-func (c *committedContentIndex) merge(ctx context.Context, indexFiles []blob.ID) (merged index.Merged, used map[blob.ID]index.Index, finalErr error) {
-	used = map[blob.ID]index.Index{}
-
-	defer func() {
-		// we failed along the way, close the merged index.
-		if finalErr != nil {
-			merged.Close() //nolint:errcheck
-		}
-	}()
+// +checklocks:c.mu
+func (c *committedContentIndex) merge(ctx context.Context, indexFiles []blob.ID) (index.Merged, map[blob.ID]index.Index, error) {
+	var (
+		newMerged   index.Merged
+		newlyOpened index.Merged // new indexes that were not in c.inUse before
+		newUsedMap  = map[blob.ID]index.Index{}
+	)
 
 	for _, e := range indexFiles {
-		ndx, err := c.cache.openIndex(ctx, e)
-		if err != nil {
-			return nil, nil, errors.Wrapf(err, "unable to open pack index %q", e)
+		ndx := c.inUse[e]
+		if ndx == nil {
+			var err error
+
+			ndx, err = c.cache.openIndex(ctx, e)
+			if err != nil {
+				newlyOpened.Close() // nolint:errcheck
+
+				return nil, nil, errors.Wrapf(err, "unable to open pack index %q", e)
+			}
+
+			newlyOpened = append(newlyOpened, ndx)
 		}
 
-		merged = append(merged, ndx)
-		used[e] = ndx
+		newMerged = append(newMerged, ndx)
+		newUsedMap[e] = ndx
 	}
 
-	mergedAndCombined, err := c.combineSmallIndexes(merged)
+	mergedAndCombined, err := c.combineSmallIndexes(newMerged)
 	if err != nil {
+		newlyOpened.Close() // nolint:errcheck
+
 		return nil, nil, errors.Wrap(err, "unable to combine small indexes")
 	}
 
-	c.log.Debugf("combined %v into %v index segments", len(merged), len(mergedAndCombined))
+	c.log.Debugf("combined %v into %v index segments", len(newMerged), len(mergedAndCombined))
 
-	merged = mergedAndCombined
-
-	return
+	return mergedAndCombined, newUsedMap, nil
 }
 
 // Uses indexFiles for indexing. An error is returned if the
@@ -205,9 +212,19 @@ func (c *committedContentIndex) use(ctx context.Context, indexFiles []blob.ID, i
 	}
 
 	atomic.AddInt64(&c.rev, 1)
-
 	c.merged = mergedAndCombined
+
+	oldInUse := c.inUse
 	c.inUse = newInUse
+
+	// close indices that were previously in use but are no longer.
+	for k, old := range oldInUse {
+		if newInUse[k] == nil {
+			if err := old.Close(); err != nil {
+				c.log.Errorf("unable to close unused index file: %v", err)
+			}
+		}
+	}
 
 	if err := c.cache.expireUnused(ctx, indexFiles); err != nil {
 		c.log.Errorf("unable to expire unused index files: %v", err)

repo/content/committed_content_index_disk_cache.go
@@ -72,6 +72,10 @@ func (c *diskCommittedContentIndexCache) mmapOpenWithRetry(path string) (mmap.MM
 		f, err = os.Open(path) // nolint:gosec
 	}
 
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "unable to open file despite retries")
+	}
+
 	mm, err := mmap.Map(f, mmap.RDONLY, 0)
 	if err != nil {
 		f.Close() // nolint:errcheck