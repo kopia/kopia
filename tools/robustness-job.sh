e512834ce67b23d9911f4d1aa9dbc81e85d9777c
Chaitali Gondhalekar <chaitali@kasten.io>
2022-06-16 13:36:45 -0700

    Revert changes to robustness job script

--- tools/robustness-job.sh
+++ tools/robustness-job.sh
@@ -106,11 +106,7 @@ readonly test_flags="-v -timeout=${test_timeout}\
 
 # Set the make target based on ENGINE_MODE
 ENGINE_MODE="${ENGINE_MODE:-}"
-if [[ "${ENGINE_MODE}" == "" ]]; then
-    make_target="recovery-tests"
-else
-    make_target="robustness-tests"
-fi
+make_target="robustness-tests"
 if [[ "${ENGINE_MODE}" = SERVER ]]; then
     make_target="robustness-server-tests"
 fi
