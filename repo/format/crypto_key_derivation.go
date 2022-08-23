e81a099baa3145153ddd7dd1c639c338314de953
Jarek Kowalski <jaak@jkowalski.net>
2022-08-23 18:46:05 +0100

    partial revert

--- repo/format/crypto_key_derivation.go
+++ repo/format/crypto_key_derivation.go
@@ -5,8 +5,6 @@ import (
 	"io"
 
 	"golang.org/x/crypto/hkdf"
-
-	"github.com/kopia/kopia/internal/impossible"
 )
 
 // DeriveKeyFromMasterKey computes a key for a specific purpose and length using HKDF based on the master key.
@@ -14,8 +12,9 @@ func DeriveKeyFromMasterKey(masterKey, uniqueID, purpose []byte, length int) []b
 	key := make([]byte, length)
 	k := hkdf.New(sha256.New, masterKey, uniqueID, purpose)
 
-	_, err := io.ReadFull(k, key)
-	impossible.PanicOnError(err)
+	if _, err := io.ReadFull(k, key); err != nil {
+		panic("unable to derive key from master key, this should never happen")
+	}
 
 	return key
 }
