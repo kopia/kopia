3b1c37270e77119908ec2b887abb2f2593756c5e
Jarek Kowalski <jaak@jkowalski.net>
2022-09-23 07:00:39 -0700

    fixed executable path on linux

--- app/tests/main.spec.js
+++ app/tests/main.spec.js
@@ -1,14 +1,11 @@
 import { test, expect } from '@playwright/test'
-import { 
-   findLatestBuild, 
-   parseElectronApp } from 'electron-playwright-helpers'
- import { _electron as electron } from 'playwright'
+import { _electron as electron } from 'playwright'
 
- import path from 'path';
+import path from 'path';
 
- let electronApp
+let electronApp
 
- function getKopiaUIBuiltPath() {
+function getKopiaUIUnpackedDir() {
   switch (process.platform + "/" + process.arch) {
     case "darwin/x64":
       return path.resolve("../dist/kopia-ui/mac");
@@ -23,50 +20,78 @@ import {
     default:
       return null;
   }
- }
+}
  
- test.beforeAll(async () => {
-   const latestBuild = getKopiaUIBuiltPath();
-   expect(latestBuild).not.toBeNull();
+function getMainPath(unpackedDir) {
+  switch (process.platform) {
+    case "darwin":
+      return path.join(unpackedDir, "KopiaUI.app", "Contents", "Resources", "app.asar", "public", "electron.js");
+    default:
+      return path.join(unpackedDir, "resources", "app.asar", "public", "electron.js");
+  }
+}
 
-   // parse the directory and find paths and other info
-   const appInfo = parseElectronApp(latestBuild)
-   console.log('appInfo', appInfo);
-   process.env.CI = 'e2e'
-   process.env.KOPIA_UI_TESTING = '1'
-   electronApp = await electron.launch({
-     args: [appInfo.main],
-     executablePath: appInfo.executable
-   })
-   electronApp.on('window', async (page) => {
-     const filename = page.url()?.split('/').pop()
-     console.log(`Window opened: ${filename}`)
+function getExecutablePath(unpackedDir) {
+  switch (process.platform) {
+    case "win32":
+      return path.join(unpackedDir, "KopiaUI.exe");
+    case "darwin":
+      return path.join(unpackedDir, "KopiaUI.app", "Contents", "MacOS", "KopiaUI");
+    default:
+      return path.join(unpackedDir, "kopia-ui");
+  }
+}
+
+test.beforeAll(async () => {
+  const unpackedDir = getKopiaUIUnpackedDir();
+  expect(unpackedDir).not.toBeNull();
+
+  const mainPath = getMainPath(unpackedDir);
+  const executablePath = getExecutablePath(unpackedDir);
+
+  console.log('main path', mainPath);
+  console.log('executable path', executablePath);
+
+  process.env.CI = 'e2e'
+  process.env.KOPIA_UI_TESTING = '1'
+  electronApp = await electron.launch({
+    args: [mainPath],
+    executablePath: executablePath,
+  })
+  electronApp.on('window', async (page) => {
+    const filename = page.url()?.split('/').pop()
+    console.log(`Window opened: ${filename}`)
+
+    // capture errors
+    page.on('pageerror', (error) => {
+      console.error(error)
+    })
+    // capture console messages
+    page.on('console', (msg) => {
+      console.log(msg.text())
+    })
+  })
+})
  
-     // capture errors
-     page.on('pageerror', (error) => {
-       console.error(error)
-     })
-     // capture console messages
-     page.on('console', (msg) => {
-       console.log(msg.text())
-     })
-   })
- })
+test.afterAll(async () => {
+  await electronApp.close()
+})
  
- test.afterAll(async () => {
-   await electronApp.close()
- })
- 
- test('renders the first page', async () => {
+test('renders the first page', async () => {
   await electronApp.evaluate(async ({app}) => {
     app.testHooks.showRepoWindow('repository');
   });
+
   const page = await electronApp.firstWindow();
 
   expect(page).toBeTruthy();
   expect(await page.title()).toMatch(/KopiaUI v\d+/);
 
   // TODO - we can exercise some UI scenario using 'page'
+
+  await electronApp.evaluate(async ({app}) => {
+    return app.testHooks.tray.popUpContextMenu();
+  })
 
   await electronApp.evaluate(async ({app}) => {
     return app.testHooks.tray.closeContextMenu();
