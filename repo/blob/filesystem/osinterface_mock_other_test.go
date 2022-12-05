dbe9aaa9ad07887f75b5f2f54b54a7ec01930004
Shikhar Mall <small@kopia.io>
2022-12-05 07:30:24 -0800

    fix(general): prevent retries on ESTALE errors

---
+++ repo/blob/filesystem/osinterface_mock_other_test.go
@@ -0,0 +1,12 @@
+//go:build !linux && !freebsd && !darwin
+// +build !linux,!freebsd,!darwin
+
+package filesystem
+
+func (osi *mockOS) Stat(fname string) (fs.FileInfo, error) {
+	if osi.statRemainingErrors.Add(-1) >= 0 {
+		return nil, &os.PathError{Op: "stat", Err: errors.Errorf("underlying problem")}
+	}
+
+	return osi.osInterface.Stat(fname)
+}
