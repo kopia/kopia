44eed9a921d33ec4d8afe2437f4436e7f09cb23a
Shikhar Mall <small@kopia.io>
2022-12-06 20:55:30 -0800

    adding missing imports

--- repo/blob/filesystem/osinterface_mock_other_test.go
+++ repo/blob/filesystem/osinterface_mock_other_test.go
@@ -3,6 +3,13 @@
 
 package filesystem
 
+import (
+	"io/fs"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
 func (osi *mockOS) Stat(fname string) (fs.FileInfo, error) {
 	if osi.statRemainingErrors.Add(-1) >= 0 {
 		return nil, &os.PathError{Op: "stat", Err: errors.Errorf("underlying problem")}
