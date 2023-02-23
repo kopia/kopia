5c901abc90857d4b85e6694ef4eeb86de7e5e89b
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2023-02-23 10:18:07 -0800

    test(general): parse stdout and stderr in robustness framework helper (#2779)
    Parse stdout as well as stderr in `KopiaSnapshotter.CreateSnapshot`.
    This is contained to the Robustness Framework.
    
    Co-authored-by: Carl Braganza <carl@kasten.io>

--- tests/tools/kopiarunner/kopia_snapshotter.go
+++ tests/tools/kopiarunner/kopia_snapshotter.go
@@ -132,9 +132,13 @@ func (ks *KopiaSnapshotter) ConnectOrCreateFilesystem(repoPath string) error {
 // CreateSnapshot implements the Snapshotter interface, issues a kopia snapshot
 // create command on the provided source path.
 func (ks *KopiaSnapshotter) CreateSnapshot(source string) (snapID string, err error) {
-	_, errOut, err := ks.Runner.Run("snapshot", "create", parallelFlag, strconv.Itoa(parallelSetting), noProgressFlag, source)
+	stdOut, errOut, err := ks.Runner.Run("snapshot", "create", parallelFlag, strconv.Itoa(parallelSetting), noProgressFlag, source)
 	if err != nil {
 		return "", err
+	}
+
+	if stdOut != "" {
+		return parseSnapID(strings.Split(stdOut, "\n"))
 	}
 
 	return parseSnapID(strings.Split(errOut, "\n"))
