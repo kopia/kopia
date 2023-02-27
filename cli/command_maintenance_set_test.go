de348d13cb001c36bf008b15a5783c32ac351590
PhracturedBlue <rc2012@pblue.org>
2023-02-27 11:29:38 -0800

    fix typos in comments

--- cli/command_maintenance_set_test.go
+++ cli/command_maintenance_set_test.go
@@ -51,19 +51,19 @@ func (s *formatSpecificTestSuite) TestInvalidExtendRetainOptions(t *testing.T) {
 
 	e.RunAndExpectSuccess(t, "maintenance", "set", "--full-interval", "24h01m")
 
-	// Cannot enable extend object locks when retention_perion-full_maintenance_interval < 24h
+	// Cannot enable extend object locks when retention_period-full_maintenance_interval < 24h
 	e.RunAndExpectFailure(t, "maintenance", "set", "--extend-object-locks", "true")
 
 	testutil.MustParseJSONLines(t, e.RunAndExpectSuccess(t, "maintenance", "info", "--json"), &mi)
 
 	require.False(t, mi.ExtendObjectLocks, "ExtendOjectLocks should be disabled.")
 
-	// Enable extend object locks when retention_perion-full_maintenance_interval > 24h
+	// Enable extend object locks when retention_period-full_maintenance_interval > 24h
 	e.RunAndExpectSuccess(t, "maintenance", "set", "--full-interval", "23h59m")
 
 	e.RunAndExpectSuccess(t, "maintenance", "set", "--extend-object-locks", "true")
 
-	// Cannot change full_maintenance_interval when retention_perion-full_maintenance_interval < 24h
+	// Cannot change full_maintenance_interval when retention_period-full_maintenance_interval < 24h
 	e.RunAndExpectFailure(t, "maintenance", "set", "--full-interval", "24h01m")
 
 	testutil.MustParseJSONLines(t, e.RunAndExpectSuccess(t, "maintenance", "info", "--json"), &mi)
@@ -71,12 +71,12 @@ func (s *formatSpecificTestSuite) TestInvalidExtendRetainOptions(t *testing.T) {
 	require.True(t, mi.ExtendObjectLocks, "ExtendOjectLocks should be enabled.")
 	require.True(t, mi.FullCycle.Interval == 86340000000000, "maintenance-interval should be unchanged.")
 
-	// Cannot change retention_period when retention_perion-full_maintenance_interval < 24h
+	// Cannot change retention_period when retention_period-full_maintenance_interval < 24h
 	e.RunAndExpectFailure(t, "repository", "set-parameters", "--retention-period", "47h")
 	testutil.MustParseJSONLines(t, e.RunAndExpectSuccess(t, "repo", "status", "--json"), &rs)
 	require.True(t, rs.BlobRetention.RetentionPeriod == 172800000000000, "retention-interval should be unchanged.")
 
-	// Can change retention_period when retention_perion-full_maintenance_interval > 24h
+	// Can change retention_period when retention_period-full_maintenance_interval > 24h
 	e.RunAndExpectSuccess(t, "repository", "set-parameters", "--retention-period", "49h")
 	testutil.MustParseJSONLines(t, e.RunAndExpectSuccess(t, "repo", "status", "--json"), &rs)
 	require.True(t, rs.BlobRetention.RetentionPeriod == 176400000000000, "retention-interval should be unchanged.")
