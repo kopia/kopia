1a7527c5896a2acfc5843bd7a312af46c0f7aff4
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2023-03-28 00:21:18 -0700

    breaking(cli): remove default behavior for `maintenance` command
    command: maintenance
    default-subcommand: run

--- cli/command_maintenance_run.go
+++ cli/command_maintenance_run.go
@@ -17,7 +17,7 @@ type commandMaintenanceRun struct {
 }
 
 func (c *commandMaintenanceRun) setup(svc appServices, parent commandParent) {
-	cmd := parent.Command("run", "Run repository maintenance").Default()
+	cmd := parent.Command("run", "Run repository maintenance")
 	cmd.Flag("full", "Full maintenance").BoolVar(&c.maintenanceRunFull)
 	cmd.Flag("force", "Run maintenance even if not owned (unsafe)").Hidden().BoolVar(&c.maintenanceRunForce)
 	safetyFlagVar(cmd, &c.safety)
