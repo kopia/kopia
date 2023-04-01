6aa5be882c0da9a7a35aad5acb2f5bbb00406cf2
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-03-31 17:13:04 -0700

    Add some tests

---
+++ repo/manifest/serialized_test.go
@@ -0,0 +1,73 @@
+package manifest
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kopia/kopia/repo/manifest/testdata"
+)
+
+func newDecodeInput(t *testing.T, input []byte) manifest {
+	t.Helper()
+
+	arrReader := bytes.NewReader([]byte(input))
+	arrDec, err := decodeManifestArray(arrReader)
+	require.NoError(t, err)
+
+	return arrDec
+}
+
+func TestManifestDecode_GoodInput(t *testing.T) {
+	table := []struct {
+		name  string
+		input []byte
+	}{
+		{
+			name:  "MultipleManifests",
+			input: []byte(testdata.GoodManifests),
+		},
+		{
+			name:  "IgnoredField",
+			input: []byte(testdata.IgnoredField),
+		},
+		{
+			name:  "StopsAtStructEnd",
+			input: []byte(testdata.ExtraInputAtEnd),
+		},
+	}
+
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			stdlibDec := manifest{}
+
+			stdReader := bytes.NewReader(test.input)
+			require.NoError(t, json.NewDecoder(stdReader).Decode(&stdlibDec))
+
+			arrReader := bytes.NewReader(test.input)
+			arrDec, err := decodeManifestArray(arrReader)
+			require.NoError(t, err)
+
+			assert.Equal(t, stdlibDec, arrDec)
+
+			assert.True(t, reflect.DeepEqual(stdlibDec, arrDec))
+		})
+	}
+}
+
+func TestManifestDecode_BadInput(t *testing.T) {
+	for _, test := range testdata.BadInputs {
+		t.Run(test.Name, func(t *testing.T) {
+			r := bytes.NewReader([]byte(test.Input))
+			_, err := decodeManifestArray(r)
+
+			t.Logf("%v", err)
+
+			assert.Error(t, err)
+		})
+	}
+}
