e8c5215426dfd91bba1bdb89578ad913aba8375e
Aaron H. Alpar <aaron@kasten.io>
2023-08-03 15:16:28 -0700

    cleanup kats, tests

--- tools/kats/kats.go
+++ tools/kats/kats.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"bytes"
-	"encoding/pem"
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path"
-	"strings"
+
+	"github.com/kopia/kopia/tools/kats/pems"
 )
 
-var verbose bool
-var flgs *flag.FlagSet
+const (
+	FILE_SIZE_B_MAX = 1 << 24
+)
+
+var (
+	ErrOverflow = errors.New(fmt.Sprintf("input exceeds maximum input size of %d", FILE_SIZE_B_MAX))
+	flgs        *flag.FlagSet
+	verbose     bool
+)
 
 func init() {
 	flgs = flag.NewFlagSet(path.Base(os.Args[0]), flag.ExitOnError)
@@ -25,89 +33,30 @@ func init() {
 	}
 }
 
-func isPipe(f *os.File) (bool, error) {
-	instat, err := f.Stat()
-	if err != nil {
-		return false, err
-	}
-	return (instat.Mode() & os.ModeNamedPipe) != 0, nil
-}
-
-func createOutFile(blknm, ext string) (*os.File, error) {
-	max := 1000
-	i := 0
-	f, err := tryFile(blknm, ext, i)
-	for i < max && err != nil && os.IsExist(err) {
-		f.Close()
-		i++
-		f, err = tryFile(blknm, ext, i)
-	}
-	return f, err
-}
-
-func tryFile(blknm, ext string, i int) (*os.File, error) {
-	// PEM specs spaces only - so this is safe
-	nm := strings.ReplaceAll(strings.ToLower(blknm), " ", "_")
-	var fnm string
-	if i <= 0 {
-		fnm = fmt.Sprintf("%s.%s", nm, ext)
-	} else {
-		fnm = fmt.Sprintf("%s.%d.%s", nm, i, ext)
-	}
-	return os.OpenFile(fnm, os.O_CREATE|os.O_EXCL|os.O_RDWR, 0o644)
-}
-
-func exportFiles(bs []byte) error {
-	var err error
-	for len(bs) > 0 && err == nil {
-		blk, rest := pem.Decode(bs)
-		if blk == nil {
-			return nil
-		}
-		var f *os.File
-		f, err = createOutFile(blk.Type, "bin")
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%s\n", f.Name())
-		if verbose {
-			fmt.Fprintf(os.Stderr, "writing PEM %q as %q\n", blk.Type, f.Name())
-		}
-		_, err = f.Write(blk.Bytes)
-		err1 := f.Close()
-		err = errors.Join(err, err1)
-		if err != nil {
-			return err
-		}
-		bs = rest
-	}
-	return nil
-}
-
 func main() {
+	ctx := context.Background()
 	args := flgs.Args()
+	// will use stdin if no args are supplied
 	if len(args) == 0 {
-		isp, err := isPipe(os.Stdin)
-		if !isp {
-			flgs.Usage()
-			exit("open", err)
-		}
 		// not ideal ... will work for smallish files on stdin
 		buf := &bytes.Buffer{}
-		_, err = io.CopyN(buf, os.Stdin, 1<<31)
-		if errors.Is(err, io.EOF) {
-			err = nil
+		// copy Stdin, up to FILE_SIZE_B_MAX bytes.
+		n, err := io.CopyN(buf, os.Stdin, FILE_SIZE_B_MAX)
+		if n == FILE_SIZE_B_MAX {
+			exit("read", ErrOverflow)
 		}
-		err1 := exportFiles(buf.Bytes())
-		err = errors.Join(err, err1)
-		if err != nil {
+		if err != nil && !errors.Is(err, io.EOF) {
+			exit("read", err)
+		}
+		err = pems.ExportPEMsAsFiles(ctx, verbose, "", buf.Bytes())
+		if err != nil && !errors.Is(err, pems.ErrNoPEMFound) {
 			exit("export", err)
 		}
 		return
 	}
 	for i := range args {
 		bs, err := os.ReadFile(args[i])
-		err1 := exportFiles(bs)
+		err1 := pems.ExportPEMsAsFiles(ctx, verbose, "", bs)
 		err = errors.Join(err, err1)
 		if err != nil {
 			exit("export", err)
---
+++ tools/kats/pems/pems.go
@@ -0,0 +1,84 @@
+package pems
+
+import (
+	"context"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+const (
+	OUTPUT_FILEEXT  = "bin"
+	FILE_COLLISIONS = 1000
+)
+
+var (
+	elidePunctAndSpace = regexp.MustCompile("([[:space:][:cntrl:][:punct:]])")
+	ErrNoPEMFound      = errors.New("no PEM found")
+)
+
+func CreateOutFile(ctx context.Context, prefix, blknm, ext string) (*os.File, error) {
+	max := FILE_COLLISIONS
+	i := 0
+	f, err := TryFile(ctx, prefix, blknm, ext, i)
+	for i < max && err != nil && os.IsExist(err) {
+		f.Close()
+		i++
+		f, err = TryFile(ctx, prefix, blknm, ext, i)
+	}
+	return f, err
+}
+
+// FilenameFromBlockName turn PEM header name into string that can be used in a filename.
+func FilenameFromBlockName(blknm string, ext string, i int) string {
+	// PEM specs spaces only - so this is safe
+	nm := elidePunctAndSpace.ReplaceAllLiteralString(strings.ToLower(blknm), "_")
+	var q string
+	if i <= 0 {
+		q = fmt.Sprintf("%s.%s", nm, ext)
+	} else {
+		q = fmt.Sprintf("%s.%d.%s", nm, i, ext)
+	}
+	return q
+}
+
+// TryFile try and create a file.
+func TryFile(ctx context.Context, prefix, blknm, ext string, i int) (*os.File, error) {
+	// PEM specs spaces only - so this is safe
+	fnm := FilenameFromBlockName(blknm, ext, i)
+	return os.OpenFile(filepath.Join(prefix, fnm), os.O_CREATE|os.O_EXCL|os.O_RDWR, 0o644)
+}
+
+func ExportPEMAsFile(ctx context.Context, verbose bool, prefix string, bs []byte) ([]byte, error) {
+	// try and decode the next PEM in bs
+	blk, rest := pem.Decode(bs)
+	if blk == nil {
+		// no more PEMs. return.
+		return rest, ErrNoPEMFound
+	}
+	f, err := CreateOutFile(ctx, prefix, blk.Type, OUTPUT_FILEEXT)
+	if err != nil {
+		return rest, err
+	}
+	fmt.Fprintf(os.Stdout, "%s\n", f.Name())
+	if verbose {
+		fmt.Fprintf(os.Stderr, "writing PEM %q as %q\n", blk.Type, f.Name())
+	}
+	_, err = f.Write(blk.Bytes)
+	err1 := f.Close()
+	err = errors.Join(err, err1)
+	return rest, err
+}
+
+// ExportPEMsAsFiles look for byte blocks encoded as PEM in bs.  Export byte blocks to files.
+func ExportPEMsAsFiles(ctx context.Context, verbose bool, prefix string, bs []byte) error {
+	var err error
+	for len(bs) > 0 && err == nil {
+		bs, err = ExportPEMAsFile(ctx, verbose, prefix, bs)
+	}
+	return err
+}
