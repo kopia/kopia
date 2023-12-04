eb2d8dc4e50393b41acb5edcdbfb261d5778afbd
lupusA <lupuapps@gmail.com>
2023-12-04 20:31:24 +0100

    Delete duplicates in time of day array

--- snapshot/policy/scheduling_policy.go
+++ snapshot/policy/scheduling_policy.go
@@ -1,10 +1,11 @@
 package policy
 
 import (
+	"cmp"
 	"context"
 	"fmt"
 	"reflect"
-	"sort"
+	"slices"
 	"strings"
 	"time"
 
@@ -43,16 +44,16 @@ func (t TimeOfDay) String() string {
 	return fmt.Sprintf("%v:%02v", t.Hour, t.Minute)
 }
 
-// SortAndDedupeTimesOfDay sorts the slice of times of day and removes duplicates.
+// SortAndDedupeTimesOfDay sorts the slice of times of day.
 func SortAndDedupeTimesOfDay(tod []TimeOfDay) []TimeOfDay {
-	sort.Slice(tod, func(i, j int) bool {
-		if a, b := tod[i].Hour, tod[j].Hour; a != b {
-			return a < b
+	slices.SortFunc(tod, func(a, b TimeOfDay) int {
+		if n := cmp.Compare(a.Hour, b.Hour); n != 0 {
+			return n
 		}
-		return tod[i].Minute < tod[j].Minute
+		// If hours are equal sort by minute
+		return cmp.Compare(a.Minute, b.Minute)
 	})
-
-	return tod
+	return slices.Compact[[]TimeOfDay, TimeOfDay](tod)
 }
 
 // SchedulingPolicy describes policy for scheduling snapshots.
