e5104a4e6959803847d05e7049f7897d1db26681
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-01-08 13:57:45 -0800

    refactor(general): add epoch.getKeyRange helper
    Extracted from #3651.
    
    Thanks to @plar and @redgoat650 for the suggestions.
    
    Ref:
    
    - #3603
    - #3645
    - #3638
    - #3639

--- internal/epoch/epoch_utils_test.go
+++ internal/epoch/epoch_utils_test.go
@@ -1,6 +1,8 @@
 package epoch
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -73,4 +75,112 @@ func TestGroupByEpochNumber(t *testing.T) {
 		got := groupByEpochNumber(tc.input)
 		require.Equal(t, tc.want, got)
 	}
+}
+
+func TestGetKeyRange(t *testing.T) {
+	cases := []struct {
+		input     map[int]bool
+		want      intRange
+		shouldErr bool
+		length    uint
+	}{
+		{},
+		{
+			input:  map[int]bool{-5: true},
+			want:   intRange{lo: -5, hi: -4},
+			length: 1,
+		},
+		{
+			input:  map[int]bool{-5: true, -4: true},
+			want:   intRange{lo: -5, hi: -3},
+			length: 2,
+		},
+		{
+			input:  map[int]bool{0: true},
+			want:   intRange{lo: 0, hi: 1},
+			length: 1,
+		},
+		{
+			input:  map[int]bool{5: true},
+			want:   intRange{lo: 5, hi: 6},
+			length: 1,
+		},
+		{
+			input:  map[int]bool{0: true, 1: true},
+			want:   intRange{lo: 0, hi: 2},
+			length: 2,
+		},
+		{
+			input:  map[int]bool{8: true, 9: true},
+			want:   intRange{lo: 8, hi: 10},
+			length: 2,
+		},
+		{
+			input:  map[int]bool{1: true, 2: true, 3: true, 4: true, 5: true},
+			want:   intRange{lo: 1, hi: 6},
+			length: 5,
+		},
+		{
+			input:     map[int]bool{8: true, 10: true},
+			want:      intRange{},
+			shouldErr: true,
+		},
+		{
+			input:     map[int]bool{1: true, 2: true, 3: true, 5: true},
+			want:      intRange{},
+			shouldErr: true,
+		},
+		{
+			input:     map[int]bool{-5: true, -7: true},
+			want:      intRange{},
+			shouldErr: true,
+		},
+		{
+			input:     map[int]bool{0: true, minInt: true},
+			want:      intRange{},
+			shouldErr: true,
+		},
+		{
+			input:     map[int]bool{0: true, maxInt: true},
+			want:      intRange{},
+			shouldErr: true,
+		},
+		{
+			input:     map[int]bool{maxInt: true, minInt: true},
+			want:      intRange{},
+			shouldErr: true,
+		},
+		{
+			input:  map[int]bool{minInt: true},
+			want:   intRange{lo: minInt, hi: minInt + 1},
+			length: 1,
+		},
+		{
+			input:  map[int]bool{maxInt - 1: true},
+			want:   intRange{lo: maxInt - 1, hi: maxInt},
+			length: 1,
+		},
+		{
+			input:  map[int]bool{maxInt: true},
+			want:   intRange{lo: maxInt, hi: minInt}, // not representable corner case :(
+			length: 1,
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprint("case: ", i), func(t *testing.T) {
+			got, err := getKeyRange(tc.input)
+			if tc.shouldErr {
+				require.Error(t, err, "input: %v", tc.input)
+			}
+
+			require.Equal(t, tc.want, got, "input: %#v", tc.input)
+			require.Equal(t, tc.length, got.length())
+		})
+	}
+}
+
+func TestAssertMinMaxIntConstants(t *testing.T) {
+	require.Equal(t, math.MinInt, minInt)
+	require.Equal(t, math.MaxInt, maxInt)
 }
