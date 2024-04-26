a2289cd895451bbf233ccbdc6d6a576d6f68f3cd
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-04-26 10:34:57 -0700

    refactor(general): unexport user.{scrypt,pbkdf2}HashAlgorithm consts

--- internal/user/user_profile.go
+++ internal/user/user_profile.go
@@ -12,13 +12,13 @@ import (
 const (
 	// ScryptHashVersion is the version representation of the scrypt algorithm.
 	ScryptHashVersion = 1
-	// ScryptHashAlgorithm is the scrypt password hashing algorithm. This must match crypto.ScryptAlgorithm.
-	ScryptHashAlgorithm = "scrypt-65536-8-1"
+	// scryptHashAlgorithm is the scrypt password hashing algorithm. This must match crypto.ScryptAlgorithm.
+	scryptHashAlgorithm = "scrypt-65536-8-1"
 
 	// Pbkdf2HashVersion is the version representation of the pbkdf2 algorithm.
 	Pbkdf2HashVersion = 2
-	// Pbkdf2HashAlgorithm is the pbkdf2 password hashing algorithm. This must match crypto.Pbkdf2Algorithm.
-	Pbkdf2HashAlgorithm = "pbkdf2-sha256-600000"
+	// pbkdf2HashAlgorithm is the pbkdf2 password hashing algorithm. This must match crypto.Pbkdf2Algorithm.
+	pbkdf2HashAlgorithm = "pbkdf2-sha256-600000"
 )
 
 // Profile describes information about a single user.
@@ -69,9 +69,9 @@ func (p *Profile) IsValidPassword(password string) bool {
 func GetPasswordHashAlgorithm(passwordHashVersion int) (string, error) {
 	switch passwordHashVersion {
 	case ScryptHashVersion:
-		return ScryptHashAlgorithm, nil
+		return scryptHashAlgorithm, nil
 	case Pbkdf2HashVersion:
-		return Pbkdf2HashAlgorithm, nil
+		return pbkdf2HashAlgorithm, nil
 	default:
 		return "", errors.Errorf("unsupported hash version (%d)", passwordHashVersion)
 	}
@@ -80,9 +80,9 @@ func GetPasswordHashAlgorithm(passwordHashVersion int) (string, error) {
 // GetPasswordHashVersion returns the password hash version given an algorithm.
 func GetPasswordHashVersion(passwordHashAlgorithm string) (int, error) {
 	switch passwordHashAlgorithm {
-	case ScryptHashAlgorithm:
+	case scryptHashAlgorithm:
 		return ScryptHashVersion, nil
-	case Pbkdf2HashAlgorithm:
+	case pbkdf2HashAlgorithm:
 		return Pbkdf2HashVersion, nil
 	default:
 		return 0, errors.Errorf("unsupported hash algorithm (%s)", passwordHashAlgorithm)
