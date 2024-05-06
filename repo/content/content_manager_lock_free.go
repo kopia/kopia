399a478476ded59bf1f1bf87cd9cd145026fb432
Prasad Ghangal <prasad.ganghal@veeam.com>
2024-05-06 19:00:42 +0530

    Disable metadata compression if repo flag is set
    Signed-off-by: Prasad Ghangal <prasad.ganghal@veeam.com>

--- repo/content/content_manager_lock_free.go
+++ repo/content/content_manager_lock_free.go
@@ -32,8 +32,11 @@ func (sm *SharedManager) maybeCompressAndEncryptDataForPacking(data gather.Bytes
 	// If the content is prefixed (which represents Kopia's own metadata as opposed to user data),
 	// and we're on V2 format or greater, enable internal compression even when not requested.
 	if contentID.HasPrefix() && comp == NoCompression && mp.IndexVersion >= index.Version2 {
-		// 'zstd-fastest' has a good mix of being fast, low memory usage and high compression for JSON.
-		comp = compression.HeaderZstdFastest
+		mp := sm.format.GetCachedMutableParameters()
+		if !mp.DisableMetadataCompression {
+			// 'zstd-fastest' has a good mix of being fast, low memory usage and high compression for JSON.
+			comp = compression.HeaderZstdFastest
+		}
 	}
 
 	//nolint:nestif
--- repo/format/content_format.go
+++ repo/format/content_format.go
@@ -63,10 +63,11 @@ func (f *ContentFormat) SupportsPasswordChange() bool {
 // MutableParameters represents parameters of the content manager that can be mutated after the repository
 // is created.
 type MutableParameters struct {
-	Version         Version          `json:"version,omitempty"`         // version number, must be "1", "2" or "3"
-	MaxPackSize     int              `json:"maxPackSize,omitempty"`     // maximum size of a pack object
-	IndexVersion    int              `json:"indexVersion,omitempty"`    // force particular index format version (1,2,..)
-	EpochParameters epoch.Parameters `json:"epochParameters,omitempty"` // epoch manager parameters
+	Version                    Version          `json:"version,omitempty"`                    // version number, must be "1", "2" or "3"
+	MaxPackSize                int              `json:"maxPackSize,omitempty"`                // maximum size of a pack object
+	IndexVersion               int              `json:"indexVersion,omitempty"`               // force particular index format version (1,2,..)
+	EpochParameters            epoch.Parameters `json:"epochParameters,omitempty"`            // epoch manager parameters
+	DisableMetadataCompression bool             `json:"disableMetadataCompression,omitempty"` // Disable metadata compression
 }
 
 // Validate validates the parameters.
