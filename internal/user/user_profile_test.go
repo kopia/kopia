3278d83b3f8754f85e4a9447839a7ec920e57b8b
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-06-06 21:35:42 -0700

    refactor(general): use 'require' in user profile tests

--- internal/user/user_profile_test.go
+++ internal/user/user_profile_test.go
@@ -3,6 +3,8 @@ package user_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/kopia/kopia/internal/user"
 )
 
@@ -11,25 +13,25 @@ func TestUserProfile(t *testing.T) {
 		PasswordHashVersion: user.ScryptHashVersion,
 	}
 
-	if p.IsValidPassword("bar") {
-		t.Fatalf("password unexpectedly valid!")
-	}
+	isValid := p.IsValidPassword("bar")
+
+	require.False(t, isValid, "password unexpectedly valid!")
 
 	p.SetPassword("foo")
 
-	if !p.IsValidPassword("foo") {
-		t.Fatalf("password not valid!")
-	}
+	isValid = p.IsValidPassword("foo")
 
-	if p.IsValidPassword("bar") {
-		t.Fatalf("password unexpectedly valid!")
-	}
+	require.True(t, isValid, "password not valid!")
+
+	isValid = p.IsValidPassword("bar")
+
+	require.False(t, isValid, "password unexpectedly valid!")
 
 	// Different key derivation algorithm besides the original should fail
 	p.PasswordHashVersion = user.Pbkdf2HashVersion
-	if p.IsValidPassword("foo") {
-		t.Fatalf("password unexpectedly valid!")
-	}
+	isValid = p.IsValidPassword("foo")
+
+	require.False(t, isValid, "password unexpectedly valid!")
 }
 
 func TestBadPasswordHashVersion(t *testing.T) {
@@ -37,21 +39,23 @@ func TestBadPasswordHashVersion(t *testing.T) {
 	p := &user.Profile{
 		PasswordHashVersion: user.ScryptHashVersion,
 	}
+
 	p.SetPassword("foo")
 	// Assume the key derivation algorithm is bad. This will cause
 	// a panic when validating
 	p.PasswordHashVersion = 0
-	if p.IsValidPassword("foo") {
-		t.Fatalf("password unexpectedly valid!")
-	}
+
+	isValid := p.IsValidPassword("foo")
+
+	require.False(t, isValid, "password unexpectedly valid!")
 }
 
 func TestNilUserProfile(t *testing.T) {
 	var p *user.Profile
 
-	if p.IsValidPassword("bar") {
-		t.Fatalf("password unexpectedly valid!")
-	}
+	isValid := p.IsValidPassword("bar")
+
+	require.False(t, isValid, "password unexpectedly valid!")
 }
 
 func TestInvalidPasswordHash(t *testing.T) {
@@ -65,8 +69,8 @@ func TestInvalidPasswordHash(t *testing.T) {
 			PasswordHash:        tc,
 			PasswordHashVersion: 1,
 		}
-		if p.IsValidPassword("some-password") {
-			t.Fatalf("password unexpectedly valid for %v", tc)
-		}
+		isValid := p.IsValidPassword("some-password")
+
+		require.False(t, isValid, "password unexpectedly valid for %v", tc)
 	}
 }
