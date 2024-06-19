1064e445ea06c28cc136128b1e695387c6ac8473
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-06-18 21:37:43 -0700

    fix format in Errorf

--- internal/crypto/aesgcm.go
+++ internal/crypto/aesgcm.go
@@ -65,7 +65,7 @@ func DecryptAes256Gcm(data, masterKey, salt []byte) ([]byte, error) {
 
 	data = append([]byte(nil), data...)
 	if len(data) < aead.NonceSize() {
-		return nil, errors.Errorf("invalid encrypted payload, too short")
+		return nil, errors.New("invalid encrypted payload, too short")
 	}
 
 	nonce := data[0:aead.NonceSize()]
@@ -73,7 +73,7 @@ func DecryptAes256Gcm(data, masterKey, salt []byte) ([]byte, error) {
 
 	plainText, err := aead.Open(payload[:0], nonce, payload, authData)
 	if err != nil {
-		return nil, errors.Errorf("unable to decrypt repository blob, invalid credentials?")
+		return nil, errors.New("unable to decrypt repository blob, invalid credentials?")
 	}
 
 	return plainText, nil
