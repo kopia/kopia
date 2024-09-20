cbbc60be4bfe7d3dfab414e14b01ff869cc77810
Alessandro Viganò <alvistar@gmail.com>
2024-09-20 11:59:15 +0200

    Added e2e for setting policy metric for specific path

--- tests/end_to_end_test/server_metrics_test.go
+++ tests/end_to_end_test/server_metrics_test.go
@@ -121,7 +121,7 @@ func TestServerMetrics(t *testing.T) {
 	require.NotContains(t, metrics, fmt.Sprintf(`kopia_last_snapshot_dirs{host="fake-hostname",path=%q,username="fake-username"}`, sharedTestDataDir2))
 }
 
-func TestServerMetricsWithDisablePolicy(t *testing.T) {
+func TestServerMetricsWithGlobalDisablePolicy(t *testing.T) {
 	ctx := testlogging.Context(t)
 
 	runner := testenv.NewInProcRunner(t)
@@ -201,4 +201,79 @@ func TestServerMetricsWithDisablePolicy(t *testing.T) {
 	for _, path := range expectedPaths {
 		require.NotContains(t, metrics, fmt.Sprintf(`kopia_last_snapshot_dirs{host="fake-hostname",path=%q,username="fake-username"}`, path))
 	}
+}
+
+func TestServerMetricsWithSpecificDisablePolicy(t *testing.T) {
+	ctx := testlogging.Context(t)
+
+	runner := testenv.NewInProcRunner(t)
+	e := testenv.NewCLITest(t, testenv.RepoFormatNotImportant, runner)
+
+	defer e.RunAndExpectSuccess(t, "repo", "disconnect")
+
+	e.RunAndExpectSuccess(t, "repo", "create", "filesystem", "--path", e.RepoDir, "--override-hostname=fake-hostname", "--override-username=fake-username")
+
+	e.RunAndExpectSuccess(t, "policy", "set", "--expose-metrics", "false", sharedTestDataDir2)
+
+	e.RunAndExpectSuccess(t, "snapshot", "create", sharedTestDataDir1)
+	e.RunAndExpectSuccess(t, "snapshot", "create", sharedTestDataDir2)
+
+	var sp testutil.ServerParameters
+
+	wait, _ := e.RunAndProcessStderr(t, sp.ProcessOutput,
+		"server", "start",
+		"--ui",
+		"--address=localhost:0",
+		"--random-password",
+		"--random-server-control-password",
+		"--tls-generate-cert",
+		"--tls-generate-rsa-key-size=2048",      // use shorter key size to speed up generation
+		"--metrics-listen-addr=localhost:10000", // enable metrics
+	)
+
+	defer wait()
+
+	t.Logf("detected server parameters %#v", sp)
+
+	cli, err := apiclient.NewKopiaAPIClient(apiclient.Options{
+		BaseURL:                             sp.BaseURL,
+		Username:                            "kopia",
+		Password:                            sp.Password,
+		TrustedServerCertificateFingerprint: sp.SHA256Fingerprint,
+		LogRequests:                         true,
+	})
+	require.NoError(t, err)
+	require.NoError(t, cli.FetchCSRFTokenForTesting(ctx))
+
+	controlClient, err := apiclient.NewKopiaAPIClient(apiclient.Options{
+		BaseURL:                             sp.BaseURL,
+		Username:                            "server-control",
+		Password:                            sp.ServerControlPassword,
+		TrustedServerCertificateFingerprint: sp.SHA256Fingerprint,
+		LogRequests:                         true,
+	})
+	require.NoError(t, err)
+
+	defer serverapi.Shutdown(ctx, controlClient)
+
+	waitUntilServerStarted(ctx, t, controlClient)
+
+	// Check response on the captured metrics address
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://"+sp.MetricsAddress+"/metrics", http.NoBody)
+	require.NoError(t, err)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	// Response body should not be empty
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	require.NotEmpty(t, body)
+	metrics := string(body)
+
+	// Only sharedTestDataDir should be present
+	require.Contains(t, metrics, fmt.Sprintf(`kopia_last_snapshot_dirs{host="fake-hostname",path=%q,username="fake-username"}`, sharedTestDataDir1))
+	require.NotContains(t, metrics, fmt.Sprintf(`kopia_last_snapshot_dirs{host="fake-hostname",path=%q,username="fake-username"}`, sharedTestDataDir2))
 }
