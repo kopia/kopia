0c762355c43dbc6c13996078dd5d19557046bf15
KastenMike <93532247+KastenMike@users.noreply.github.com>
2024-09-26 18:03:08 +0200

    feat(repository): Google Immutability - PITR/versioning tests

--- repo/blob/gcs/gcs_storage_test.go
+++ repo/blob/gcs/gcs_storage_test.go
@@ -7,10 +7,13 @@ import (
 	"encoding/base64"
 	"io"
 	"os"
+	"strings"
 	"testing"
 
+	gcsclient "cloud.google.com/go/storage"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/api/option"
 
 	"github.com/kopia/kopia/internal/blobtesting"
 	"github.com/kopia/kopia/internal/providervalidation"
@@ -19,6 +22,72 @@ import (
 	"github.com/kopia/kopia/repo/blob"
 	"github.com/kopia/kopia/repo/blob/gcs"
 )
+
+const (
+	testBucketEnv                 = "KOPIA_GCS_TEST_BUCKET"
+	testBucketProjectID           = "KOPIA_GCS_TEST_PROJECT_ID"
+	testBucketCredentialsFile     = "KOPIA_GCS_CREDENTIALS_FILE"
+	testBucketCredentialsJSONGzip = "KOPIA_GCS_CREDENTIALS_JSON_GZIP"
+	testImmutableBucketEnv        = "KOPIA_GCS_TEST_IMMUTABLE_BUCKET"
+)
+
+type bucketOpts struct {
+	bucket          string
+	credentialsFile string
+	projectID       string
+	isLockedBucket  bool
+}
+
+func createBucket(t *testing.T, opts bucketOpts) {
+	t.Helper()
+	ctx := context.Background()
+
+	cli, err := gcsclient.NewClient(ctx, option.WithCredentialsFile(opts.credentialsFile))
+	if err != nil {
+		t.Fatalf("unable to create GCS client: %v", err)
+	}
+
+	attrs := &gcsclient.BucketAttrs{}
+
+	bucketHandle := cli.Bucket(opts.bucket)
+	if opts.isLockedBucket {
+		attrs.VersioningEnabled = true
+		bucketHandle = bucketHandle.SetObjectRetention(true)
+	}
+
+	err = bucketHandle.Create(ctx, opts.projectID, attrs)
+	if err == nil {
+		return
+	}
+
+	if strings.Contains(err.Error(), "The requested bucket name is not available") {
+		return
+	}
+
+	if strings.Contains(err.Error(), "Your previous request to create the named bucket succeeded and you already own it") {
+		return
+	}
+
+	t.Fatalf("issue creating bucket: %v", err)
+}
+
+func validateBucket(t *testing.T, opts bucketOpts) {
+	t.Helper()
+	ctx := context.Background()
+
+	cli, err := gcsclient.NewClient(ctx, option.WithCredentialsFile(opts.credentialsFile))
+	if err != nil {
+		t.Fatalf("unable to create GCS client: %v", err)
+	}
+
+	attrs, err := cli.Bucket(opts.bucket).Attrs(ctx)
+	require.NoError(t, err)
+
+	if opts.isLockedBucket {
+		require.True(t, attrs.VersioningEnabled)
+		require.Equal(t, "Enabled", attrs.ObjectRetentionMode)
+	}
+}
 
 func TestCleanupOldData(t *testing.T) {
 	t.Parallel()
@@ -59,16 +128,13 @@ func TestGCSStorageInvalid(t *testing.T) {
 	t.Parallel()
 	testutil.ProviderTest(t)
 
-	bucket := os.Getenv("KOPIA_GCS_TEST_BUCKET")
-	if bucket == "" {
-		t.Skip("KOPIA_GCS_TEST_BUCKET not provided")
-	}
+	bucket := os.Getenv(testBucketEnv)
 
 	ctx := testlogging.Context(t)
 
 	if _, err := gcs.New(ctx, &gcs.Options{
 		BucketName:                    bucket + "-no-such-bucket",
-		ServiceAccountCredentialsFile: os.Getenv("KOPIA_GCS_CREDENTIALS_FILE"),
+		ServiceAccountCredentialsFile: os.Getenv(testBucketCredentialsFile),
 	}, false); err == nil {
 		t.Fatalf("unexpected success connecting to GCS, wanted error")
 	}
@@ -88,12 +154,12 @@ func gunzip(d []byte) ([]byte, error) {
 func mustGetOptionsOrSkip(t *testing.T, prefix string) *gcs.Options {
 	t.Helper()
 
-	bucket := os.Getenv("KOPIA_GCS_TEST_BUCKET")
+	bucket := os.Getenv(testBucketEnv)
 	if bucket == "" {
 		t.Skip("KOPIA_GCS_TEST_BUCKET not provided")
 	}
 
-	credDataGZ, err := base64.StdEncoding.DecodeString(os.Getenv("KOPIA_GCS_CREDENTIALS_JSON_GZIP"))
+	credDataGZ, err := base64.StdEncoding.DecodeString(os.Getenv(testBucketCredentialsJSONGzip))
 	if err != nil {
 		t.Skip("skipping test because GCS credentials file can't be decoded")
 	}
@@ -108,4 +174,18 @@ func mustGetOptionsOrSkip(t *testing.T, prefix string) *gcs.Options {
 		ServiceAccountCredentialJSON: credData,
 		Prefix:                       prefix,
 	}
+}
+
+func getBlobCount(ctx context.Context, t *testing.T, st blob.Storage, prefix blob.ID) int {
+	t.Helper()
+
+	var count int
+
+	err := st.ListBlobs(ctx, prefix, func(bm blob.Metadata) error {
+		count++
+		return nil
+	})
+	require.NoError(t, err)
+
+	return count
 }
