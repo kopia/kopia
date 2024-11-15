f0be8a0ea40c1ce059a36b40683a5c9449bfd55b
Jarek Kowalski <jaak@jkowalski.net>
2024-11-14 21:51:30 -0800

    feat(cli): improved snapshot reports

--- notification/notifytemplate/embeddedtemplate.go
+++ notification/notifytemplate/embeddedtemplate.go
@@ -3,8 +3,10 @@ package notifytemplate
 
 import (
 	"embed"
+	"fmt"
 	"slices"
 	"sort"
+	"strconv"
 	"text/template"
 	"time"
 
@@ -29,6 +31,10 @@ type Options struct {
 	TimeFormat string
 }
 
+func formatCount(v int64) string {
+	return strconv.FormatInt(v, 10)
+}
+
 // functions is a map of functions that can be used in templates.
 func functions(opt Options) template.FuncMap {
 	if opt.Timezone == nil {
@@ -40,11 +46,52 @@ func functions(opt Options) template.FuncMap {
 	}
 
 	return template.FuncMap{
-		"bytes": units.BytesString[int64],
+		"bytes":       units.BytesString[int64],
+		"formatCount": formatCount,
+		"bytesDelta": func(v int64) string {
+			switch {
+			case v == 0:
+				return ""
+			case v > 0:
+				return " (+" + units.BytesString(v) + ")"
+			default:
+				return " (-" + units.BytesString(-v) + ")"
+			}
+		},
+		"bytesDeltaHTML": func(v int64) string {
+			switch {
+			case v == 0:
+				return ""
+			case v > 0:
+				return " <span class='increase'>(&#x2191; " + units.BytesString(v) + ")</span>"
+			default:
+				return " <span class='decrease'>(&#x2193; " + units.BytesString(-v) + ")</span>"
+			}
+		},
+		"countDelta": func(v int64) string {
+			switch {
+			case v == 0:
+				return ""
+			case v > 0:
+				return fmt.Sprintf(" (+%v)", formatCount(v))
+			default:
+				return fmt.Sprintf(" (-%v)", formatCount(-v))
+			}
+		},
+		"countDeltaHTML": func(v int64) string {
+			switch {
+			case v == 0:
+				return ""
+			case v > 0:
+				return fmt.Sprintf(" <span class='increase'>(&#x2191; %v)</span>", formatCount(v))
+			default:
+				return fmt.Sprintf(" <span class='decrease'>(&#x2193; %v)</span>", formatCount(-v))
+			}
+		},
 		"sortSnapshotManifestsByName": func(man []*notifydata.ManifestWithError) []*notifydata.ManifestWithError {
 			res := slices.Clone(man)
 			sort.Slice(res, func(i, j int) bool {
-				return res[i].Source.String() < res[j].Source.String()
+				return res[i].Manifest.Source.String() < res[j].Manifest.Source.String()
 			})
 			return res
 		},
