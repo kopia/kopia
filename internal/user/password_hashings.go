8e69b9c0dcb8e813c4af62a450da61e5e4f1d9dc
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-11-17 23:08:30 -0800

    remove previous workaround for unset hash version

--- internal/user/password_hashings.go
+++ internal/user/password_hashings.go
@@ -5,8 +5,7 @@ import "github.com/pkg/errors"
 // getPasswordHashAlgorithm returns the password hash algorithm given a version.
 func getPasswordHashAlgorithm(passwordHashVersion int) (string, error) {
 	switch passwordHashVersion {
-	// when the version is unsetDefaulHashVersion, map it to ScryptHashVersion
-	case unsetDefaulHashVersion, ScryptHashVersion:
+	case ScryptHashVersion:
 		return scryptHashAlgorithm, nil
 	case Pbkdf2HashVersion:
 		return pbkdf2HashAlgorithm, nil
