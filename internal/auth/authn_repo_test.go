37fc9dfca39eda9fe01dbf194b0024c695d08994
Julio <1953782+julio-lopez@users.noreply.github.com>
2024-11-17 21:48:08 -0800

    split repository authenticator tests

--- internal/auth/authn_repo_test.go
+++ internal/auth/authn_repo_test.go
@@ -2,6 +2,7 @@ package auth_test
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,49 +20,21 @@ func TestRepositoryAuthenticator(t *testing.T) {
 
 	require.NoError(t, repo.WriteSession(ctx, env.Repository, repo.WriteSessionOptions{},
 		func(ctx context.Context, w repo.RepositoryWriter) error {
-			for _, testProfile := range []struct {
-				profile  *user.Profile
-				password string
-			}{
-				{
-					profile: &user.Profile{
-						Username:            "user1@host1",
-						PasswordHashVersion: user.ScryptHashVersion,
-					},
-					password: "password1",
-				},
-				{
-					profile: &user.Profile{
-						Username:            "user2@host2",
-						PasswordHashVersion: user.ScryptHashVersion,
-					},
-					password: "password2",
-				},
-				{
-					profile: &user.Profile{
-						Username: "user3@host3",
-						// PasswordHashVersion is not set
-					},
-					password: "password3",
-				},
-				{
-					profile: &user.Profile{
-						Username:            "user4@host4",
-						PasswordHashVersion: user.Pbkdf2HashVersion,
-					},
-					password: "password4",
-				},
-			} {
-				err := testProfile.profile.SetPassword(testProfile.password)
-				if err != nil {
-					return err
-				}
-
-				err = user.SetUserProfile(ctx, w, testProfile.profile)
-				if err != nil {
-					return err
-				}
+			testProfile := user.Profile{
+				Username:            "user1@host1",
+				PasswordHashVersion: user.ScryptHashVersion,
 			}
+
+			err := testProfile.SetPassword("password1")
+			if err != nil {
+				return err
+			}
+
+			err = user.SetUserProfile(ctx, w, &testProfile)
+			if err != nil {
+				return err
+			}
+
 			return nil
 		}))
 
@@ -75,13 +48,57 @@ func TestRepositoryAuthenticator(t *testing.T) {
 	verifyRepoAuthenticator(ctx, t, a, env.Repository, "user1@host1a", "password1", false)
 	// invalid user, invalid password
 	verifyRepoAuthenticator(ctx, t, a, env.Repository, "user1@host1a", "password1a", false)
+}
 
-	// user profile with ScryptHashVersion
-	verifyRepoAuthenticator(ctx, t, a, env.Repository, "user2@host2", "password2", true)
-	// user profile with no PasswordHashVersion set
-	verifyRepoAuthenticator(ctx, t, a, env.Repository, "user3@host3", "password3", true)
-	// user profile with Pbkdf2HashVersion
-	verifyRepoAuthenticator(ctx, t, a, env.Repository, "user4@host4", "password4", true)
+func TestRepositoryAuthenticatorPasswordHashVersion(t *testing.T) {
+	for _, tc := range []struct {
+		profile  *user.Profile
+		password string
+	}{
+		{
+			profile: &user.Profile{
+				Username:            "user2@host2",
+				PasswordHashVersion: user.ScryptHashVersion,
+			},
+			password: "password2",
+		},
+		{
+			profile: &user.Profile{
+				Username: "user3@host3",
+				// PasswordHashVersion is not set
+			},
+			password: "password3",
+		},
+		{
+			profile: &user.Profile{
+				Username:            "user4@host4",
+				PasswordHashVersion: user.Pbkdf2HashVersion,
+			},
+			password: "password4",
+		},
+	} {
+		t.Run(strconv.Itoa(tc.profile.PasswordHashVersion), func(t *testing.T) {
+			a := auth.AuthenticateRepositoryUsers()
+			ctx, env := repotesting.NewEnvironment(t, repotesting.FormatNotImportant)
+
+			require.NoError(t, repo.WriteSession(ctx, env.Repository, repo.WriteSessionOptions{},
+				func(ctx context.Context, w repo.RepositoryWriter) error {
+					err := tc.profile.SetPassword(tc.password)
+					if err != nil {
+						return err
+					}
+
+					err = user.SetUserProfile(ctx, w, tc.profile)
+					if err != nil {
+						return err
+					}
+
+					return nil
+				}))
+
+			verifyRepoAuthenticator(ctx, t, a, env.Repository, tc.profile.Username, tc.password, true)
+		})
+	}
 }
 
 func verifyRepoAuthenticator(ctx context.Context, t *testing.T, a auth.Authenticator, r repo.Repository, username, password string, want bool) {
