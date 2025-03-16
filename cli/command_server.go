1bac34f51cb290d0cd9618ad1ee48cd171b138f0
Daniel Lambert <danjl1100@gmail.com>
2025-03-16 09:33:29 -0500

    update server args and env vars for UI credentials

--- cli/command_server.go
+++ cli/command_server.go
@@ -10,6 +10,10 @@ import (
 	"github.com/kopia/kopia/internal/apiclient"
 )
 
+const (
+	defaultServerUIUsername = "kopia"
+)
+
 type commandServer struct {
 	acl      commandServerACL
 	user     commandServerUser
@@ -26,15 +30,42 @@ type commandServer struct {
 }
 
 type serverFlags struct {
-	serverAddress  string
-	serverUsername string
-	serverPassword string
+	serverAddress            string
+	serverUsername           string
+	serverPassword           string
+	serverUsernameDeprecated string
+	serverPasswordDeprecated string
 }
 
 func (c *serverFlags) setup(svc appServices, cmd *kingpin.CmdClause) {
 	cmd.Flag("address", "Server address").Default("http://127.0.0.1:51515").StringVar(&c.serverAddress)
-	cmd.Flag("server-username", "HTTP server username (basic auth)").Envar(svc.EnvName("KOPIA_SERVER_USERNAME")).Default("kopia").StringVar(&c.serverUsername)
-	cmd.Flag("server-password", "HTTP server password (basic auth)").Envar(svc.EnvName("KOPIA_SERVER_PASSWORD")).StringVar(&c.serverPassword)
+	cmd.Flag("server-ui-username", "HTTP server username (basic auth)").Envar(svc.EnvName("KOPIA_SERVER_UI_USERNAME")).StringVar(&c.serverUsername)
+	cmd.Flag("server-ui-password", "HTTP server password (basic auth)").Envar(svc.EnvName("KOPIA_SERVER_UI_PASSWORD")).StringVar(&c.serverPassword)
+
+	cmd.Flag("server-username", "HTTP server username (basic auth)").Hidden().Envar(svc.EnvName("KOPIA_SERVER_USERNAME")).StringVar(&c.serverUsernameDeprecated)
+	cmd.Flag("server-password", "HTTP server password (basic auth)").Hidden().Envar(svc.EnvName("KOPIA_SERVER_PASSWORD")).StringVar(&c.serverPasswordDeprecated)
+}
+
+func (c *serverFlags) mergeDeprecatedFlags(stderrWriter io.Writer) error {
+	username, err := mergeDeprecatedFlags(stderrWriter, c.serverUsernameDeprecated, c.serverUsername, "--server-username", "KOPIA_SERVER_USERNAME", "--server-ui-username", "KOPIA_SERVER_UI_USERNAME")
+	if err != nil {
+		return err
+	}
+
+	if username == "" {
+		username = defaultServerUIUsername
+	}
+
+	c.serverUsername = username
+
+	password, err := mergeDeprecatedFlags(stderrWriter, c.serverPasswordDeprecated, c.serverPassword, "--server-password", "KOPIA_SERVER_PASSWORD", "--server-ui-password", "KOPIA_SERVER_UI_PASSWORD")
+	if err != nil {
+		return err
+	}
+
+	c.serverPassword = password
+
+	return nil
 }
 
 type serverClientFlags struct {
--- tests/compat_test/compat_test.go
+++ tests/compat_test/compat_test.go
@@ -1,6 +1,10 @@
 package compat_test
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -8,6 +12,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/kopia/kopia/internal/apiclient"
 	"github.com/kopia/kopia/internal/testutil"
 	"github.com/kopia/kopia/tests/testenv"
 )
@@ -203,7 +208,7 @@ func TestClientConnectedUsingV017CanConnectUsingCurrent(t *testing.T) {
 	e2.RunAndExpectSuccess(t, "snapshot", "ls")
 }
 
-// Verify `server status` environment variables compatibility for *control* username/password.
+// Verify `server status` compatibility for *control* username/password environment variables.
 func TestServerControlArgs(t *testing.T) {
 	t.Parallel()
 
@@ -282,7 +287,7 @@ func TestServerControlArgs(t *testing.T) {
 	)
 }
 
-// Verify `server start` environment variables compatibility for *control* username/password.
+// Verify `server start` compatibility for *control* username/password arguments and environment variables.
 func TestServerStartControlArgs(t *testing.T) {
 	t.Parallel()
 
@@ -329,6 +334,8 @@ func TestServerStartControlArgs(t *testing.T) {
 	t.Logf("detected server parameters %#v", spExePrevEnvPrev)
 
 	defer wait()
+	// prevent test hang if an assertion fails before reaching `killServer` call
+	defer killServer()
 
 	time.Sleep(3 * time.Second)
 
@@ -364,6 +371,8 @@ func TestServerStartControlArgs(t *testing.T) {
 	t.Logf("detected server parameters %#v", spExeNewEnvPrev)
 
 	defer wait()
+	// prevent test hang if an assertion fails before reaching `killServer` call
+	defer killServer()
 
 	time.Sleep(3 * time.Second)
 
@@ -407,4 +416,187 @@ func TestServerStartControlArgs(t *testing.T) {
 		"--address", spExeNewEnvNew.BaseURL+"/",
 		"--server-cert-fingerprint", spExeNewEnvNew.SHA256Fingerprint,
 	)
+}
+
+// Verify `server start` environment variables compatibility for *UI* username/password.
+func TestServerStartUIArgs(t *testing.T) {
+	t.Parallel()
+
+	if kopiaCurrentExe == "" {
+		t.Skip()
+	}
+
+	runnerCurrent := testenv.NewExeRunnerWithBinary(t, kopiaCurrentExe)
+
+	// create repository and start a server
+	e1 := testenv.NewCLITest(t, testenv.RepoFormatNotImportant, runnerCurrent)
+	e1.RunAndExpectSuccess(t, "repo", "create", "filesystem", "--path", e1.RepoDir)
+	e1.RunAndExpectSuccess(t, "server", "users", "add", "foo@bar", "--user-password", "baz")
+
+	// Use different key files for each `server start` invocation
+	// since ServerParameters parses output from TLS key generation
+	tlsCert1 := filepath.Join(e1.ConfigDir, "tls1.cert")
+	tlsCert2 := filepath.Join(e1.ConfigDir, "tls2.cert")
+	tlsCert3 := filepath.Join(e1.ConfigDir, "tls3.cert")
+	tlsCert4 := filepath.Join(e1.ConfigDir, "tls4.cert")
+	tlsKey1 := filepath.Join(e1.ConfigDir, "tls1.key")
+	tlsKey2 := filepath.Join(e1.ConfigDir, "tls2.key")
+	tlsKey3 := filepath.Join(e1.ConfigDir, "tls3.key")
+	tlsKey4 := filepath.Join(e1.ConfigDir, "tls4.key")
+
+	var spArgPrev testutil.ServerParameters
+
+	wait, killServer := e1.RunAndProcessStderr(t, spArgPrev.ProcessOutput,
+		"server", "start",
+		"--ui",
+		"--address=localhost:0",
+		"--server-control-username=admin-user",
+		"--server-control-password=admin-pwd",
+		"--tls-generate-cert",
+		"--tls-key-file", tlsKey1,
+		"--tls-cert-file", tlsCert1,
+		"--tls-generate-rsa-key-size=2048", // use shorter key size to speed up generation
+		// set v0.22 `server start` arguments
+		"--server-username=ui-user-argPrev",
+		"--server-password=ui-pwd-argPrev",
+	)
+
+	t.Logf("detected server parameters %#v", spArgPrev)
+
+	defer wait()
+	// prevent test hang if an assertion fails before reaching `killServer` call
+	defer killServer()
+
+	time.Sleep(3 * time.Second)
+
+	// check server UI connects
+	verifyUICredentials(t, spArgPrev, "ui-user-argPrev", "ui-pwd-argPrev")
+
+	// restart server using v0.22 environment variables
+	killServer()
+
+	var spEnvPrev testutil.ServerParameters
+
+	// set v0.22 `server start` environment variables
+	e1.Environment["KOPIA_SERVER_USERNAME"] = "ui-user-envPrev"
+	e1.Environment["KOPIA_SERVER_PASSWORD"] = "ui-pwd-envPrev"
+
+	wait, killServer = e1.RunAndProcessStderr(t, spEnvPrev.ProcessOutput,
+		"server", "start",
+		"--ui",
+		"--address=localhost:0",
+		"--server-control-username=admin-user",
+		"--server-control-password=admin-pwd",
+		"--tls-generate-cert",
+		"--tls-key-file", tlsKey2,
+		"--tls-cert-file", tlsCert2,
+		"--tls-generate-rsa-key-size=2048", // use shorter key size to speed up generation
+	)
+
+	t.Logf("detected server parameters %#v", spEnvPrev)
+
+	defer wait()
+	// prevent test hang if an assertion fails before reaching `killServer` call
+	defer killServer()
+
+	time.Sleep(3 * time.Second)
+
+	// check server UI connects
+	verifyUICredentials(t, spEnvPrev, "ui-user-envPrev", "ui-pwd-envPrev")
+
+	// restart server using post-0.22 `server start` arguments
+	killServer()
+	delete(e1.Environment, "KOPIA_SERVER_USERNAME")
+	delete(e1.Environment, "KOPIA_SERVER_PASSWORD")
+
+	var spArgNew testutil.ServerParameters
+
+	wait, killServer = e1.RunAndProcessStderr(t, spArgNew.ProcessOutput,
+		"server", "start",
+		"--ui",
+		"--address=localhost:0",
+		"--server-control-username=admin-user",
+		"--server-control-password=admin-pwd",
+		"--tls-generate-cert",
+		"--tls-key-file", tlsKey3,
+		"--tls-cert-file", tlsCert3,
+		"--tls-generate-rsa-key-size=2048", // use shorter key size to speed up generation
+		// set post-0.22 `server start` arguments
+		"--server-ui-username=ui-user-argNew",
+		"--server-ui-password=ui-pwd-argNew",
+	)
+
+	t.Logf("detected server parameters %#v", spArgNew)
+
+	defer wait()
+	// prevent test hang if an assertion fails before reaching `killServer` call
+	defer killServer()
+
+	time.Sleep(3 * time.Second)
+
+	// check server UI connects
+	verifyUICredentials(t, spArgNew, "ui-user-argNew", "ui-pwd-argNew")
+
+	// restart server using post-0.22 `server start` environment variables
+	killServer()
+
+	e1.Environment["KOPIA_SERVER_UI_USERNAME"] = "ui-user-envNew"
+	e1.Environment["KOPIA_SERVER_UI_PASSWORD"] = "ui-pwd-envNew"
+
+	var spEnvNew testutil.ServerParameters
+
+	wait, killServer = e1.RunAndProcessStderr(t, spEnvNew.ProcessOutput,
+		"server", "start",
+		"--ui",
+		"--address=localhost:0",
+		"--server-control-username=admin-user",
+		"--server-control-password=admin-pwd",
+		"--tls-generate-cert",
+		"--tls-key-file", tlsKey4,
+		"--tls-cert-file", tlsCert4,
+		"--tls-generate-rsa-key-size=2048", // use shorter key size to speed up generation
+	)
+
+	t.Logf("detected server parameters %#v", spEnvNew)
+
+	defer wait()
+	defer killServer()
+
+	time.Sleep(3 * time.Second)
+
+	// check server UI connects
+	verifyUICredentials(t, spEnvNew, "ui-user-envNew", "ui-pwd-envNew")
+}
+
+func verifyUICredentials(t *testing.T, sp testutil.ServerParameters, username, password string) {
+	t.Helper()
+
+	require.NotEmpty(t, sp.BaseURL, "ServerParameters must have non-empty BaseURL, check `kopia server start` stderr via KOPIA_TEST_LOG_OUTPUT=1")
+
+	cli, err := apiclient.NewKopiaAPIClient(apiclient.Options{
+		BaseURL:                             sp.BaseURL,
+		Username:                            username,
+		Password:                            password,
+		TrustedServerCertificateFingerprint: sp.SHA256Fingerprint,
+		LogRequests:                         true,
+	})
+	require.NoError(t, err)
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, sp.BaseURL, http.NoBody)
+	require.NoError(t, err)
+
+	resp, err := cli.HTTPClient.Do(req)
+	require.NoError(t, err)
+
+	fmt.Println("CLIENT CONNECTED: ", resp)
+
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	fmt.Println("CLIENT READ BODY: ", string(b))
+
+	// make sure the credentials were accepted (e.g. HTTP 200 OK status)
+	require.Equal(t, 200, resp.StatusCode, "UI request did not succeed: %v.", string(b))
 }
