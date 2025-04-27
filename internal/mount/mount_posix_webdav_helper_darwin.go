52b05895e3f334c72e53a74ee3d5ca3e2773caa9
J <1953782+julio-lopez@users.noreply.github.com>
2025-04-26 22:39:24 -0700

    remove unused ctx parameter

--- internal/mount/mount_posix_webdav_helper_darwin.go
+++ internal/mount/mount_posix_webdav_helper_darwin.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-func mountWebDavHelper(ctx context.Context, url, path string) error {
+func mountWebDavHelper(_ context.Context, url, path string) error {
 	mount := exec.Command("/sbin/mount", "-t", "webdav", "-r", url, path)
 	if err := mount.Run(); err != nil {
 		return errors.Errorf("webdav mount %q on %q failed: %v", url, path, err)
@@ -16,7 +16,7 @@ func mountWebDavHelper(ctx context.Context, url, path string) error {
 	return nil
 }
 
-func unmountWebDevHelper(ctx context.Context, path string) error {
+func unmountWebDevHelper(_ context.Context, path string) error {
 	unmount := exec.Command("/usr/sbin/diskutil", "unmount", path)
 	if err := unmount.Run(); err != nil {
 		return errors.Errorf("unmount %q failed: %v", path, err)
