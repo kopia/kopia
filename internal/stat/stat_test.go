67b5fb58711de8819fb2dbeb3455eb8c02360dde
J <1953782+julio-lopez@users.noreply.github.com>
2025-06-11 11:34:51 -0700

    nit: require in stat_test

--- internal/stat/stat_test.go
+++ internal/stat/stat_test.go
@@ -13,30 +13,24 @@ import (
 )
 
 func TestGetBlockSize(t *testing.T) {
-	s, err := GetBlockSize(os.DevNull)
+	size, err := GetBlockSize(os.DevNull)
 	require.NoError(t, err)
 
-	if s <= 0 {
-		t.Fatalf("invalid disk block size: %d, must be greater than 0", s)
-	}
+	require.Positive(t, size)
 }
 
 func TestGetFileAllocSize(t *testing.T) {
-	const size = 4096
+	const writeSize = 4096
 
 	d := t.TempDir()
 	f := filepath.Join(d, "test")
-	data := bytes.Repeat([]byte{1}, size)
+	data := bytes.Repeat([]byte{1}, writeSize)
 
 	err := os.WriteFile(f, data, os.ModePerm)
 	require.NoError(t, err)
 
 	s, err := GetFileAllocSize(f)
-	if err != nil {
-		t.Fatalf("error getting file alloc size for %s: %v", f, err)
-	}
 
-	if s < size {
-		t.Fatalf("invalid allocated file size %d, expected at least %d", s, size)
-	}
+	require.NoError(t, err, "error getting file alloc size for %s: %v", f, err)
+	require.GreaterOrEqual(t, s, uint64(writeSize), "invalid allocated file size %d, expected at least %d", s, writeSize)
 }
