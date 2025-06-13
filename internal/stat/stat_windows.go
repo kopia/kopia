fd4ab8f12618031b63ba27b0db823d2f068e50fa
J <1953782+julio-lopez@users.noreply.github.com>
2025-06-13 12:30:35 -0700

    stat.GetBlockSize for windows

--- internal/stat/stat_windows.go
+++ internal/stat/stat_windows.go
@@ -5,7 +5,14 @@
 // common stat commands.
 package stat
 
-import "errors"
+import (
+	"math"
+	"runtime"
+	"unsafe"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sys/windows"
+)
 
 var errNotImplemented = errors.New("not implemented")
 
@@ -21,5 +28,22 @@ func GetFileAllocSize(fname string) (uint64, error) {
 //
 //nolint:revive
 func GetBlockSize(path string) (uint64, error) {
-	return 0, errNotImplemented
+	kernel32 := windows.NewLazyDLL("kernel32.dll")
+	getDiskFreeSpace := kernel32.NewProc("GetDiskFreeSpaceW")
+
+	var sectorsPerCluster, bytesPerSector, freeClusters, totalClusters uint32
+
+	ret, _, err := getDiskFreeSpace.Call(
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(path))),
+		uintptr(unsafe.Pointer(&sectorsPerCluster)),
+		uintptr(unsafe.Pointer(&bytesPerSector)),
+		uintptr(unsafe.Pointer(&freeClusters)),
+		uintptr(unsafe.Pointer(&totalClusters)),
+	)
+	if ret == 0 {
+		return math.MaxUint64, errors.Wrapf(err, "Error while getting block size for %v", runtime.GOOS)
+	}
+
+	// Calculate the block size as sectors per cluster * bytes per sector
+	return uint64(sectorsPerCluster) * uint64(bytesPerSector), nil
 }
