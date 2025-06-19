3052a7218d35a01325106d33852fa94427c005b2
Jarek Kowalski <jaak@jkowalski.net>
2025-06-19 12:59:17 -0700

    fix(server): fixed snapshot scheduling in the event of machine goes to sleep
    Fixes #3512

---
+++ internal/sleepable/sleepable_timer.go
@@ -0,0 +1,78 @@
+// Package sleepable implements a timer that can trigger at or soon after a given time and supports the computer going to sleep while the timer is waiting.
+package sleepable
+
+import (
+	"sync"
+	"time"
+)
+
+// MaxSleepTime is the maximum duration the timer will sleep in one interval. Can be overridden for testing.
+var MaxSleepTime = 15 * time.Second
+
+// Timer is a timer that can trigger at or soon after a given time and supports the computer going to sleep while the timer is waiting.
+type Timer struct {
+	C        <-chan struct{}
+	stopChan chan struct{}
+	stopOnce sync.Once
+}
+
+// Stop stops the timer.
+func (c *Timer) Stop() {
+	c.stopOnce.Do(func() {
+		close(c.stopChan)
+	})
+}
+
+// NewTimer creates a new timer that will trigger at the given time.
+func NewTimer(nowFunc func() time.Time, until time.Time) *Timer {
+	ch := make(chan struct{})
+
+	t := &Timer{
+		C:        ch,
+		stopChan: make(chan struct{}),
+	}
+
+	var currentTimer *time.Timer
+
+	// start a goroutine that will sleep until the timer is triggered or the timer is stopped.
+	go func() {
+		defer func() {
+			if currentTimer != nil {
+				currentTimer.Stop()
+			}
+		}()
+
+		for {
+			now := nowFunc()
+
+			if now.After(until) {
+				close(ch)
+				return
+			}
+
+			nextSleepTime := until.Sub(now)
+			if nextSleepTime > MaxSleepTime {
+				nextSleepTime = MaxSleepTime
+			}
+
+			if currentTimer != nil {
+				currentTimer.Stop()
+			}
+
+			currentTimer = time.NewTimer(nextSleepTime)
+
+			select {
+			case <-t.stopChan:
+				// stop channel was closed, exit without closing the "C" channel
+				return
+
+			case <-currentTimer.C:
+				// timer did trigger, close the channel to signal that sleepable.Timer.C is done
+				close(ch)
+				return
+			}
+		}
+	}()
+
+	return t
+}
