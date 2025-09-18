fb7ecee534828c753c3fb3da2f853508f1ff1fc0
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2025-09-17 21:44:25 -0700

    refactor(general): move `content verify` functionality to the `content` package (#4827)
    Move general functionality from the `content verify` CLI
    command implementation to helpers in the content package.
    
    The primary motivation is to allow reusing the content
    verification functionality during maintenance.
    
    A separate followup change also extends content
    verification to include additional stats useful for
    debugging repository corruptions.
    
    Overview of the changes:
    - Relocation of the content verification functionality
      to the content package. The entry point is
      content.WriteManager.VerifyContents.
      This is primarily code movement with no functional changes.
    - Addition of unit tests for the content verification functionality
      by exercising content.WriteManager.VerifyContents.
    - Minor functional change: changing the logging level from
      Error to Warn for the "inner loop" error messages. This allows
      filtering out these messages if needed, while still observing the
      error message that is logged for the overall operation.

--- cli/command_content_verify.go
+++ cli/command_content_verify.go
@@ -2,7 +2,6 @@ package cli
 
 import (
 	"context"
-	"math/rand"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -11,7 +10,6 @@ import (
 
 	"github.com/kopia/kopia/internal/timetrack"
 	"github.com/kopia/kopia/repo"
-	"github.com/kopia/kopia/repo/blob"
 	"github.com/kopia/kopia/repo/content"
 )
 
@@ -38,27 +36,13 @@ func (c *commandContentVerify) setup(svc appServices, parent commandParent) {
 }
 
 func (c *commandContentVerify) run(ctx context.Context, rep repo.DirectRepository) error {
-	downloadPercent := c.contentVerifyPercent
-
-	if c.contentVerifyFull {
-		downloadPercent = 100.0
-	}
-
-	blobMap, err := blob.ReadBlobMap(ctx, rep.BlobReader())
-	if err != nil {
-		return errors.Wrap(err, "unable to read blob map")
-	}
-
 	var (
-		verifiedCount atomic.Int32
-		successCount  atomic.Int32
-		errorCount    atomic.Int32
-		totalCount    atomic.Int32
+		totalCount atomic.Int32
+
+		wg sync.WaitGroup
 	)
 
 	subctx, cancel := context.WithCancel(ctx)
-
-	var wg sync.WaitGroup
 
 	// ensure we cancel estimation goroutine and wait for it before returning
 	defer func() {
@@ -74,56 +58,50 @@ func (c *commandContentVerify) run(ctx context.Context, rep repo.DirectRepositor
 		c.getTotalContentCount(subctx, rep, &totalCount)
 	}()
 
-	log(ctx).Info("Verifying all contents...")
-
 	rep.DisableIndexRefresh()
 
-	throttle := new(timetrack.Throttle)
+	var throttle timetrack.Throttle
+
 	est := timetrack.Start()
 
-	if err := rep.ContentReader().IterateContents(ctx, content.IterateOptions{
-		Range:          c.contentRange.contentIDRange(),
-		Parallel:       c.contentVerifyParallel,
-		IncludeDeleted: c.contentVerifyIncludeDeleted,
-	}, func(ci content.Info) error {
-		if err := c.contentVerify(ctx, rep.ContentReader(), ci, blobMap, downloadPercent); err != nil {
-			log(ctx).Errorf("error %v", err)
-			errorCount.Add(1)
-		} else {
-			successCount.Add(1)
-		}
+	if c.contentVerifyFull {
+		c.contentVerifyPercent = 100.0
+	}
 
-		verifiedCount.Add(1)
+	opts := content.VerifyOptions{
+		ContentIDRange:            c.contentRange.contentIDRange(),
+		ContentReadPercentage:     c.contentVerifyPercent,
+		IncludeDeletedContents:    c.contentVerifyIncludeDeleted,
+		ContentIterateParallelism: c.contentVerifyParallel,
+		ProgressCallbackInterval:  1,
 
-		if throttle.ShouldOutput(c.progressInterval) {
-			timings, ok := est.Estimate(float64(verifiedCount.Load()), float64(totalCount.Load()))
+		ProgressCallback: func(vps content.VerifyProgressStats) {
+			if !throttle.ShouldOutput(c.progressInterval) {
+				return
+			}
+
+			verifiedCount := vps.SuccessCount + vps.ErrorCount
+			timings, ok := est.Estimate(float64(verifiedCount), float64(totalCount.Load()))
 			if ok {
 				log(ctx).Infof("  Verified %v of %v contents (%.1f%%), %v errors, remaining %v, ETA %v",
-					verifiedCount.Load(),
+					verifiedCount,
 					totalCount.Load(),
 					timings.PercentComplete,
-					errorCount.Load(),
+					vps.ErrorCount,
 					timings.Remaining,
 					formatTimestamp(timings.EstimatedEndTime),
 				)
 			} else {
-				log(ctx).Infof("  Verified %v contents, %v errors, estimating...", verifiedCount.Load(), errorCount.Load())
+				log(ctx).Infof("  Verified %v contents, %v errors, estimating...", verifiedCount, vps.ErrorCount)
 			}
-		}
-
-		return nil
-	}); err != nil {
-		return errors.Wrap(err, "iterate contents")
+		},
 	}
 
-	log(ctx).Infof("Finished verifying %v contents, found %v errors.", verifiedCount.Load(), errorCount.Load())
-
-	ec := errorCount.Load()
-	if ec == 0 {
-		return nil
+	if err := rep.ContentReader().VerifyContents(ctx, opts); err != nil {
+		return errors.Wrap(err, "verify contents")
 	}
 
-	return errors.Errorf("encountered %v errors", ec)
+	return nil
 }
 
 func (c *commandContentVerify) getTotalContentCount(ctx context.Context, rep repo.DirectRepository, totalCount *atomic.Int32) {
@@ -145,26 +123,4 @@ func (c *commandContentVerify) getTotalContentCount(ctx context.Context, rep rep
 	}
 
 	totalCount.Store(tc)
-}
-
-func (c *commandContentVerify) contentVerify(ctx context.Context, r content.Reader, ci content.Info, blobMap map[blob.ID]blob.Metadata, downloadPercent float64) error {
-	bi, ok := blobMap[ci.PackBlobID]
-	if !ok {
-		return errors.Errorf("content %v depends on missing blob %v", ci.ContentID, ci.PackBlobID)
-	}
-
-	if int64(ci.PackOffset+ci.PackedLength) > bi.Length {
-		return errors.Errorf("content %v out of bounds of its pack blob %v", ci.ContentID, ci.PackBlobID)
-	}
-
-	//nolint:gosec
-	if 100*rand.Float64() < downloadPercent {
-		if _, err := r.GetContent(ctx, ci.ContentID); err != nil {
-			return errors.Wrapf(err, "content %v is invalid", ci.ContentID)
-		}
-
-		return nil
-	}
-
-	return nil
 }
