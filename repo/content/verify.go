490fa4b5b34fcfe4f7018ab9a0994f811eda9889
Julio Lopez <1953782+julio-lopez@users.noreply.github.com>
2025-09-17 23:36:09 -0700

    feat(cli): extend stats for content verification (#4830)
    Track and report errors separately according to the type of error:
    - missing pack
    - truncated pack
    - unreadable content.
    
    Add a counter stat for the contents that are
    read and fully verified (via `GetContent`).
    
    Count errors grouped by pack ID using a `CountersMap`.
    This allows determining the number of referenced contents
    that were missing in a particular pack.
    
    Report the counter stats via structured logging.
    
    ---
    
    Sample output:
    
    $ kopia content verify --progress-interval=0.5s --download-percent=100
    Listing blobs...
    Listed 102 blobs.
    Verifying contents...
      Verified 1 contents, 0 errors, estimating...
      Verified 279 contents, 0 errors, estimating...
      Verified 512 of 624 contents (82.1%), 0 errors, remaining 0s, ETA 2025-09-17 23:03:38 PDT
    Finished verifying contents
    verifyCounters:	{"verifiedContents":624,"totalErrorCount":0,"contentsInMissingPacks":0,\
    "contentsInTruncatedPacks":0,"unreadableContents":0,"readContents":624,\
    "missingPacks":0,"truncatedPacks":0,"corruptedPacks":0}

--- repo/content/verify.go
+++ repo/content/verify.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/kopia/kopia/internal/stats"
 	"github.com/kopia/kopia/repo/blob"
 	"github.com/kopia/kopia/repo/logging"
 )
@@ -53,8 +54,18 @@ type contentVerifier struct {
 	contentReadProbability float64
 
 	// content verification stats
-	successCount atomic.Uint32
-	errorCount   atomic.Uint32
+	successCount              atomic.Uint32
+	readContentCount          atomic.Uint32
+	missingPackContentCount   atomic.Uint32
+	truncatedPackContentCount atomic.Uint32
+	errorContentCount         atomic.Uint32
+
+	// Per pack counts for content errors. Notice that a truncated pack, can
+	// also appear in the corruptedPacks map. A missing pack can only be in
+	// missingPacks, but not in the other sets.
+	missingPacks   stats.CountersMap[blob.ID]
+	truncatedPacks stats.CountersMap[blob.ID]
+	corruptedPacks stats.CountersMap[blob.ID]
 
 	verifiedCount atomic.Uint32 // used for calling the progress callback at the specified interval.
 
@@ -93,17 +104,36 @@ func (v *contentVerifier) verifyContents(ctx context.Context, bm *WriteManager, 
 
 	err = bm.IterateContents(ctx, itOpts, cb)
 
-	ec := v.errorCount.Load()
-	contentCount := v.successCount.Load() + ec
+	contentInMissingPackCount := v.missingPackContentCount.Load()
+	contentInTruncatedPackCount := v.truncatedPackContentCount.Load()
+	contentErrorCount := v.errorContentCount.Load()
+	totalErrorCount := contentInMissingPackCount + contentInTruncatedPackCount + contentErrorCount
 
-	v.log.Infof("Finished verifying %v contents, found %v errors.", contentCount, ec)
+	contentCount := v.verifiedCount.Load()
+
+	v.log.Info("Finished verifying contents")
+	v.log.Infow("verifyCounters:",
+		"verifiedContents", contentCount,
+		"totalErrorCount", totalErrorCount,
+		"contentsInMissingPacks", contentInMissingPackCount,
+		"contentsInTruncatedPacks", contentInTruncatedPackCount,
+		"unreadableContents", contentErrorCount,
+		"readContents", v.readContentCount.Load(),
+		"missingPacks", v.missingPacks.Length(),
+		"truncatedPacks", v.truncatedPacks.Length(),
+		"corruptedPacks", v.corruptedPacks.Length(),
+	)
+
+	logCountMap(v.log, "missingPack", &v.missingPacks)
+	logCountMap(v.log, "truncatedPack", &v.truncatedPacks)
+	logCountMap(v.log, "corruptedPack", &v.corruptedPacks)
 
 	if err != nil {
 		return err
 	}
 
-	if ec != 0 {
-		return errors.Wrapf(errMissingPacks, "encountered %v errors", ec)
+	if totalErrorCount != 0 {
+		return errors.Wrapf(errMissingPacks, "encountered %v errors", totalErrorCount)
 	}
 
 	return nil
@@ -119,7 +149,7 @@ func (v *contentVerifier) verify(ctx context.Context, ci Info) {
 	if v.progressCallbackInterval > 0 && count%v.progressCallbackInterval == 0 {
 		s := VerifyProgressStats{
 			SuccessCount: v.successCount.Load(),
-			ErrorCount:   v.errorCount.Load(),
+			ErrorCount:   v.missingPackContentCount.Load() + v.truncatedPackContentCount.Load() + v.errorContentCount.Load(),
 		}
 
 		v.progressCallback(s)
@@ -129,14 +159,16 @@ func (v *contentVerifier) verify(ctx context.Context, ci Info) {
 func (v *contentVerifier) verifyContentImpl(ctx context.Context, ci Info) {
 	bi, found := v.existingPacks[ci.PackBlobID]
 	if !found {
-		v.errorCount.Add(1)
+		v.missingPackContentCount.Add(1)
+		v.missingPacks.Increment(ci.PackBlobID)
 		v.log.Warnf("content %v depends on missing blob %v", ci.ContentID, ci.PackBlobID)
 
 		return
 	}
 
 	if int64(ci.PackOffset+ci.PackedLength) > bi.Length {
-		v.errorCount.Add(1)
+		v.truncatedPackContentCount.Add(1)
+		v.truncatedPacks.Increment(ci.PackBlobID)
 		v.log.Warnf("content %v out of bounds of its pack blob %v", ci.ContentID, ci.PackBlobID)
 
 		return
@@ -144,8 +176,11 @@ func (v *contentVerifier) verifyContentImpl(ctx context.Context, ci Info) {
 
 	//nolint:gosec
 	if v.contentReadProbability > 0 && rand.Float64() < v.contentReadProbability {
+		v.readContentCount.Add(1)
+
 		if _, err := v.bm.GetContent(ctx, ci.ContentID); err != nil {
-			v.errorCount.Add(1)
+			v.errorContentCount.Add(1)
+			v.corruptedPacks.Increment(ci.PackBlobID)
 			v.log.Warnf("content %v is invalid: %v", ci.ContentID, err)
 
 			return
@@ -153,4 +188,13 @@ func (v *contentVerifier) verifyContentImpl(ctx context.Context, ci Info) {
 	}
 
 	v.successCount.Add(1)
+}
+
+// nothing is logged for an empty map.
+func logCountMap(log logging.Logger, mapName string, m *stats.CountersMap[blob.ID]) {
+	m.Range(func(packID blob.ID, contentCount uint32) bool {
+		log.Warnw(mapName, "packID", packID, "numberOfReferencedContents", contentCount)
+
+		return true
+	})
 }
