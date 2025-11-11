e8582df750907d869bb01a94a23579f44cf76d43
Nathan Baulch <nathan.baulch@gmail.com>
2025-11-11 11:43:23 +1100

    fix: macos build error

--- internal/mount/mount_posix_webdav_helper_darwin.go
+++ internal/mount/mount_posix_webdav_helper_darwin.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-func mountWebDavHelper(_ context.Context, url, path string) error {
+func mountWebDavHelper(ctx context.Context, url, path string) error {
 	mount := exec.CommandContext(ctx, "/sbin/mount", "-t", "webdav", "-r", url, path)
 	if err := mount.Run(); err != nil {
 		return errors.Errorf("webdav mount %q on %q failed: %v", url, path, err)
@@ -16,7 +16,7 @@ func mountWebDavHelper(_ context.Context, url, path string) error {
 	return nil
 }
 
-func unmountWebDevHelper(_ context.Context, path string) error {
+func unmountWebDevHelper(ctx context.Context, path string) error {
 	unmount := exec.CommandContext(ctx, "/usr/sbin/diskutil", "unmount", path)
 	if err := unmount.Run(); err != nil {
 		return errors.Errorf("unmount %q failed: %v", path, err)
