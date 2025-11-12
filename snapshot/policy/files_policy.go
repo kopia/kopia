35a2d4e7617bac58fb84377f0a0c4bc0ff70ca5b
J <1953782+julio-lopez@users.noreply.github.com>
2025-11-12 14:06:03 -0800

    Use `omitzero` in `FilesPolicy`
    The behavior for `omitempty` and `omitzero` differs for slices, and
    maps as well.
    
    The struct fields of slice type (e.g., []string) are tagged with
    `omitempty` to be able to tell the difference between a nil slice
    and a non-nil zero-length slice.

--- snapshot/policy/files_policy.go
+++ snapshot/policy/files_policy.go
@@ -4,24 +4,24 @@ import "github.com/kopia/kopia/snapshot"
 
 // FilesPolicy describes files to be ignored when taking snapshots.
 type FilesPolicy struct {
-	IgnoreRules            []string      `json:"ignore,omitempty"`
-	NoParentIgnoreRules    bool          `json:"noParentIgnore,omitempty"`
-	DotIgnoreFiles         []string      `json:"ignoreDotFiles,omitempty"`
-	NoParentDotIgnoreFiles bool          `json:"noParentDotFiles,omitempty"`
-	IgnoreCacheDirectories *OptionalBool `json:"ignoreCacheDirs,omitempty"`
-	MaxFileSize            int64         `json:"maxFileSize,omitempty"`
-	OneFileSystem          *OptionalBool `json:"oneFileSystem,omitempty"`
+	IgnoreRules            []string      `json:"ignore,omitzero"`
+	NoParentIgnoreRules    bool          `json:"noParentIgnore,omitzero"`
+	DotIgnoreFiles         []string      `json:"ignoreDotFiles,omitzero"`
+	NoParentDotIgnoreFiles bool          `json:"noParentDotFiles,omitzero"`
+	IgnoreCacheDirectories *OptionalBool `json:"ignoreCacheDirs,omitzero"`
+	MaxFileSize            int64         `json:"maxFileSize,omitzero"`
+	OneFileSystem          *OptionalBool `json:"oneFileSystem,omitzero"`
 }
 
 // FilesPolicyDefinition specifies which policy definition provided the value of a particular field.
 type FilesPolicyDefinition struct {
-	IgnoreRules            snapshot.SourceInfo `json:"ignore,omitempty"`
-	NoParentIgnoreRules    snapshot.SourceInfo `json:"noParentIgnore,omitempty"`
-	DotIgnoreFiles         snapshot.SourceInfo `json:"ignoreDotFiles,omitempty"`
-	NoParentDotIgnoreFiles snapshot.SourceInfo `json:"noParentDotFiles,omitempty"`
-	IgnoreCacheDirectories snapshot.SourceInfo `json:"ignoreCacheDirs,omitempty"`
-	MaxFileSize            snapshot.SourceInfo `json:"maxFileSize,omitempty"`
-	OneFileSystem          snapshot.SourceInfo `json:"oneFileSystem,omitempty"`
+	IgnoreRules            snapshot.SourceInfo `json:"ignore,omitzero"`
+	NoParentIgnoreRules    snapshot.SourceInfo `json:"noParentIgnore,omitzero"`
+	DotIgnoreFiles         snapshot.SourceInfo `json:"ignoreDotFiles,omitzero"`
+	NoParentDotIgnoreFiles snapshot.SourceInfo `json:"noParentDotFiles,omitzero"`
+	IgnoreCacheDirectories snapshot.SourceInfo `json:"ignoreCacheDirs,omitzero"`
+	MaxFileSize            snapshot.SourceInfo `json:"maxFileSize,omitzero"`
+	OneFileSystem          snapshot.SourceInfo `json:"oneFileSystem,omitzero"`
 }
 
 // Merge applies default values from the provided policy.
