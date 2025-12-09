829036340568e72b574356fc2b61b50a24e60973
Jonathan Melly <jonathan.melly@gmail.com>
2025-12-09 09:15:07 +0100

    chore(btime support): indicate target oses for unsupported btime

--- fs/localfs/local_fs_btime_unix.go
+++ fs/localfs/local_fs_btime_unix.go
@@ -6,6 +6,8 @@ import (
 	"syscall"
 )
 
+// platformSpecificBirthTimeFromStat is a fallback for Unix systems without birth time support.
+// This applies to: OpenBSD, NetBSD, Solaris, AIX, and other Unix-like systems.
 func platformSpecificBirthTimeFromStat(_ *syscall.Stat_t, path string) int64 {
 	// Birth time not supported on this platform
 	return 0
