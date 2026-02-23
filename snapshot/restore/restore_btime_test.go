362a0cfbaa367edf2f528b3f8ea521569a909789
Jonathan Melly <jmy@ik.me>
2026-02-23 10:19:09 +0100

    chore(btime): align to time directives

--- snapshot/restore/restore_btime_test.go
+++ snapshot/restore/restore_btime_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/kopia/kopia/fs"
 	"github.com/kopia/kopia/fs/localfs"
+	"github.com/kopia/kopia/internal/clock"
 	"github.com/kopia/kopia/internal/repotesting"
 	"github.com/kopia/kopia/repo"
 	"github.com/kopia/kopia/snapshot"
@@ -64,7 +65,7 @@ func setupTestFile(t *testing.T, dir string) string {
 	require.NoError(t, os.WriteFile(testFile, []byte("test"), 0o644))
 
 	// Set mtime to 1 minute in the future to differentiate from birth time
-	futureTime := time.Now().Add(1 * time.Minute) //nolint:forbidigo
+	futureTime := clock.Now().Add(1 * time.Minute)
 	require.NoError(t, os.Chtimes(testFile, futureTime, futureTime))
 
 	return testFile
@@ -310,7 +311,7 @@ func restoreSnapshot(t *testing.T, ctx context.Context, rep repo.RepositoryWrite
 func assertTimeIsRecent(t *testing.T, timeToCheck time.Time, message string) {
 	t.Helper()
 
-	timeSince := time.Since(timeToCheck) //nolint:forbidigo
+	timeSince := clock.Now().Sub(timeToCheck)
 	require.GreaterOrEqual(t, timeSince, time.Duration(0), message+" (should not be in the future)")
 	require.Less(t, timeSince, 2*time.Minute, message+" (should be within last 2 minutes)")
 }
