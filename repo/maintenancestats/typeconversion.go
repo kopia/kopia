0d55678bc8f52b4b8cc00cb6f94a4f295192960a
J <1953782+julio-lopez@users.noreply.github.com>
2026-04-10 21:58:37 -0700

    rename package variable for clarity

--- repo/maintenancestats/typeconversion.go
+++ repo/maintenancestats/typeconversion.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/time/rate"
 )
 
-var limit = rate.Sometimes{First: 10} //nolint:mnd
+var negativeValueWarningLimit = rate.Sometimes{First: 10} //nolint:mnd
 
 // ToUint64 converts v from a signed integer type T to uint64 while checking that
 // the value is non-negative. It returns 0 for negative values.
@@ -15,7 +15,7 @@ func ToUint64[T int8 | int16 | int32 | int | int64](v T) uint64 {
 		return uint64(v)
 	}
 
-	limit.Do(func() {
+	negativeValueWarningLimit.Do(func() {
 		log.Println("warning, converting negative value to uint64:", v)
 	})
 
