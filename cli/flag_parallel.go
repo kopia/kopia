c7fd4ef10bef7bf11975e981d08b503e1e94c90c
Julio López <1953782+julio-lopez@users.noreply.github.com>
2026-09-09 18:49:18 -0700

    address linter issue

--- cli/flag_parallel.go
+++ cli/flag_parallel.go
@@ -2,7 +2,7 @@ package cli
 
 import "math"
 
-// parallelismAsInt converts --parallel-style flag values from 
+// parallelismAsInt converts --parallel-style flag values from
 // uint to int and clamps returned values at MaxInt32.
 func parallelismAsInt(v uint) int {
 	if v > math.MaxInt32 {
