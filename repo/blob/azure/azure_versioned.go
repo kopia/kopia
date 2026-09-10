d8b1c641c4a8d9f9135921f47791cc3294a3bdaa
J <1953782+julio-lopez@users.noreply.github.com>
2026-09-09 21:18:00 -0700

    DNM: print blob name in error

--- repo/blob/azure/azure_versioned.go
+++ repo/blob/azure/azure_versioned.go
@@ -22,7 +22,7 @@ type versionMetadataCallback func(versionMetadata) error
 
 func (az *azPointInTimeStorage) getVersionedBlobMeta(it *azblobmodels.BlobItem) (*versionMetadata, error) {
 	if it.VersionID == nil {
-		return nil, errors.New("versionID is nil. Versioning must be enabled on the container for PIT")
+		return nil, errors.Errorf("versionID is nil for %q: versioning must be enabled on the container for PIT", *it.Name)
 	}
 
 	bm := az.getBlobMeta(it)
