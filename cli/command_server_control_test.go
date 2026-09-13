26e92b61ace81ea3dc5ffa08c28a7fa9aad587dd
J <1953782+julio-lopez@users.noreply.github.com>
2026-09-12 17:19:57 -0700

    refactor(testing): TestServerControl

--- cli/command_server_control_test.go
+++ cli/command_server_control_test.go
@@ -28,29 +28,12 @@ func TestServerControl(t *testing.T) {
 	env.RunAndExpectSuccess(t, "snap", "create", dir1)
 	env.RunAndExpectSuccess(t, "snap", "create", dir2)
 
-	serverStarted := make(chan struct{})
-	serverStopped := make(chan struct{})
-
 	var sp testutil.ServerParameters
 
-	go func() {
-		wait, _ := env.RunAndProcessStderr(t, sp.ProcessOutput,
-			"server", "start", "--insecure", "--random-server-control-password", "--address=127.0.0.1:0")
+	wait, _ := env.RunAndProcessStderr(t, sp.ProcessOutput,
+		"server", "start", "--insecure", "--random-server-control-password", "--address=127.0.0.1:0")
 
-		close(serverStarted)
-
-		wait()
-
-		close(serverStopped)
-	}()
-
-	select {
-	case <-serverStarted:
-		t.Logf("server started on %v", sp.BaseURL)
-
-	case <-time.After(5 * time.Second):
-		t.Fatalf("server did not start in time")
-	}
+	t.Logf("server started on %v", sp.BaseURL)
 
 	const (
 		pollFrequency = 100 * time.Millisecond
@@ -150,6 +133,14 @@ func TestServerControl(t *testing.T) {
 	}, limits)
 
 	env.RunAndExpectSuccess(t, "server", "shutdown", "--address", sp.BaseURL, "--server-control-password", sp.ServerControlPassword)
+
+	serverStopped := make(chan struct{})
+
+	go func() {
+		wait()
+
+		close(serverStopped)
+	}()
 
 	select {
 	case <-serverStopped:
